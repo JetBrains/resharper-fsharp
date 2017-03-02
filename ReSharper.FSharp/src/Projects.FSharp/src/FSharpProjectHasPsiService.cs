92d9e9c3f0106bf085d7ed5fe40e4ae8e7bf2215
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-03-02 17:51:54 +0300

    Override ProjectHasPsiService

---
+++ ReSharper.FSharp/src/Projects.FSharp/src/FSharpProjectHasPsiService.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Application;
+using JetBrains.Application.Components;
+using JetBrains.DataFlow;
+using JetBrains.Platform.ProjectModel.FSharp.Properties;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Interfaces;
+using JetBrains.Util;
+
+namespace JetBrains.Platform.ProjectModel.FSharp
+{
+  /// <summary>
+  /// Should be removed once ReSharper.SDK allows to create new project languages.
+  /// </summary>
+  [ShellComponent]
+  public class FSharpProjectHasPsiService : ProjectHasPsiService, IProjectHasPsiService,
+    IHideImplementation<ProjectHasPsiService>
+  {
+    public FSharpProjectHasPsiService(Lifetime lifetime, ILogger logger) : base(lifetime, logger)
+    {
+    }
+
+    public new bool ProjectHasPsi(IProject project)
+    {
+      if (project.ProjectProperties is FSharpProjectProperties) return true;
+      return base.ProjectHasPsi(project);
+    }
+  }
+}
