e14ad5444d8911deb2039aa3213b74aab903d0ef
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-03-13 13:48:26 +0300

    Remove redundant checks during set resolved symbols stage

--- ReSharper.FSharp/src/Daemon.FSharp/src/Stages/SetResolvedSymbolsStage.cs
+++ ReSharper.FSharp/src/Daemon.FSharp/src/Stages/SetResolvedSymbolsStage.cs
@@ -5,7 +5,6 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi.FSharp.Impl.Tree;
 using JetBrains.ReSharper.Psi.FSharp.Tree;
 using JetBrains.ReSharper.Psi.FSharp.Util;
-using JetBrains.Util.Extension;
 using Microsoft.FSharp.Compiler.SourceCodeServices;
 
 namespace JetBrains.ReSharper.Daemon.FSharp.Stages
@@ -21,9 +20,6 @@ namespace JetBrains.ReSharper.Daemon.FSharp.Stages
 
   public class SetResolvedSymbolsStageProcess : FSharpDaemonStageProcessBase
   {
-    private const string AttributeSuffix = "Attribute";
-    private const int EscapedNameAffixLength = 4;
-
     private readonly IFSharpFile myFsFile;
     private readonly IDocument myDocument;
 
@@ -71,26 +67,7 @@ namespace JetBrains.ReSharper.Daemon.FSharp.Stages
 
       // range includes qualifiers, we're looking for the last identifier
       var endOffset = FSharpRangeUtil.GetEndOffset(myDocument, symbolUse.RangeAlternate) - 1;
-      var token = myFsFile.FindTokenAt(endOffset) as FSharpIdentifierToken;
-      if (token == null) return null;
-
-      // if token length matches the name length then found the right token
-      // otherwise name in code differs from symbol display name, trying to find it
-      // todo: check if the last identifier is always the one needed
-      if (name.Length == token.Length) return token;
-
-      // "Some" or "SomeAttribute" in element attribute, "SomeAttribute" elsewhere
-      var attrName = symbolUse.IsFromAttribute ? name.SubstringBeforeLast(AttributeSuffix) : null;
-      if (attrName != null && attrName.Length == token.Length) return token;
-
-      // e.g. name: "( |> )", token: "|>"
-      if (FSharpNamesUtil.IsEscapedWithParens(name) && name.Length - EscapedNameAffixLength == token.Length) return token;
-
-      // e.g. name: "foo bar", token: "``foo bar``"
-      if (name.Length + EscapedNameAffixLength == token.Length &&
-          name == FSharpNamesUtil.RemoveBackticks(token.GetText())) return token;
-
-      return null;
+      return myFsFile.FindTokenAt(endOffset) as FSharpIdentifierToken;
     }
   }
 }
