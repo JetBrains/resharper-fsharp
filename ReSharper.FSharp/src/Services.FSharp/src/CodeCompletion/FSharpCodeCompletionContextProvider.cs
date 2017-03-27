08909cce74f2f9579d85bc07486cf11728220b86
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-03-28 01:12:30 +0300

    Fix race in code completion context provider

--- ReSharper.FSharp/src/Services.FSharp/src/CodeCompletion/FSharpCodeCompletionContextProvider.cs
+++ ReSharper.FSharp/src/Services.FSharp/src/CodeCompletion/FSharpCodeCompletionContextProvider.cs
@@ -1,7 +1,6 @@
 ﻿using JetBrains.Annotations;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion.Impl;
 using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure;
-using JetBrains.ReSharper.Feature.Services.Util;
 using JetBrains.ReSharper.Psi.FSharp.Parsing;
 using JetBrains.ReSharper.Psi.FSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
@@ -19,23 +18,23 @@ namespace JetBrains.ReSharper.Feature.Services.FSharp.CodeCompletion
 
     public override ISpecificCodeCompletionContext GetCompletionContext(CodeCompletionContext context)
     {
-      var caretOffset = context.CaretTreeOffset.Offset;
-      var solution = context.Solution;
-      var textControl = context.TextControl;
+      var file = context.File;
+      var caretTreeOffset = context.CaretTreeOffset;
+      var caretOffset = caretTreeOffset.Offset;
 
-      var tokenAtCaret = TextControlToPsi.GetSourceTokenAtCaret(solution, textControl);
-      var tokenBeforeCaret = TextControlToPsi.GetSourceTokenBeforeCaret(solution, textControl);
+      var tokenBeforeCaret = file.FindTokenAt(caretTreeOffset - 1);
+      var tokenAtCaret = file.FindTokenAt(caretTreeOffset);
       var tokenType = tokenBeforeCaret?.GetTokenType();
 
       if (tokenAtCaret == tokenBeforeCaret && tokenType != null &&
           (tokenType.IsComment || tokenType.IsStringLiteral || tokenType.IsConstantLiteral))
         return null;
 
-      var completedStartRange =
+      var completedRangeStartOffset =
         tokenType != null && (tokenType == FSharpTokenType.IDENTIFIER || tokenType.IsKeyword)
           ? tokenBeforeCaret.GetTreeStartOffset().Offset
           : caretOffset;
-      var completedRange = new TextRange(completedStartRange, caretOffset);
+      var completedRange = new TextRange(completedRangeStartOffset, caretOffset);
       var defaultRanges = GetTextLookupRanges(context, completedRange);
 
       var ranges = ShouldReplace(tokenAtCaret)
