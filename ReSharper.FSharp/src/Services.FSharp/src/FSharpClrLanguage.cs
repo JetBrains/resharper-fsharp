623ea46a8e178f819651f330f546552ef88cd7a7
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-04-10 20:58:26 +0300

    Implement IClrLanguagesKnown component for F#
    Needed for proper handling output assemblies

---
+++ ReSharper.FSharp/src/Services.FSharp/src/FSharpClrLanguage.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Application;
+using JetBrains.ReSharper.Feature.Services.ClrLanguages;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.FSharp;
+
+namespace JetBrains.ReSharper.Feature.Services.FSharp
+{
+  [ShellComponent]
+  public class FSharpClrLanguage : IClrLanguagesKnown
+  {
+    public PsiLanguageType Language => FSharpLanguage.Instance;
+  }
+}
