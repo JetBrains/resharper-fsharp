6d812b37af533da6639c1764f7fdf3269b1f4f5f
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-06-02 21:04:17 +0300

    Disable ReSharper usage analysis in F# files

--- ReSharper.FSharp/src/FSharp.Daemon/src/UsageChecking/FSharpUsageCheckingService.fs
+++ ReSharper.FSharp/src/FSharp.Daemon/src/UsageChecking/FSharpUsageCheckingService.fs
@@ -1,11 +1,17 @@
-namespace JetBrains.ReSharper.Plugins.FSharp.Daemon
+namespace JetBrains.ReSharper.Plugins.FSharp.Daemon.UsageChecking
 
 open JetBrains.ReSharper.Daemon.UsageChecking
 open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.FSharp
 
+type FSharpDummyUsageAnalyzer(lifetime, suppressors) =
+    inherit UsageAnalyzer(lifetime, suppressors)
+    
+    override x.InteriorShouldBeProcessed(_,_) = false
+    override x.ProcessElement(_,_) = () 
+
 [<Language(typeof<FSharpLanguage>)>]
 type FSharpUsageCheckingServices(lifetime, suppressors) =
-    inherit UsageCheckingServices(lifetime, suppressors)
+    inherit UsageCheckingServices(FSharpDummyUsageAnalyzer(lifetime, suppressors), null, null)
     
     override x.CreateUnusedLocalDeclarationAnalyzer(_,_) = null
