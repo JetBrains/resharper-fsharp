a5696e2958762d2347886eb1d1f8d7df727bc4c4
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-09-10 01:00:48 +0300

    Add comment block action provider

---
+++ ReSharper.FSharp/src/FSharp.Services/Comment/FSharpBlockCommentActionProvider.fs
@@ -0,0 +1,34 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Services.Comment
+
+open JetBrains.ReSharper.Feature.Services.Comment
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Parsing
+open JetBrains.ReSharper.Psi
+open JetBrains.Util
+open System.Runtime.InteropServices
+
+[<Language(typeof<FSharpLanguage>)>]
+type FSharpBlockCommentActionProvider() = 
+    interface IBlockCommentActionProvider with
+        
+        member x.StartBlockCommentMarker = "(*"
+        member x.EndBlockCommentMarker = "*)"
+        member x.NestedStartBlockCommentMarker = null
+        member x.NestedEndBlockCommentMarker = null
+
+        member x.GetBlockComment(lexer) = 
+            if lexer.TokenType = FSharpTokenType.COMMENT then TextRange(lexer.TokenStart, lexer.TokenEnd)
+            else TextRange.InvalidRange
+
+        member x.IsAvailable(file, range, [<Out>] disableAllProviders) =
+            disableAllProviders <- false
+            true
+
+        member x.InsertBockCommentPosition(lexer, position) = 
+            let tokenType = lexer.TokenType
+            if tokenType = FSharpTokenType.LINE_COMMENT then position
+            else if tokenType = FSharpTokenType.WHITESPACE ||
+                    tokenType = FSharpTokenType.NEW_LINE ||
+                    tokenType = FSharpTokenType.COMMENT then lexer.TokenStart
+            else if position = lexer.TokenStart then position
+            else lexer.TokenEnd
