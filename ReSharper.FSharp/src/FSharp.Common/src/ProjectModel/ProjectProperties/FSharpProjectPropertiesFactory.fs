5af0e04ae8db2cbd0b294b9fdaeb896b53e34ce9
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-09-28 02:05:02 +0300

    Support opening VS2017 CPS-style F# projects

--- ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/ProjectProperties/FSharpProjectPropertiesFactory.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/ProjectProperties/FSharpProjectPropertiesFactory.fs
@@ -7,13 +7,16 @@ open JetBrains.Util
 
 let factoryGuid = Guid("{7B32A26D-3EC5-4A2A-B40C-EC79FF38A223}")
 let fsProjectTypeGuid = Guid("{F2A71F9B-5D33-465A-A702-920D77279786}")
+let fsCpsProjectTypeGuid = Guid("{6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705}")
 
 [<ProjectModelExtension>]
 type Factory() =
     inherit UnknownProjectPropertiesFactory()
 
-    override x.IsApplicable(parameters) = parameters.ProjectTypeGuid.Equals(fsProjectTypeGuid)
-    override x.IsKnownProjectTypeGuid(guid) = guid.Equals(fsProjectTypeGuid)
+    override x.IsApplicable(parameters) = x.IsKnownProjectTypeGuid(parameters.ProjectTypeGuid)
+
+    override x.IsKnownProjectTypeGuid(guid) = Factory.IsKnownProjectTypeGuid(guid)
+
     override x.FactoryGuid = factoryGuid
 
     override x.CreateProjectProperties(parameters) =
@@ -27,3 +30,5 @@ type Factory() =
         let projectProperties = FSharpProjectProperties(factoryGuid)
         projectProperties.ReadProjectProperties(reader, index)
         projectProperties :> _
+
+    static member IsKnownProjectTypeGuid(guid) = guid.Equals(fsProjectTypeGuid) || guid.Equals(fsCpsProjectTypeGuid)
