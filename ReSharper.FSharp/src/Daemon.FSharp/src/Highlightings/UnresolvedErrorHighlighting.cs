a1c54a4898296cbd81d655deefa62eb5f871548f
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-10-03 17:38:23 +0300

    Proper unresolved name errors overlapping

--- ReSharper.FSharp/src/Daemon.FSharp/src/Highlightings/UnresolvedErrorHighlighting.cs
+++ ReSharper.FSharp/src/Daemon.FSharp/src/Highlightings/UnresolvedErrorHighlighting.cs
@@ -5,7 +5,7 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 namespace JetBrains.ReSharper.Plugins.FSharp.Daemon.Cs.Highlightings
 {
   [StaticSeverityHighlighting(Severity.ERROR, HighlightingGroupIds.IdentifierHighlightingsGroup,
-    AttributeId = HighlightingAttributeIds.UNRESOLVED_ERROR_ATTRIBUTE, OverlapResolve = OverlapResolveKind.ERROR)]
+    AttributeId = HighlightingAttributeIds.UNRESOLVED_ERROR_ATTRIBUTE, OverlapResolve = OverlapResolveKind.UNRESOLVED_ERROR)]
   public class UnresolvedHighlighting : ErrorOrWarningHighlightingBase
   {
     public UnresolvedHighlighting([NotNull] string message, DocumentRange range) : base(message, range)
