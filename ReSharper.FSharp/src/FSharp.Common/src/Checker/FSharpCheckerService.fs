fbdd8631286b2544b902e04b1545475876bb51c6
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-10-09 18:49:13 +0300

    Pass single file to parser when msbuild project hasn't been loaded yet

--- ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpCheckerService.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpCheckerService.fs
@@ -1,25 +1,12 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 
-open System
-open System.Collections.Generic
-open System.Linq
-open System.Threading
 open JetBrains
 open JetBrains.Annotations
 open JetBrains.Application
 open JetBrains.Application.Progress
-open JetBrains.Application.Threading
-open JetBrains.DataFlow
-open JetBrains.ProjectModel
 open JetBrains.ReSharper.Feature.Services
 open JetBrains.ReSharper.Psi
-open JetBrains.ReSharper.Psi.Tree
-open JetBrains.ProjectModel.Properties
-open JetBrains.ProjectModel.Properties.CSharp
-open JetBrains.ProjectModel.Properties.Managed
 open JetBrains.ReSharper.Plugins.FSharp.Common.Util
-open JetBrains.ReSharper.Plugins.FSharp.ProjectModelBase
-open JetBrains.ReSharper.Plugins.FSharp.ProjectModel.ProjectProperties
 open JetBrains.ReSharper.Psi.Modules
 open Microsoft.FSharp.Compiler
 open Microsoft.FSharp.Compiler.SourceCodeServices
@@ -36,10 +23,11 @@ type FSharpParseAndCheckResults =
 
 [<ShellComponent>]
 type FSharpCheckerService(lifetime, logger: Util.ILogger, onSolutionCloseNotifier: OnSolutionCloseNotifier) =
+    do System.Environment.SetEnvironmentVariable("FCS_CheckFileInProjectCacheSize", "100")
     let checker = lazy FSharpChecker.Create(projectCacheSize = 200, keepAllBackgroundResolutions = false,
                                             legacyReferenceResolver = MSBuildReferenceResolver.Resolver)
     do
-        onSolutionCloseNotifier.SolutionIsAboutToClose.Advise(lifetime, fun () ->
+        onSolutionCloseNotifier.SolutionIsAboutToClose.Advise(lifetime, fun _ ->
             checker.Value.ClearLanguageServiceRootCachesAndCollectAndFinalizeAllTransients())
 
     member val OptionsProvider: IFSharpProjectOptionsProvider = null with get, set
@@ -47,18 +35,24 @@ type FSharpCheckerService(lifetime, logger: Util.ILogger, onSolutionCloseNotifie
 
     member x.ParseFile([<NotNull>] file: IPsiSourceFile) =
         match x.OptionsProvider.GetParsingOptions(file) with
-        | Some parsingOptions as options ->
+        | Some parsingOptions ->
             let filePath = file.GetLocation().FullPath
+            let parsingOptions =
+                if not (Array.isEmpty parsingOptions.SourceFiles) then parsingOptions
+                else
+                    let project  = file.GetProject().GetLocation().FullPath
+                    Logger.LogMessage(logger, LoggingLevel.WARN, "Loading from caches, don't have source files for {0} yet.", project)
+                    { parsingOptions with SourceFiles = [| filePath |] }
             let source = file.Document.GetText()
             try
                 let parseResults = x.Checker.ParseFile(filePath, source, parsingOptions).RunAsTask() 
-                options, Some parseResults
+                Some parsingOptions, Some parseResults
             with
             | :? ProcessCancelledException -> reraise()
             | exn ->
                 Util.Logging.Logger.LogException(exn)
                 Logger.LogMessage(logger, LoggingLevel.WARN, sprintf "Parse file error, parsing options: %A" parsingOptions)
-                options, None
+                Some parsingOptions, None
         | _ -> None, None
 
     member x.HasPairFile([<NotNull>] file: IPsiSourceFile) =
@@ -72,10 +66,13 @@ type FSharpCheckerService(lifetime, logger: Util.ILogger, onSolutionCloseNotifie
     member x.ParseAndCheckFile([<NotNull>] file: IPsiSourceFile, allowStaleResults) =
         match x.OptionsProvider.GetProjectOptions(file) with
         | Some options ->
-            let filePath = file.GetLocation().FullPath
+            let path = file.GetLocation().FullPath
+            if Array.isEmpty options.SourceFiles then
+                Logger.LogMessage(logger, LoggingLevel.WARN, "Requested type check for {0}, but msbuild project doesn't have any source files yet", path)
+
             let source = file.Document.GetText()
             // todo: don't cancel the computation when file didn't change
-            match x.Checker.ParseAndCheckDocument(filePath, source, options, allowStaleResults).RunAsTask() with
+            match x.Checker.ParseAndCheckDocument(path, source, options, allowStaleResults).RunAsTask() with
             | Some (parseResults, checkResults) when parseResults.ParseTree.IsSome ->
                 Some { ParseResults = parseResults; ParseTree = parseResults.ParseTree.Value; CheckResults = checkResults }
             | _ -> None
