2fda284ef75c3f3633e49502da41c06dbbbbd023
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-10-12 22:01:48 +0300

    Better extend selection for strings

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/SelectEmbracingConsruct/FSharpSelectEmbracingConstructProvider.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/SelectEmbracingConsruct/FSharpSelectEmbracingConstructProvider.fs
@@ -19,10 +19,10 @@ open Microsoft.FSharp.Compiler.SourceCodeServices.AstTraversal
 
 type FSharpSelection(file, range, parentRanges: DocumentRange list) =
     inherit SelectedRangeBase<IFSharpFile>(file, range)
-    
+
     override x.Parent =
         match parentRanges with
-        | parent :: rest -> FSharpSelection(file, parent, rest) :> ISelectedRange
+        | parent :: rest -> FSharpSelection(file, parent, rest) :> _
         | _ -> null
 
 
@@ -30,7 +30,7 @@ type FSharpDotSelection(file, document, range: TreeTextRange, parentRanges: Docu
     inherit DotSelection<IFSharpFile>(file, range.StartOffset, range.Length = 0, false)
 
     override x.CreateTreeNodeSelection(tokenNode) =
-        FSharpSelection(file, tokenNode.GetDocumentRange(), parentRanges) :> ISelectedRange
+        FSharpSelection(file, tokenNode.GetDocumentRange(), parentRanges) :> _
 
     override x.IsWordToken(token) =
         let tokenType = token.GetTokenType()
@@ -42,11 +42,20 @@ type FSharpDotSelection(file, document, range: TreeTextRange, parentRanges: Docu
 
     override x.IsSpaceToken(token) = token.GetTokenType().IsWhitespace
     override x.IsNewLineToken(token) = token.GetTokenType() = FSharpTokenType.NEW_LINE
-    override x.GetParentInternal(tokenNode) = null
+    override x.GetParentInternal(token) =
+        match token.GetTokenType() with
+        | :? FSharpTokenType.FSharpTokenNodeType as t when t.IsStringLiteral ->
+            let tokRange = token.GetDocumentRange()
+            let ranges = tokRange :: parentRanges
+            match token.GetText().[0] with
+            | ''' | '"' -> FSharpSelection(file, tokRange.TrimLeft(1).TrimRight(1), ranges) :> _
+            | '@' -> FSharpSelection(file, tokRange.TrimLeft(2).TrimRight(1), tokRange.TrimLeft(1) :: ranges) :> _
+            | _ -> null
+        | _ -> null
     override x.CreateTokenPartSelection(tokenNode, treeTextRange) = null
 
 [<AbstractClass>]
-type FSharpSelectEmbracingConstructProviderBase(settingsStore: ISettingsStore) =
+type FSharpSelectEmbracingConstructProviderBase() =
     let getRanges = function
         | TraverseStep.Expr(expr) -> [expr.Range]
         | TraverseStep.Module(moduleDecl) -> [moduleDecl.Range]
@@ -55,7 +64,7 @@ type FSharpSelectEmbracingConstructProviderBase(settingsStore: ISettingsStore) =
         | TraverseStep.MemberDefn(memberDefn) -> [memberDefn.Range]
         | TraverseStep.MatchClause(matchClause) -> [matchClause.Range; matchClause.RangeOfGuardAndRhs]
         | TraverseStep.Binding(binding) -> [binding.RangeOfHeadPat; binding.RangeOfBindingAndRhs; binding.RangeOfBindingSansRhs]
-        
+
     abstract IsAvailableImpl: IPsiSourceFile -> bool
 
     interface ISelectEmbracingConstructProvider with
@@ -65,26 +74,26 @@ type FSharpSelectEmbracingConstructProviderBase(settingsStore: ISettingsStore) =
             let fsFile = sourceFile.GetTheOnlyPsiFile() :?> IFSharpFile
             match isNotNull fsFile, fsFile.ParseResults with
             | true, Some parseResults when parseResults.ParseTree.IsSome ->
-                let document = documentRange.Document 
+                let document = documentRange.Document
                 let pos = document.GetPos(documentRange.StartOffset.Offset)
                 let visitor = { new AstVisitorBase<_>() with
                     // todo: cover more cases (inner expressions in bindings, match guards)
-                    member x.VisitExpr(path,_,defaultTraverse,expr) =
+                    member x.VisitExpr(path, _, defaultTraverse, expr) =
                         match expr with
                         | SynExpr.Ident _ | SynExpr.Const _ -> Some path
-                        | SynExpr.LongIdent _ -> Some (TraverseStep.Expr expr :: path) 
+                        | SynExpr.LongIdent _ -> Some (TraverseStep.Expr expr :: path)
                         | _ -> defaultTraverse expr }
 
                 let containingDeclarations =
                     match fsFile.FindTokenAt(documentRange.EndOffset) with
                     | null -> []
-                    | token -> 
+                    | token ->
                         let rec getParents = function
-                        | null -> []
-                        | (t: ITreeNode) -> t.GetDocumentRange() :: getParents t.Parent
+                            | null -> []
+                            | (t: ITreeNode) -> t.GetDocumentRange() :: getParents t.Parent
                         getParents token
 
-                let parentRanges =
+                let ranges =
                     match Traverse(pos, parseResults.ParseTree.Value, visitor) with
                     | Some traversePath ->
                         List.map getRanges traversePath
@@ -92,20 +101,22 @@ type FSharpSelectEmbracingConstructProviderBase(settingsStore: ISettingsStore) =
                         |> List.map (fun r -> r.ToDocumentRange(document))
                     | None -> []
                     |> List.append containingDeclarations
-                    |> List.filter (fun r -> r.Contains(documentRange.EndOffset))
+                    |> List.filter (fun r -> r.Contains(documentRange))
                     |> List.sortBy (fun r -> r.Length)
 
-                FSharpDotSelection(fsFile, document, fsFile.Translate(documentRange), parentRanges) :> ISelectedRange
+                FSharpDotSelection(fsFile, document, fsFile.Translate(documentRange), ranges) :> _
             | _ -> null
 
+
 [<ProjectFileType(typeof<FSharpProjectFileType>)>]
-type FSharpFileSelectEmbracingConstructProvider(settingsStore: ISettingsStore) =
-    inherit FSharpSelectEmbracingConstructProviderBase(settingsStore)
+type FSharpSelectEmbracingConstructProvider() =
+    inherit FSharpSelectEmbracingConstructProviderBase()
 
     override x.IsAvailableImpl(sourceFile) = true
 
+
 [<ProjectFileType(typeof<FSharpScriptProjectFileType>)>]
-type FSharpScriptSelectEmbracingConstructProvider(settingsStore: ISettingsStore) =
-    inherit FSharpSelectEmbracingConstructProviderBase(settingsStore)
+type FSharpScriptSelectEmbracingConstructProvider() =
+    inherit FSharpSelectEmbracingConstructProviderBase()
 
     override x.IsAvailableImpl(sourceFile) = true
