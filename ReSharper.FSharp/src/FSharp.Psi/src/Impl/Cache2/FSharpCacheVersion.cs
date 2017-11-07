83cb21f1212d6411ed835983bb08ec79c4a69248
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-11-07 14:10:02 +0300

    Update F# cache version

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/Cache2/FSharpCacheVersion.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/Cache2/FSharpCacheVersion.cs
@@ -4,7 +4,7 @@ using JetBrains.Util.PersistentMap;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Cache2
 {
-  [PolymorphicMarshaller(3)]
+  [PolymorphicMarshaller(4)]
   public class FSharpCacheVersion
   {
     [UsedImplicitly] public static UnsafeReader.ReadDelegate<object> ReadDelegate = r => new FSharpCacheVersion();
