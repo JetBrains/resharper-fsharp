b35d579d05777c982807c4123fb49c76e6174825
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2017-11-25 10:09:10 +0300

    Better waiting for Fcs in frontend test

--- rider-fsharp/src/test/kotlin/FSharpCompletionTest.kt
+++ rider-fsharp/src/test/kotlin/FSharpCompletionTest.kt
@@ -2,7 +2,10 @@ import com.intellij.openapi.editor.impl.EditorImpl
 import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.test.CompletionTestBase
 import com.jetbrains.rider.test.framework.frameworkLogger
-import com.jetbrains.rider.test.scriptingApi.*
+import com.jetbrains.rider.test.scriptingApi.callBasicCompletion
+import com.jetbrains.rider.test.scriptingApi.completeWithTab
+import com.jetbrains.rider.test.scriptingApi.typeWithLatency
+import com.jetbrains.rider.test.scriptingApi.waitForCompletion
 import com.jetbrains.rider.util.idea.waitAndPump
 import com.jetbrains.rider.util.lifetime.Lifetime
 import org.testng.annotations.Test
@@ -13,10 +16,12 @@ class FSharpCompletionTest : CompletionTestBase() {
     override val restoreNuGetPackages = true
 
     private val rdFcsHost get() = project.solution.fsharpCompilerServiceHost
+    private var isFcsReady = false
 
     @Test
     fun basicCompletion() {
         doTest {
+            isFcsReady = false
             typeWithLatency("filt")
             waitForFcs()
             callBasicCompletion()
@@ -26,20 +31,18 @@ class FSharpCompletionTest : CompletionTestBase() {
     }
 
     private fun waitForFcs() {
-        val lifetimeDefinition = Lifetime.create(Lifetime.Eternal)
-
-        var isFcsReady = false
-        rdFcsHost.projectChecked.advise(lifetimeDefinition.lifetime, { project ->
-            isFcsReady = true
-            frameworkLogger.info("FCS: $project checked")
-        })
-
         waitAndPump(Lifetime.Eternal, { isFcsReady }, 60000)
-        lifetimeDefinition.terminate()
     }
 
     private fun doTest(test: EditorImpl.() -> Unit) {
         val fileName = "Program.fs"
+
+        rdFcsHost.projectChecked.advise(Lifetime.Eternal, { project ->
+            isFcsReady = true
+            frameworkLogger.info("FCS: $project checked")
+        })
+
+        isFcsReady = false
 
         doTestWithDocuments {
             withCaret(fileName, fileName) {
