656ee48cbe5cd638b52141220c17f23009c8d515
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-03-07 15:33:09 +0300

    Revert rescheduling completion when starting new identifiers

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/services/completion/FSharpCompletionStrategy.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/services/completion/FSharpCompletionStrategy.kt
@@ -9,5 +9,5 @@ import com.jetbrains.rider.completion.ICompletionSessionStrategy
 class FSharpCompletionStrategy : ICompletionSessionStrategy {
     override fun shouldForbidCompletion(editor: Editor, type: CompletionType) = editor.selectionModel.hasSelection()
     override fun shouldRescheduleCompletion(prefix: String, psiFile: PsiFile, char: Char, offset: Int) =
-            CompletionSessionHeuristics.shouldRescheduleDefaultStrategy(char)
+            prefix.isEmpty()
 }
