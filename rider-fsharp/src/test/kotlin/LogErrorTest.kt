3bcb91c3e43fc1b6a96912f3ffa3a745bd6e136d
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-05-08 13:12:27 +0300

    Update for Rider APIs changes
    ReSharperHostException isn't wrapped into LogErrorMessageException anymore

--- rider-fsharp/src/test/kotlin/LogErrorTest.kt
+++ rider-fsharp/src/test/kotlin/LogErrorTest.kt
@@ -1,13 +1,11 @@
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.testFramework.LoggedErrorProcessor
-import com.jetbrains.rider.test.LogErrorMessageException
 import com.jetbrains.rider.test.TestCaseRunner
 import com.jetbrains.rider.util.idea.ReSharperHostException
 import org.testng.annotations.Test
 
 class LogErrorTest : TestCaseRunner() {
 
-    @Test(expectedExceptions = [(LogErrorMessageException::class)])
+    @Test(expectedExceptions = [(ReSharperHostException::class)])
     fun testThrowEx() {
         Logger.getInstance("Projected Logger").error(ReSharperHostException("a", "b"))
         testCaseErrorsProcessor.throwIfNotEmpty()
