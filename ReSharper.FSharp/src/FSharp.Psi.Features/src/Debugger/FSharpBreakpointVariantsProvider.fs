8a134351125725741ee6cbe952ae535d4081b01c
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-05-28 19:36:08 +0300

    Add more supported extensions to breakpoint variants provider
    * Less allocations

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Debugger/FSharpBreakpointVariantsProvider.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Debugger/FSharpBreakpointVariantsProvider.fs
@@ -5,6 +5,7 @@ open System.Collections.Generic
 open JetBrains.DocumentModel
 open JetBrains.ReSharper.Feature.Services.Debugger
 open JetBrains.ReSharper.Plugins.FSharp.Common.Util
+open JetBrains.ReSharper.Plugins.FSharp.ProjectModelBase
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
@@ -19,7 +20,11 @@ type FSharpBreakpointVariantsProvider() =
     let [<Literal>] multilineSuffix = " ..."
 
     interface IBreakpointVariantsProvider with
-        member x.GetSupportedFileExtensions() = List([|"fs"|])
+        member x.GetSupportedFileExtensions() =
+            List([| FSharpProjectFileType.FsExtension
+                    FSharpProjectFileType.MlExtension
+                    FSharpScriptProjectFileType.FsxExtension
+                    FSharpScriptProjectFileType.FsScriptExtension |])
 
         member x.GetBreakpointVariants(file, line, solution) =
             match file.GetPrimaryPsiFile() with
@@ -30,18 +35,20 @@ type FSharpBreakpointVariantsProvider() =
                     let docLine = docLine line
                     let lineStart = document.GetLineStartOffset(docLine)
                     let lineEnd = document.GetLineEndOffsetWithLineBreak(docLine)
-                    let variants: BreakpointVariantModelBase seq = 
-                        fsFile.FindTokensAt(TreeTextRange(TreeOffset(lineStart), TreeOffset(lineEnd)))
-                        |> Seq.choose (fun token ->
-                            let pos = document.GetPos(token.GetTreeEndOffset().Offset)
-                            match parseResults.ValidateBreakpointLocation(pos) with
-                            | Some range when range.StartLine - 1 = line ->
-                                let startOffset = document.GetTreeStartOffset(range).Offset
-                                let endOffset = document.GetTreeEndOffset(range).Offset
-                                let breakpointText = document.GetText(TextRange(startOffset, Math.Min(lineEnd, endOffset)))
-                                let text = if endOffset > lineEnd then breakpointText + multilineSuffix else breakpointText
-                                Some (BreakpointVariantModel(startOffset, endOffset, text) :> _)
-                            | _ -> None)
-                    JetHashSet(variants).AsList()
+
+                    let variants = JetHashSet<BreakpointVariantModelBase>() 
+                    for token in fsFile.FindTokensAt(TreeTextRange(TreeOffset(lineStart), TreeOffset(lineEnd))) do
+                        let pos = document.GetPos(token.GetTreeEndOffset().Offset)
+                        match parseResults.ValidateBreakpointLocation(pos) with
+                        | Some range when range.StartLine - 1 = line ->
+                            let startOffset = document.GetTreeStartOffset(range).Offset
+                            let endOffset = document.GetTreeEndOffset(range).Offset
+                            let breakpointText = document.GetText(TextRange(startOffset, Math.Min(lineEnd, endOffset)))
+                            let text = if endOffset > lineEnd then breakpointText + multilineSuffix else breakpointText
+                            let breakpoinVariant = BreakpointVariantModel(startOffset, endOffset, text)
+                            variants.Add(breakpoinVariant) |> ignore
+                        | _ -> ()
+
+                    variants.AsList()
                 | _ -> null
             | _ -> null
