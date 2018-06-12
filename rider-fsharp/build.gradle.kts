cd272c231e5552c662160fb3905642a63d0020f1
Dmitry Jemerov <intelliyole@gmail.com>
2018-06-12 11:20:39 +0200

    Use rdgen Gradle plugin to generate models

--- rider-fsharp/build.gradle.kts
+++ rider-fsharp/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.jetbrains.rider.generator.gradle.RdgenParams
+import com.jetbrains.rider.generator.nova.GenerationSpec
+import groovy.lang.Closure
 import org.apache.tools.ant.taskdefs.condition.Os
 import org.gradle.internal.jvm.Jvm
 import org.jetbrains.intellij.tasks.PublishTask
@@ -7,9 +10,23 @@ import org.jetbrains.intellij.IntelliJPlugin
 import org.jetbrains.intellij.tasks.PrepareSandboxTask
 import org.jetbrains.kotlin.daemon.common.toHexString
 
+buildscript {
+    repositories {
+        maven { setUrl("https://repo.labs.intellij.net/rd-snapshots-maven") }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.jetbrains.rd:rd-gen:0.1.19")
+    }
+}
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.2.41"
     id("org.jetbrains.intellij") version "0.3.2"
+}
+
+apply {
+    plugin("com.jetbrains.rdgen")
 }
 
 repositories {
@@ -32,7 +49,7 @@ intellij {
     // This will download from www.jetbrains.com/intellij-repository/snapshots or
     // www.jetbrains.com/intellij-repository/releases, respectively.
     // Note that there's no guarantee that these are kept up to date
-    // version = 'LATEST-TRUNK-SNAPSHOT'
+     version = "LATEST-EAP-SNAPSHOT"
     // If the build isn't available in intellij-repository, use an installed version via `localPath`
     // localPath = '/Users/matt/Library/Application Support/JetBrains/Toolbox/apps/Rider/ch-1/171.4089.265/Rider EAP.app/Contents'
     // localPath = "C:\\Users\\Ivan.Shakhov\\AppData\\Local\\JetBrains\\Toolbox\\apps\\Rider\\ch-0\\171.4456.459"
@@ -112,6 +129,40 @@ fun File.writeTextIfChanged(content: String) {
     }
 }
 
+configure<RdgenParams> {
+    val csOutput = File(repoRoot, "Resharper.FSharp/src/FSharp.ProjectModelBase/src/Protocol")
+    val ktOutput = File(repoRoot, "rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/protocol")
+
+    verbose.set(true)
+    hashFolder.set("build/rdgen")
+    logger.info("Configuring rdgen params")
+    classpath({
+        logger.info("Calculating classpath for rdgen, intellij.ideaDependency is ${intellij.ideaDependency}")
+        val sdkPath = intellij.ideaDependency.classes
+        val rdLibDirectory = File(sdkPath, "lib/rd").canonicalFile
+
+        "$rdLibDirectory/rider-model.jar"
+    })
+    sources(File(repoRoot, "rider-fsharp/protocol/src/kotlin/model"))
+    packages.set("model")
+
+    generator {
+        language = "kotlin"
+        transform = "asis"
+        root = "com.jetbrains.rider.model.nova.ide.IdeRoot"
+        namespace = "com.jetbrains.rider.model"
+        directory = "$ktOutput"
+    }
+
+    generator {
+        language = "csharp"
+        transform = "reversed"
+        root = "com.jetbrains.rider.model.nova.ide.IdeRoot"
+        namespace = "JetBrains.Rider.Model"
+        directory = "$csOutput"
+    }
+}
+
 tasks {
     withType<PrepareSandboxTask> {
         var files = libFiles + pluginFiles.map { "$it.dll" } + pluginFiles.map { "$it.pdb" }
@@ -142,7 +193,7 @@ tasks {
 
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
-        dependsOn("generateModel")
+        dependsOn("rdgen")
     }
 
     withType<Test> {
@@ -183,60 +234,11 @@ tasks {
 
     "prepare" {
         group = riderFSharpTargetsGroup
-        dependsOn("generateModel", "writeNuGetConfig", "writeRiderSdkVersionProps")
+        dependsOn("rdgen", "writeNuGetConfig", "writeRiderSdkVersionProps")
         doLast {
             exec {
                 executable = "dotnet"
                 args = listOf("restore", "$resharperPluginPath/ReSharper.FSharp.sln")
-            }
-        }
-    }
-
-    "generateModel" {
-        group = riderFSharpTargetsGroup
-        doLast {
-            val sdkPath = intellij.ideaDependency.classes
-            val rdLibDirectory = File(sdkPath, "lib/rd").canonicalFile
-            ext.properties["rdLibDirectory"] = rdLibDirectory
-            val rdgenJar = File(rdLibDirectory.absolutePath, "rd-gen.jar")
-
-            assert(rdLibDirectory.isDirectory)
-            assert(rdgenJar.isFile)
-
-            // If we specify these as outputs of the task, gradle will create them for us, but we then
-            // need to also specify inputs. RdGen does up to date checks for us, but we could skip that
-            // altogether if we hard code them in the task. Downside is that we'd need to keep the list
-            // of inputs/outputs up to date manually, instead of leaving it to rdgen
-            mkdir("build/rdgen")
-
-            val modelDir = File(repoRoot, "rider-fsharp/protocol/src/kotlin/model")
-            val packageName = "model"
-
-            val cpSeparator = if (Os.isFamily(Os.FAMILY_WINDOWS)) ";" else ":"
-            val compilerClasspath = "$rdLibDirectory/rd-gen.jar$cpSeparator$rdLibDirectory/rider-model.jar"
-            val rdGenClasspath = files(compilerClasspath)
-
-            // Protocol between backend and frontend
-            // Direction isn't important for this model, so we don't specify it in the model. Which means
-            // we have to specify output directories here
-            // Inputs: rider/protocol/src/main/kotlin/model/rider/RdUnityModel.kt
-            // Outputs: resharper/src/resharper-unity/Rider/RdUnityProtocol/RdUnityModel.Generated.cs
-            //          rider/src/main/kotlin/com/jetbrains/rider/protocol/RdUnityProtocol/RdUnityModel.Generated.kt
-            val csOutput = File(repoRoot, "Resharper.FSharp/src/FSharp.ProjectModelBase/src/Protocol")
-            val ktOutput = File(repoRoot, "rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/protocol")
-            javaexec {
-                main = "com.jetbrains.rider.generator.nova.MainKt"
-                classpath = rdGenClasspath
-                systemProperty("model.out.src.cs.dir", "$csOutput")
-                systemProperty("model.out.src.kt.dir", "$ktOutput")
-                errorOutput = System.out // kotlin throws warnings in stderr which causes appveyor build to fail
-                args = listOf(
-                        "--verbose",
-                        "--hash-folder=build/rdgen",
-                        "--compiler-classpath=$compilerClasspath",
-                        "--source=$modelDir",
-                        "--packages=$packageName"
-                )
             }
         }
     }
