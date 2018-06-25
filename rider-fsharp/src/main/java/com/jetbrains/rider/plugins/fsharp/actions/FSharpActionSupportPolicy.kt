be1f42eacc8cbee03c5a9863ea82a2c5502c45bd
Kirill.Skrygan <kirill.skrygan@jetbrains.com>
2018-06-25 12:10:22 +0200

    sync api

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/actions/FSharpActionSupportPolicy.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/actions/FSharpActionSupportPolicy.kt
@@ -1,13 +1,14 @@
 package com.jetbrains.rider.plugins.fsharp.actions
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.jetbrains.rider.actions.RiderActionCallStrategy
 import com.jetbrains.rider.actions.RiderActionSupportPolicy
 
 class FSharpActionSupportPolicy : RiderActionSupportPolicy() {
-    override fun getCallStrategy(psiFile: PsiFile, offset: Int?, reSharperId: String): RiderActionCallStrategy {
+    override fun getCallStrategy(psiElement: PsiElement, offset: Int?, reSharperId: String): RiderActionCallStrategy {
         if (reSharperId == "TextControl.Paste")
             return RiderActionCallStrategy.FRONTEND_ONLY
-        return super.getCallStrategy(psiFile, offset, reSharperId)
+        return super.getCallStrategy(psiElement, offset, reSharperId)
     }
 }
