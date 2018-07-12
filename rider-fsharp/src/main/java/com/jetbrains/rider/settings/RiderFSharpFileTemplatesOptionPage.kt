15b711e10797bf7b0735e2b8afae30009a843865
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-07-12 13:31:40 +0300

    Add F# file templates options page

---
+++ rider-fsharp/src/main/java/com/jetbrains/rider/settings/RiderFSharpFileTemplatesOptionPage.kt
@@ -0,0 +1,5 @@
+package com.jetbrains.rider.settings
+
+import com.jetbrains.rider.settings.simple.SimpleOptionsPage
+
+class RiderFSharpFileTemplatesOptionPage: SimpleOptionsPage("F#", "RiderFSharpFileTemplatesSettings")
