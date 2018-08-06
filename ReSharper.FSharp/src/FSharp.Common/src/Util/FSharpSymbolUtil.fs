3d91e8fc42565d1d4c92cf594996703e68a45caf
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-08-06 13:51:58 +0300

    Less allocations and checks in getting lookup items icons

--- ReSharper.FSharp/src/FSharp.Common/src/Util/FSharpSymbolUtil.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Util/FSharpSymbolUtil.fs
@@ -5,7 +5,6 @@ open JetBrains.Application.UI.Icons.ComposedIcons
 open JetBrains.ReSharper.Psi.Resources
 open JetBrains.UI.Icons
 open Microsoft.FSharp.Compiler.SourceCodeServices
-open Microsoft.FSharp.Compiler.SourceCodeServices.Extensions
 
 [<CompiledName("GetReturnType")>]
 let getReturnType (symbol: FSharpSymbol) =
@@ -16,96 +15,103 @@ let getReturnType (symbol: FSharpSymbol) =
     | :? FSharpUnionCase as case -> Some case.ReturnType
     | _ -> None
 
-[<CompiledName("TryGetFullCompiledName")>]    
-let tryGetFullCompiledName (entity: FSharpEntity) =
-    entity.TryGetFullCompiledName()
+let compose a b = CompositeIconId.Compose(a, b)
 
 let addAccessibility (iconId: IconId) (accessibility: FSharpAccessibility) =
-    let accessibilityIcon =
-        if accessibility.IsInternal then PsiSymbolsThemedIcons.ModifiersInternal.Id else
-        if accessibility.IsPrivate then PsiSymbolsThemedIcons.ModifiersPrivate.Id else
-        null
-    if isNull accessibilityIcon then iconId else CompositeIconId.Compose(iconId, accessibilityIcon)
+    if accessibility.IsPublic then iconId else
+    if accessibility.IsInternal then compose iconId PsiSymbolsThemedIcons.ModifiersInternal.Id else
+    if accessibility.IsPrivate then compose iconId PsiSymbolsThemedIcons.ModifiersPrivate.Id else
+    compose iconId PsiSymbolsThemedIcons.ModifiersProtected.Id
 
-// todo: provide an option to use F# style icons (e.g. functions, values (with no static modifiers), modules)
-let rec getIconId = fun (symbol: FSharpSymbol) ->
+let staticProperty = compose PsiSymbolsThemedIcons.Property.Id PsiSymbolsThemedIcons.ModifiersStatic.Id
+let staticMethod   = compose PsiSymbolsThemedIcons.Method.Id   PsiSymbolsThemedIcons.ModifiersStatic.Id
+let staticClass    = compose PsiSymbolsThemedIcons.Class.Id    PsiSymbolsThemedIcons.ModifiersStatic.Id
+
+let rec getIconId (symbol: FSharpSymbol) =
     match symbol with
     | :? FSharpEntity as entity ->
+        if entity.IsNamespace then PsiSymbolsThemedIcons.Namespace.Id else
+
         let baseIcon =
             if entity.IsClass || entity.IsFSharpRecord
             then PsiSymbolsThemedIcons.Class.Id else
 
-            if entity.IsFSharpModule
-            then CompositeIconId.Compose(PsiSymbolsThemedIcons.Class.Id, PsiSymbolsThemedIcons.ModifiersStatic.Id) else
+            if entity.IsFSharpModule then staticClass else
 
             if entity.IsInterface then PsiSymbolsThemedIcons.Interface.Id else
             if entity.IsValueType then PsiSymbolsThemedIcons.Struct.Id else
-            if entity.IsNamespace then PsiSymbolsThemedIcons.Namespace.Id else
+            if entity.IsFSharpUnion || entity.IsEnum then PsiSymbolsThemedIcons.Enum.Id else
             if entity.IsDelegate then PsiSymbolsThemedIcons.Delegate.Id else
-            if entity.IsEnum || entity.IsFSharpUnion then PsiSymbolsThemedIcons.Enum.Id else
 
-            let mutable abbrEntity = if entity.IsFSharpAbbreviation then Some entity else None
-            let hasAbbrType (entity: FSharpEntity option) =
-                entity.IsSome &&
-                entity.Value.IsFSharpAbbreviation &&
-                entity.Value.AbbreviatedType.HasTypeDefinition
+            let isTypeAbbr (entity: FSharpEntity) =
+                entity.IsFSharpAbbreviation && entity.AbbreviatedType.HasTypeDefinition
 
-            seq { while hasAbbrType abbrEntity do
-                    abbrEntity <- Some abbrEntity.Value.AbbreviatedType.TypeDefinition
-                    yield abbrEntity.Value }
-            |> Seq.tryLast
-            |> Option.map getIconId
-            |> Option.defaultValue PsiSymbolsThemedIcons.Class.Id
+            if not (isTypeAbbr entity) then PsiSymbolsThemedIcons.Class.Id else
+
+            let mutable abbrEntity = entity
+            while isTypeAbbr abbrEntity do
+                abbrEntity <- abbrEntity.AbbreviatedType.TypeDefinition
+
+            getIconId abbrEntity
 
         addAccessibility baseIcon entity.Accessibility
 
     | :? FSharpMemberOrFunctionOrValue as mfv ->
+        let accessibility = mfv.Accessibility
+        let isModuleValueOrMember = mfv.IsModuleValueOrMember
+        let isMember = mfv.IsMember
+        let isProperty = mfv.IsProperty
+        let isCtor = mfv.IsConstructor
+
+        if isModuleValueOrMember && not isMember && accessibility.IsPublic then
+            if mfv.IsValCompiledAsMethod then staticMethod else staticProperty
+        else
+
         let baseIcon =
-            if not mfv.IsModuleValueOrMember then PsiSymbolsThemedIcons.Variable.Id else
+            if not isModuleValueOrMember then PsiSymbolsThemedIcons.Variable.Id else
 
             if mfv.IsEvent then PsiSymbolsThemedIcons.Event.Id else
             if mfv.IsExtensionMember then PsiSymbolsThemedIcons.ExtensionMethod.Id else
 
-            if mfv.IsMember then
-                if mfv.IsProperty then PsiSymbolsThemedIcons.Property.Id else
-                if mfv.IsConstructor then PsiSymbolsThemedIcons.Constructor.Id else
-                if mfv.IsExtensionMember then
-                    PsiSymbolsThemedIcons.ExtensionMethod.Id else
+            if isMember then
+                if isProperty then PsiSymbolsThemedIcons.Property.Id else
+                if isCtor then PsiSymbolsThemedIcons.Constructor.Id else
                 PsiSymbolsThemedIcons.Method.Id
 
-            else if mfv.IsValCompiledAsMethod then PsiSymbolsThemedIcons.Method.Id else
-            PsiSymbolsThemedIcons.Variable.Id
+            else PsiSymbolsThemedIcons.Variable.Id
 
         let iconId =
             let readWriteModifier =
-                if mfv.IsMutable || mfv.HasSetterMethod || mfv.IsRefCell then
-                    if mfv.HasGetterMethod then PsiSymbolsThemedIcons.ModifiersReadWrite.Id
-                    else PsiSymbolsThemedIcons.ModifiersWrite.Id
-                else if mfv.HasGetterMethod then PsiSymbolsThemedIcons.ModifiersRead.Id
+                if isProperty then
+                    if mfv.HasSetterMethod then
+                        if mfv.HasGetterMethod then PsiSymbolsThemedIcons.ModifiersReadWrite.Id else
+                        PsiSymbolsThemedIcons.ModifiersWrite.Id
+                    else
+                        if mfv.HasGetterMethod then PsiSymbolsThemedIcons.ModifiersRead.Id else null
+
+                else if mfv.IsMutable || mfv.IsRefCell then PsiSymbolsThemedIcons.ModifiersWrite.Id
                 else null
 
             if isNull readWriteModifier then baseIcon
-            else CompositeIconId.Compose(baseIcon, readWriteModifier)
+            else compose baseIcon readWriteModifier
 
         let iconId =
-            if not mfv.IsModuleValueOrMember || mfv.IsInstanceMember || mfv.IsInstanceMemberInCompiledCode || mfv.IsConstructor then iconId
+            if not isModuleValueOrMember || isCtor || mfv.IsInstanceMember then iconId
             else CompositeIconId.Compose(iconId, PsiSymbolsThemedIcons.ModifiersStatic.Id)
 
-        addAccessibility iconId mfv.Accessibility
+        addAccessibility iconId accessibility
 
     | :? FSharpField as field ->
-        let iconId =
-            if field.DeclaringEntity.IsEnum then PsiSymbolsThemedIcons.EnumMember.Id else
+        if field.DeclaringEntity.IsEnum then PsiSymbolsThemedIcons.EnumMember.Id else
 
+        let iconId =
             let iconId = PsiSymbolsThemedIcons.Field.Id
-            if field.IsStatic then CompositeIconId.Compose(iconId, PsiSymbolsThemedIcons.ModifiersStatic.Id) else iconId
+            if field.IsStatic then compose iconId PsiSymbolsThemedIcons.ModifiersStatic.Id else iconId
         addAccessibility iconId field.Accessibility
 
-    | :? FSharpUnionCase as unionCase ->
-        let iconId =  addAccessibility PsiSymbolsThemedIcons.EnumMember.Id unionCase.Accessibility
-        CompositeIconId.Compose(iconId, PsiSymbolsThemedIcons.ModifiersStatic.Id)
+    | :? FSharpUnionCase -> PsiSymbolsThemedIcons.EnumMember.Id
     | :? FSharpParameter -> PsiSymbolsThemedIcons.Parameter.Id
+    | :? FSharpActivePatternCase -> PsiSymbolsThemedIcons.Method.Id
     | :? FSharpGenericParameter
     | :? FSharpStaticParameter -> PsiSymbolsThemedIcons.Typeparameter.Id
-    | :? FSharpActivePatternCase -> PsiSymbolsThemedIcons.Method.Id
     | _ -> null
