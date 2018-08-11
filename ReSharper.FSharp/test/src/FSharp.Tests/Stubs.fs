cd505295b776b90867e15321b788eb1bbce618fa
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-08-12 01:23:35 +0300

    Hide FsiDetector in tests

--- ReSharper.FSharp/test/src/FSharp.Tests/Stubs.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Stubs.fs
@@ -1,25 +1,23 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Common
 
 open JetBrains.Application
-open JetBrains.Application.BuildScript.Application.Zones
 open JetBrains.Application.Components
 open JetBrains.ProjectModel
-open JetBrains.Platform
 open JetBrains.ReSharper.Plugins.FSharp.Common
 open JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModel
-open JetBrains.ReSharper.Plugins.FSharp.Psi.LanguageService.Parsing
 open JetBrains.ReSharper.Plugins.FSharp.Services.ContextActions
-open Microsoft.FSharp.Compiler.SourceCodeServices
-open NUnit.Framework
+open JetBrains.Util
 
 [<SolutionComponent>]
 type FsiSessionsHostStub() = 
     interface IHideImplementation<FsiSessionsHost>
 
+
 [<SolutionComponent>]
 type FSharpProjectOptionsBuilderStub() = 
     interface IHideImplementation<FSharpProjectOptionsBuilder>
+
 
 [<ShellComponent>]
 type FSharpFileServiceStub() =
@@ -28,6 +26,15 @@ type FSharpFileServiceStub() =
     interface IFSharpFileService with
         member x.IsScratchFile(_) = false
         member x.IsScriptLike(_) = false
+
+
+[<SolutionComponent>]
+type FsiDetectorStub() =
+    interface IHideImplementation<FsiDetector>
+
+    interface IFsiDetector with
+        member x.GetSystemFsiDirectoryPath() = FileSystemPath.Empty
+
 
 /// Used to add assemblies to R# subplatfrom at runtime
 type AddAssembliesToSubplatform() =
