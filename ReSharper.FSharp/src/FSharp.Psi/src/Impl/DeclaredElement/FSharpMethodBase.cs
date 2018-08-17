4db24777d538794445b4a023db1fbbf64fc4d2cb
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-08-17 15:08:57 +0300

    Fix methods id substitutions

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/DeclaredElement/FSharpMethodBase.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/DeclaredElement/FSharpMethodBase.cs
@@ -3,6 +3,8 @@ using JetBrains.ReSharper.Plugins.FSharp.Common.Util;
 using JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree;
 using JetBrains.ReSharper.Plugins.FSharp.Psi.Tree;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.DeclaredElements;
+using JetBrains.ReSharper.Psi.Resolve;
 using JetBrains.ReSharper.Psi.Tree;
 using Microsoft.FSharp.Compiler.SourceCodeServices;
 
@@ -30,5 +32,7 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.DeclaredElement
     public bool IsAsync => false;
     public bool IsVarArg => false;
     public bool IsXamlImplicitMethod => false;
+
+    public override ISubstitution IdSubstitution => MethodIdSubstitution.Create(this);
   }
 }
