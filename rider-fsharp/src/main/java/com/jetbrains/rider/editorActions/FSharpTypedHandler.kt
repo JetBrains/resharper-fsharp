677b26afee37324351e8ed46b1644354b438e0ba
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-10-18 13:07:21 +0300

    Fix for API change (FrontendTypedHandler)

--- rider-fsharp/src/main/java/com/jetbrains/rider/editorActions/FSharpTypedHandler.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/editorActions/FSharpTypedHandler.kt
@@ -5,5 +5,4 @@ import com.jetbrains.rdclient.editorActions.FrontendTypedHandler
 
 class FSharpTypedHandler : FrontendTypedHandler() {
     override fun isWritableTypingAssist(editor: Editor, typedChar: Char) = false
-    override fun executeTypedActionOnBackend(actionId: String): Boolean = false
 }
