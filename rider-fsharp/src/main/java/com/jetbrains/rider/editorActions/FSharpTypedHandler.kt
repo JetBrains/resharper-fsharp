bb8fbfdbb4c437dfb313f6a84649f44dc30e952a
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-10-18 18:55:58 +0300

    Remove old typing/paste actions overrides

--- rider-fsharp/src/main/java/com/jetbrains/rider/editorActions/FSharpTypedHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.rider.editorActions
-
-import com.intellij.openapi.editor.Editor
-import com.jetbrains.rdclient.editorActions.FrontendTypedHandler
-
-class FSharpTypedHandler : FrontendTypedHandler() {
-    override fun isWritableTypingAssist(editor: Editor, typedChar: Char) = false
-}
