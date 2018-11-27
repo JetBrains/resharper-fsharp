7f59ff22a3d17f75511171478dc4d50acb2fcc6e
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-11-27 13:17:33 +0300

    Migrate Fsi options page to Be controls

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Fsi/FsiOptionsPage.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Fsi/FsiOptionsPage.fs
@@ -3,8 +3,11 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Fsi
 open System.Linq.Expressions
 open System.Runtime.InteropServices
 open JetBrains.Application
+open JetBrains.Application.Settings
 open JetBrains.Application.UI.Options
-open JetBrains.Application.UI.Options.OptionsDialog
+open JetBrains.DataFlow
+open JetBrains.IDE.UI.Extensions
+open JetBrains.IDE.UI.Options
 open JetBrains.ProjectModel.Resources
 open JetBrains.ReSharper.Host.Features.Settings.Layers.ExportImportWorkaround
 open JetBrains.ReSharper.Plugins.FSharp.Services.Settings.Fsi
@@ -13,43 +16,46 @@ open JetBrains.Util
 open System
 
 [<OptionsPage("FsiOptionsPage", "Fsi", typeof<ProjectModelThemedIcons.Fsharp>, HelpKeyword = fsiHelpKeyword)>]
-type FsiOptionsPage(lifetime, optionsContext) as this =
-    inherit SimpleOptionsPage(lifetime, optionsContext)
+type FsiOptionsPage(lifetime: Lifetime, optionsContext) as this =
+    inherit BeSimpleOptionsPage(lifetime, optionsContext)
 
     do
         this.AddHeader(launchOptionsSectionTitle)
         this.AddBool(useAnyCpuVersionText,     fun key -> key.UseAnyCpuVersion)
         this.AddBool(shadowCopyReferencesText, fun key -> key.ShadowCopyReferences)
         this.AddDescription(shadowCopyReferencesDescription)
-        this.AddEmptyLine() |> ignore
 
-        this.AddString(fsiArgsText,            fun key -> key.FsiArgs)
-        this.AddEmptyLine() |> ignore
+        this.AddString(fsiArgsText, fun key -> key.FsiArgs)
+
+        this.AddHeader(commandsSectionTitle)
+        this.AddBool(moveCaretOnSendLineText, fun key -> key.MoveCaretOnSendLine)
+        this.AddBool(executeRecentsText,      fun key -> key.ExecuteRecents)
+        this.AddDescription(executeRecentsDescription)
 
         if PlatformUtil.IsRunningUnderWindows then
             this.AddHeader(debugSectionTitle)
-            this.AddBool(fixOptionsForDebugText,   fun key -> key.FixOptionsForDebug)
+            this.AddBool(fixOptionsForDebugText, fun key -> key.FixOptionsForDebug)
             this.AddDescription(fixOptionsForDebugDescription)
-
-        this.AddHeader(commandsSectionTitle)
-        this.AddBool(moveCaretOnSendLineText,  fun key -> key.MoveCaretOnSendLine)
-        this.AddBool(executeRecentsText,       fun key -> key.ExecuteRecents)
-        this.AddDescription(executeRecentsDescription)
-
-        this.FinishPage()
-
+    
     member x.AddBool(text, getter: Expression<Func<FsiOptions,_>>) =
         this.AddBoolOption(getter, RichText(text)) |> ignore
 
-    member x.AddString(text, getter: Expression<Func<FsiOptions,_>>) =
-        this.AddStringOption(getter, text) |> ignore
-
     member x.AddHeader(text: string) =
-        this.AddHeader(text, null) |> ignore
+        base.AddHeader(text) |> ignore
 
     member x.AddDescription(text) =
-        let option = this.AddText(text)
-        this.SetIndent(option, 1)
+        use indent = x.Indent()
+        this.AddRichText(RichText(text)) |> ignore
+
+    member x.AddString(text: string, getter: Expression<Func<FsiOptions,_>>) =
+        let property =
+            let tyString = x.GetType().ToString()
+            new Property<string>(lifetime, tyString + "_StringOptionViewModel_" + text + "_checkedProperty")
+
+        let settingsEntry = x.OptionsSettingsSmartContext.Schema.GetScalarEntry(getter)
+        x.OptionsSettingsSmartContext.SetBinding(lifetime, settingsEntry, property)
+        x.AddKeyword(text)
+        x.AddControl(property.GetBeTextBox(lifetime).WithDescription(text, lifetime))
 
 
 [<ShellComponent>]
