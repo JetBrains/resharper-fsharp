39c0fea481503ab35e1c18ece40774d22555a084
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-12-04 21:12:50 +0300

    Fix stofl in invalidating project options

--- ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpProjectOptionsProvider.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpProjectOptionsProvider.fs
@@ -2,40 +2,21 @@ namespace rec JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 
 open System
 open System.Collections.Generic
-open System.Linq
-open System.Threading
-open System.Reflection
-open JetBrains
-open JetBrains.Annotations
-open JetBrains.Application
 open JetBrains.Application.changes
-open JetBrains.Application.Components
-open JetBrains.Application.Progress
-open JetBrains.Application.Threading
-open JetBrains.Application.Threading.Tasks
 open JetBrains.DataFlow
-open JetBrains.Metadata.Reader.API
-open JetBrains.Platform.MsBuildHost.Models
 open JetBrains.ProjectModel
 open JetBrains.ProjectModel.Assemblies.Impl
 open JetBrains.ProjectModel.ProjectsHost
-open JetBrains.ProjectModel.ProjectsHost.MsBuild
-open JetBrains.ProjectModel.ProjectsHost.SolutionHost
-open JetBrains.ReSharper.Daemon.Impl
 open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.Modules
-open JetBrains.ReSharper.Resources.Shell
 open JetBrains.ReSharper.Plugins.FSharp
 open JetBrains.ReSharper.Plugins.FSharp.Common.Util
 open JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModel
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModel.ProjectItems.ItemsContainer
-open JetBrains.ReSharper.Plugins.FSharp.ProjectModelBase
 open JetBrains.Threading
 open JetBrains.Util
 open JetBrains.Util.Dotnet.TargetFrameworkIds
-open Microsoft.FSharp.Compiler
-open Microsoft.FSharp.Compiler.ErrorLogger
 open Microsoft.FSharp.Compiler.SourceCodeServices
 
 [<SolutionComponent>]
@@ -110,24 +91,28 @@ type FSharpProjectOptionsProvider
                 Some (createFSharpProject project psiModule))
         | _ -> None
 
-    let rec invalidateProject (project: IProject) =
-        logger.Info("Invalidating {0}", project)
-        tryGetValue project projects
-        |> Option.iter (fun fsProjectsForProject ->
-            for fsProject in fsProjectsForProject.Values do
-                checker.InvalidateConfiguration(fsProject.Options, false)
-            fsProjectsForProject.Clear())
+    let invalidateProject project =
+        let invalidatedProjects = HashSet()
+        let rec invalidate (project: IProject) =
+            logger.Info("Invalidating {0}", project)
+            tryGetValue project projects
+            |> Option.iter (fun fsProjectsForProject ->
+                for fsProject in fsProjectsForProject.Values do
+                    checker.InvalidateConfiguration(fsProject.Options, false)
+                fsProjectsForProject.Clear())
 
-        // todo: keep referencing projects for invalidating removed projects
-        let referencesToProject = referenceResolveStore.GetReferencesToProject(project)
-        if not (referencesToProject.IsEmpty()) then
-            logger.Info("Invalidatnig reverencing projects")
-            for reference in referencesToProject do
-                match reference.GetProject() with
-                | FSharpProject referencingProject when
-                        referencingProject <> project -> invalidateProject referencingProject
-                | _ -> ()
-            logger.Info("Done invalidating {0}", project)
+            invalidatedProjects.Add(project) |> ignore
+            // todo: keep referencing projects for invalidating removed projects
+            let referencesToProject = referenceResolveStore.GetReferencesToProject(project)
+            if not (referencesToProject.IsEmpty()) then
+                logger.Info("Invalidatnig projects reverencing {0}", project)
+                for reference in referencesToProject do
+                    match reference.GetProject() with
+                    | FSharpProject referencingProject when
+                            not (invalidatedProjects.Contains(referencingProject)) -> invalidate referencingProject
+                    | _ -> ()
+                logger.Info("Done invalidating {0}", project)
+        invalidate project
 
 
     let isScriptLike file =
