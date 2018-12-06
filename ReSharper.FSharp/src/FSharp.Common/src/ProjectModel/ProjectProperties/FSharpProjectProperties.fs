d8434dfd8f9b96c0d992e82d0cf4fab6b44b5c43
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-12-06 17:44:44 +0300

    Move F# project properties types

--- ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/ProjectProperties/FSharpProjectProperties.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/ProjectProperties/FSharpProjectProperties.fs
@@ -1,14 +1,18 @@
-namespace rec JetBrains.ReSharper.Plugins.FSharp.ProjectModel.ProjectProperties
+namespace JetBrains.ReSharper.Plugins.FSharp.ProjectModel.ProjectProperties
 
 open JetBrains.Application
 open System
 open System.Runtime.InteropServices
 open JetBrains.Metadata.Utils
+open JetBrains.ProjectModel
 open JetBrains.ProjectModel.Impl.Build
 open JetBrains.ProjectModel.ProjectsHost.MsBuild.Diagnostic.Components
+open JetBrains.ProjectModel.Properties
 open JetBrains.ProjectModel.Properties.Common
 open JetBrains.ProjectModel.Properties.Managed
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModel
+open JetBrains.Util
+
 
 type FSharpProjectProperties =
     inherit ProjectPropertiesBase<ManagedProjectConfiguration>
@@ -52,7 +56,7 @@ type FSharpProjectProperties =
         base.Dump(writer, indent + 1)
 
 
-type FSharpBuildSettings() =
+and FSharpBuildSettings() =
     inherit ManagedProjectBuildSettings()
 
     member val TailCalls = Unchecked.defaultof<bool> with get, set
@@ -76,3 +80,43 @@ type FSharpProjectApplicableProvider() =
     interface ProjectConfigurationValidator.IApplicableProvider with
         member x.IsApplicable(projectMark) =
             projectMark.Location.ExtensionNoDot.Equals("fsproj", StringComparison.OrdinalIgnoreCase)
+
+
+[<ProjectModelExtension>]
+type FSharpProjectPropertiesFactory() =
+    inherit UnknownProjectPropertiesFactory()
+
+    static let factoryGuid = Guid("{7B32A26D-3EC5-4A2A-B40C-EC79FF38A223}")
+    static let fsProjectTypeGuid = Guid("{F2A71F9B-5D33-465A-A702-920D77279786}")
+    static let fsCpsProjectTypeGuid = Guid("{6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705}")
+
+    override x.FactoryGuid = factoryGuid
+
+    override x.IsApplicable(parameters) =
+        FSharpProjectPropertiesFactory.IsKnownProjectTypeGuid(parameters.ProjectTypeGuid)
+
+    override x.IsKnownProjectTypeGuid(projectTypeGuid) =
+        FSharpProjectPropertiesFactory.IsKnownProjectTypeGuid(projectTypeGuid)
+
+    override x.CreateProjectProperties(parameters) =
+        FSharpProjectProperties(parameters.ProjectTypeGuids, factoryGuid, parameters.TargetFrameworkIds,
+                                parameters.TargetPlatformData, parameters.DotNetCoreSDK) :> _
+
+    static member CreateProjectProperties(targetFrameworkIds): IProjectProperties =
+        FSharpProjectProperties([fsProjectTypeGuid].AsCollection(), factoryGuid, targetFrameworkIds, null, null) :> _
+
+    override x.Read(reader) =
+        let projectProperties = FSharpProjectProperties(factoryGuid)
+        projectProperties.ReadProjectProperties(reader)
+        projectProperties :> _
+
+    static member IsKnownProjectTypeGuid(guid) =
+        guid.Equals(fsProjectTypeGuid) || guid.Equals(fsCpsProjectTypeGuid)
+
+
+[<ProjectModelExtension>]
+type FSharpProjectFilePropertiesProvider() =
+    inherit ProjectFilePropertiesProvider()
+
+    override x.IsApplicable(properties) = properties :? FSharpProjectProperties
+    override x.CreateProjectFileProperties() = ProjectFileProperties() :> IProjectFileProperties
