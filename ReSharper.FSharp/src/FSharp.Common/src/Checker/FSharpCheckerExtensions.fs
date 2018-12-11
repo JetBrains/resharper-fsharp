0af652ecef28af8e99863b02fbe9c385174bca40
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-12-11 20:43:13 +0300

    Fix getting stale cached type check

--- ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpCheckerExtensions.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Checker/FSharpCheckerExtensions.fs
@@ -1,69 +1,27 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 
-open System
 open Microsoft.FSharp.Compiler.SourceCodeServices
 
-module FSharpCheckerExtensions =
-    let map (f: 'T -> 'U) (a: Async<'T>) : Async<'U> =
-        async {
-            let! a = a
-            return f a
-        }
-
-    type CheckResults =
-        | Ready of (FSharpParseFileResults * FSharpCheckFileResults) option
-        | StillRunning of Async<(FSharpParseFileResults * FSharpCheckFileResults) option>
-
-open FSharpCheckerExtensions
-
 [<Extension; Sealed; AbstractClass>]
-type FSharpCheckerExtensions =
+type FSharpCheckerExtensions() =
+    static let [<Literal>] timeout = 2000
+
     [<Extension>]
-    static member ParseAndCheckDocument(checker: FSharpChecker, filePath: string, sourceText: string, options: FSharpProjectOptions, allowStaleResults: bool) =
-        let parseAndCheckFile =
-            async {
-                let! parseResults, checkFileAnswer = checker.ParseAndCheckFileInProject(filePath, sourceText.GetHashCode(), sourceText, options)
-                return
-                    match checkFileAnswer with
-                    | FSharpCheckFileAnswer.Aborted ->
-                        None
-                    | FSharpCheckFileAnswer.Succeeded(checkFileResults) ->
-                        Some (parseResults, checkFileResults)
-            }
+    static member ParseAndCheckDocument
+            (checker: FSharpChecker, filePath: string, sourceText: string, options: FSharpProjectOptions,
+             allowStaleResults: bool) =
 
-        let tryGetFreshResultsWithTimeout() : Async<CheckResults> =
-            async {
-                try
-                    let! worker = Async.StartChild(parseAndCheckFile, 1000)
-                    let! result = worker
-                    return Ready result
-                with :? TimeoutException ->
-                    return StillRunning parseAndCheckFile
-            }
+        let parseAndCheckFile = async {
+            match! checker.ParseAndCheckFileInProject(filePath, 0, sourceText, options) with
+            | _, FSharpCheckFileAnswer.Aborted -> return None
+            | parseResults, FSharpCheckFileAnswer.Succeeded(checkResults) -> return Some (parseResults, checkResults) }
 
-        let bindParsedInput(results: (FSharpParseFileResults * FSharpCheckFileResults) option) =
-            match results with
-            | Some(parseResults, checkResults) ->
-                match parseResults.ParseTree with
-                | Some parsedInput -> Some (parseResults, checkResults)
-                | None -> None
-            | None -> None
+        async {
+            let! token = Async.CancellationToken
 
-        if allowStaleResults then
-            async {
-                let! freshResults = tryGetFreshResultsWithTimeout()
+            let task = Async.StartAsTask(parseAndCheckFile, cancellationToken = token)
+            if task.Wait(timeout, token) then return task.Result else
 
-                let! results =
-                    match freshResults with
-                    | Ready x -> async.Return x
-                    | StillRunning worker ->
-                        async {
-                            match allowStaleResults, checker.TryGetRecentCheckResultsForFile(filePath, options) with
-                            | true, Some (parseResults, checkFileResults, _) ->
-                                return Some (parseResults, checkFileResults)
-                            | _ ->
-                                return! worker
-                        }
-                return bindParsedInput results
-            }
-        else parseAndCheckFile |> map bindParsedInput
+            match checker.TryGetRecentCheckResultsForFile(filePath, options) with
+            | Some (parseResults, checkFileResults, _) -> return Some (parseResults, checkFileResults)
+            | None -> return! parseAndCheckFile }
