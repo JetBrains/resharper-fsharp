bc87915942d6776815003346bcd15c9b31faaf94
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-01-31 12:12:22 +0300

    Decompile operator names when getting element source names

--- ReSharper.FSharp/src/FSharp.Psi/src/Util/FSharpNamesUtil.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Util/FSharpNamesUtil.cs
@@ -5,11 +5,11 @@ using JetBrains.Annotations;
 using JetBrains.Metadata.Reader.API;
 using JetBrains.Metadata.Reader.Impl;
 using JetBrains.ReSharper.Plugins.FSharp.Psi.Impl;
-using JetBrains.ReSharper.Plugins.FSharp.Psi.Tree;
 using JetBrains.ReSharper.Psi;
 using JetBrains.Util;
 using JetBrains.Util.Extension;
 using Microsoft.FSharp.Core;
+using static Microsoft.FSharp.Compiler.PrettyNaming;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 {
@@ -26,13 +26,6 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
     private static readonly ClrTypeName CompilationRepresentationAttr =
       new ClrTypeName("Microsoft.FSharp.Core.CompilationRepresentationAttribute");
 
-    public static bool IsEscapedWithParens([NotNull] string name)
-    {
-      var length = name.Length;
-      return length > EscapedNameAffixLength &&
-             name[0] == '(' && name[1] == ' ' && name[length - 2] == ' ' && name[length - 1] == ')';
-    }
-
     public static bool IsEscapedWithBackticks([NotNull] string name)
     {
       var length = name.Length;
@@ -47,18 +40,13 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
         : name;
 
     [NotNull]
-    public static string RemoveParens([NotNull] string name, out bool isEscaped)
-    {
-      isEscaped = IsEscapedWithParens(name);
-      return isEscaped
-        ? name.Substring(EscapedNameStartIndex, name.Length - EscapedNameAffixLength)
-        : name;
-    }
-
-    [NotNull]
     public static IEnumerable<string> GetPossibleSourceNames([NotNull] IDeclaredElement element)
     {
-      var names = new HashSet<string> {element.ShortName};
+      var name = element.ShortName;
+      var names = new HashSet<string> {name, DecompileOpName.Invoke(name)};
+
+      if (element is IFSharpDeclaredElement fsDeclaredElement)
+        names.Add(fsDeclaredElement.SourceName);
 
       var typeElement = (element as IConstructor)?.GetContainingType();
       if (typeElement != null)
@@ -72,18 +60,10 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
         if (GetAttributeValue(attrOwner, SourceNameAttributeAttr) is string sourceName)
           names.Add(sourceName);
         if (GetAttributeValue(attrOwner, CompilationRepresentationAttr) is int reprFlag && reprFlag == ModuleSuffixFlag)
-          names.Add(element.ShortName.SubstringBeforeLast(ModuleSuffix, StringComparison.Ordinal));
+          names.Add(name.SubstringBeforeLast(ModuleSuffix, StringComparison.Ordinal));
       }
 
-      foreach (var declaration in element.GetDeclarations())
-        if (declaration is IFSharpDeclaration fsDeclaration)
-          names.Add(fsDeclaration.SourceName);
-
-      var result = new List<string>(names.Count * 2);
-      result.AddRange(names);
-      result.AddRange(names.Select(n => $"``{n}``"));
-
-      return result;
+      return names.SelectMany(n => new[] {n, $"``{n}``"});
     }
 
     private static void GetPossibleSourceNames(ITypeElement type, ISet<string> names)
