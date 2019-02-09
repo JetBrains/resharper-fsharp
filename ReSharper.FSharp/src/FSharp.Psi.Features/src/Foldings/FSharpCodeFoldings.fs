85f41a4ca29cf21a2cc9050cba7d86afc3009628
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-02-09 03:40:37 +0300

    Cleanup code folding process

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Foldings/FSharpCodeFoldings.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Foldings/FSharpCodeFoldings.fs
@@ -6,19 +6,20 @@ open JetBrains.ReSharper.Feature.Services.Daemon
 open JetBrains.ReSharper.Plugins.FSharp.Common.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 open JetBrains.ReSharper.Psi
-open JetBrains.ReSharper.Psi.Tree
 open JetBrains.TextControl.DocumentMarkup
 open JetBrains.Util
-open JetBrains.Util.dataStructures.TypedIntrinsics
+open JetBrains.Util.Logging
 open Microsoft.FSharp.Compiler.SourceCodeServices
 open Microsoft.FSharp.Compiler.SourceCodeServices.Structure
 
 [<Language(typeof<FSharpLanguage>)>]
-type FSharpCodeFoldingProcessFactory(logger: ILogger) =
+type FSharpCodeFoldingProcessFactory() =
     interface ICodeFoldingProcessorFactory with
         member x.CreateProcessor() =
-            FSharpCodeFoldingProcess(logger) :> _
+            FSharpCodeFoldingProcess() :> _
+
 
 and FSharpCodeFoldingProcess(logger) =
     inherit TreeNodeVisitor<IHighlightingConsumer>()
@@ -30,55 +31,61 @@ and FSharpCodeFoldingProcess(logger) =
         | Scope.Comment -> CodeFoldingAttributes.COMMENTS_FOLDING_ATTRIBUTE
         | Scope.XmlDocComment -> CodeFoldingAttributes.DOCUMENTATION_COMMENTS_FOLDING_ATTRIBUTE
         | Scope.Member -> CodeFoldingAttributes.METHOD_FOLDING_ATTRIBUTE
-        | Scope.HashDirective -> FSharpCodeFoldingAttributes.hashDirectivesAttribute
+        | Scope.HashDirective -> FSharpCodeFoldingAttributes.HashDirective
         | _ -> CodeFoldingAttributes.DEFAULT_FOLDING_ATTRIBUTE
 
     override x.VisitNode(element, context) =
-        match element.GetContainingFile() with
-        | :? IFSharpFile as fsFile ->
-            match fsFile.ParseResults with
-            | Some parseResults when parseResults.ParseTree.IsSome ->
-                let sourcefile = element.GetSourceFile()
-                let document = sourcefile.Document
-                let lines = [| for line in 0 .. (int (document.GetLineCount().Minus1())) do
-                                yield document.GetLineText(docLine line) |]
+        match element.GetContainingNode<IFSharpFile>() with
+        | null -> ()
+        | fsFile ->
 
-                Structure.getOutliningRanges lines parseResults.ParseTree.Value
-                |> Seq.distinctBy (fun x -> x.Range.StartLine)
-                |> Seq.iter (fun x ->
-                    let mutable textRange = x.CollapseRange.ToTextRange(document)
-                    let docRange = DocumentRange(document, textRange)
-                    let placeholder =
-                        match x.Scope with
-                        | Scope.Open -> "..."
-                        | _ ->
-                            let line = (docLine x.CollapseRange.StartLine).Minus1()
-                            let lineStart = document.GetLineStartOffset(line)
-                            let lineEnd = document.GetLineEndOffsetNoLineBreak(line)
-                            match TextRange(lineStart, lineEnd).Intersect(&textRange) with
-                            | range when not range.IsEmpty -> document.GetText(range) + " ..."
-                            | _ -> " ..."
-                    if not textRange.IsEmpty then
-                        let highlighting = CodeFoldingHighlighting(getFoldingAttrId x.Scope, placeholder, docRange, 0)
-                        context.AddHighlighting(highlighting)
-                    else
-                        logger.LogMessage(LoggingLevel.WARN, sprintf "Empty folding: %O %A" textRange x))
-            | _ -> ()
-        | _ -> ()
-        processingFinished <- true
+        match fsFile.ParseTree with
+        | None -> ()
+        | Some parseTree ->
+
+        let sourcefile = element.GetSourceFile()
+        let document = sourcefile.Document
+        let lines = [| for line in 0 .. (int (document.GetLineCount().Minus1())) do
+                        yield document.GetLineText(docLine line) |]
+
+        Structure.getOutliningRanges lines parseTree
+        |> Seq.distinctBy (fun x -> x.Range.StartLine)
+        |> Seq.iter (fun x ->
+            let mutable textRange = x.CollapseRange.ToTextRange(document)
+            let docRange = DocumentRange(document, textRange)
+            let placeholder =
+                match x.Scope with
+                | Scope.Open -> "..."
+                | _ ->
+
+                let line = (docLine x.CollapseRange.StartLine).Minus1()
+                let lineStart = document.GetLineStartOffset(line)
+                let lineEnd = document.GetLineEndOffsetNoLineBreak(line)
+                match TextRange(lineStart, lineEnd).Intersect(&textRange) with
+                | range when not range.IsEmpty -> document.GetText(range) + " ..."
+                | _ -> " ..."
+
+            if not textRange.IsEmpty then
+                let highlighting = CodeFoldingHighlighting(getFoldingAttrId x.Scope, placeholder, docRange, 0)
+                context.AddHighlighting(highlighting)
+            else
+                let logger = Logger.GetLogger<FSharpCodeFoldingProcess>()
+                logger.LogMessage(LoggingLevel.WARN, sprintf "Empty folding: %O %A" textRange x))
 
     interface ICodeFoldingProcessor with
         member x.InteriorShouldBeProcessed(element, context) = false
         member x.IsProcessingFinished(context) = processingFinished
         member x.ProcessAfterInterior(element, context) = ()
         member x.ProcessBeforeInterior(element, context) =
+            processingFinished <- true
             match element with
             | :? IFSharpTreeNode as treeNode -> treeNode.Accept(x, context)
             | _ -> ()
 
-module FSharpCodeFoldingAttributes =
-    let [<Literal>] hashDirectivesAttribute = "ReSharper F# Hash Directives Block Folding"
 
-[<assembly: RegisterHighlighter(FSharpCodeFoldingAttributes.hashDirectivesAttribute, EffectType = EffectType.FOLDING)>]
-do
-    ()
+module FSharpCodeFoldingAttributes =
+    let [<Literal>] HashDirective = "ReSharper F# Hash Directives Block Folding"
+
+
+[<assembly: RegisterHighlighter(FSharpCodeFoldingAttributes.HashDirective, EffectType = EffectType.FOLDING)>]
+do ()
