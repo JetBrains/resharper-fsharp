ec415e28ebd5fef41c21fa1862fa4b9a4810fc07
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-03-26 20:20:13 +0300

    Enable searchable options

--- rider-fsharp/build.gradle.kts
+++ rider-fsharp/build.gradle.kts
@@ -207,10 +207,6 @@ tasks {
         dependsOn(generateFSharpLexer, "rdgen")
     }
 
-    getByName("buildSearchableOptions") {
-        enabled = false
-    }
-
     withType<Test> {
         useTestNG()
         testLogging {
