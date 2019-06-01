bbb3bb5be999cce9538d887a79539eae07efde7c
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-06-01 14:44:42 +0300

    Enable F# errors stage

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Analyzers/ReSpellerHelper.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Analyzers/ReSpellerHelper.fs
@@ -4,6 +4,7 @@ open JetBrains.ReSharper.Features.ReSpeller.Analyzers
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Tree
 
 [<Language(typeof<FSharpLanguage>)>]
 type ReSpellerPsiHelper() =
@@ -13,4 +14,5 @@ type ReSpellerPsiHelper() =
         match declaration with
         | :? ISynPat as synPat -> not synPat.IsDeclaration
         | :? IFSharpDeclaration as fsDeclaration -> fsDeclaration.NameIdentifier :? IActivePatternId
+        | :? INamespaceDeclaration -> true // A workaround for a spell-checker bug, todo: remove
         | _ -> true
---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpErrorsStage.fs
@@ -0,0 +1,68 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Stages
+
+open System
+open JetBrains.Application.Settings
+open JetBrains.ReSharper.Daemon.Stages.Dispatcher
+open JetBrains.ReSharper.Feature.Services.Daemon
+open JetBrains.ReSharper.Plugins.FSharp.Daemon.Cs.Stages
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Tree
+
+[<DaemonStage(StagesBefore = [| typeof<HighlightIdentifiersStage> |])>]
+type FSharpErrorStage(elementProblemAnalyzerRegistrar) =
+    inherit FSharpDaemonStageBase()
+
+    override x.CreateStageProcess(fsFile, settings, daemonProcess) =
+        FSharpErrorStageProcess(fsFile, daemonProcess, settings, elementProblemAnalyzerRegistrar) :> IDaemonStageProcess
+
+
+and FSharpErrorStageProcess
+        (fsFile: IFSharpFile, daemonProcess: IDaemonProcess, settings: IContextBoundSettingsStore,
+         analyzerRegistrar: ElementProblemAnalyzerRegistrar) =
+    inherit TreeNodeVisitor<IHighlightingConsumer>()
+
+    static let analyzerRunKind = ElementProblemAnalyzerRunKind.FullDaemon
+
+    let interruptCheck = daemonProcess.GetCheckForInterrupt()
+    let elementProblemAnalyzerData = ElementProblemAnalyzerData(fsFile, settings, analyzerRunKind, interruptCheck)
+    let analyzerDispatcher = analyzerRegistrar.CreateDispatcher(elementProblemAnalyzerData)
+
+    member x.DaemonProcess = daemonProcess
+
+    override x.VisitNode(element: ITreeNode, consumer: IHighlightingConsumer) =
+        analyzerDispatcher.Run(element, consumer)
+
+    interface IDaemonStageProcess with
+        member x.Execute(committer) =
+            // todo: schedule separate processors for different top level nodes
+            let consumer = FilteringHighlightingConsumer(daemonProcess.SourceFile, fsFile, settings)
+            fsFile.ProcessThisAndDescendants(Processor(x, consumer))
+            committer.Invoke(DaemonStageResult(consumer.Highlightings))
+
+        member x.DaemonProcess = x.DaemonProcess
+
+
+and Processor(daemonProcess: FSharpErrorStageProcess, consumer: IHighlightingConsumer) =
+    abstract InteriorShouldBeProcessed: ITreeNode -> bool
+    default x.InteriorShouldBeProcessed _ = true
+
+    interface IRecursiveElementProcessor with
+        member x.InteriorShouldBeProcessed(element) =
+            x.InteriorShouldBeProcessed(element)
+
+        member x.ProcessingIsFinished =
+            match daemonProcess.DaemonProcess.InterruptFlag with
+            | true -> OperationCanceledException() |> raise
+            | _ -> false
+
+        member x.ProcessBeforeInterior _ = ()
+
+        member x.ProcessAfterInterior(element) =
+            match element with
+            | :? FSharpToken as token ->
+                if not (token.GetTokenType().IsWhitespace) then
+                    token.Accept(daemonProcess, consumer)
+
+            | _ -> daemonProcess.VisitNode(element, consumer)
