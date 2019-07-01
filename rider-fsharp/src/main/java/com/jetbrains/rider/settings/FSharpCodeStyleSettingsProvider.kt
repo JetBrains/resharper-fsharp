28dd577001f687a23f6e95e3e85284fb2fe1392e
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-07-01 20:34:41 +0300

    Fix opening code style settings from status bar

--- rider-fsharp/src/main/java/com/jetbrains/rider/settings/FSharpCodeStyleSettingsProvider.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/settings/FSharpCodeStyleSettingsProvider.kt
@@ -1,13 +1,17 @@
 package com.jetbrains.rider.settings
 
 import com.intellij.lang.Language
+import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.FSharpLanguage
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.FSharpScriptLanguage
 
 class FSharpCodeStyleSettingsProvider : FSharpCodeStyleSettingsProviderBase(FSharpLanguage)
 class FSharpScriptCodeStyleSettingsProvider : FSharpCodeStyleSettingsProviderBase(FSharpScriptLanguage)
 
-abstract class FSharpCodeStyleSettingsProviderBase(private val lang: Language) : RiderCodeStyleSettingsProvider() {
+abstract class FSharpCodeStyleSettingsProviderBase(private val lang: Language) : RiderLanguageCodeStyleSettingsProvider() {
+    override fun createConfigurable(baseSettings: CodeStyleSettings, modelSettings: CodeStyleSettings) =
+            createRiderConfigurable(baseSettings, modelSettings, language, configurableDisplayName)
+
     override fun getLanguage() = lang
     override fun getHelpTopic() = "Settings_Code_Style_FSHARP"
     override fun getConfigurableDisplayName() = lang.displayName
