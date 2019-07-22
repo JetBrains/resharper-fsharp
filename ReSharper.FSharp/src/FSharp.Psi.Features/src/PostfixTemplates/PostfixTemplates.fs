d06e2c996b35be09ebb4c5502c31449db76f05ea
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-07-22 23:23:03 +0300

    Initial postfix templates support

---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/PostfixTemplates/PostfixTemplates.fs
@@ -0,0 +1,43 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.CodeCompletion
+
+open JetBrains.ReSharper.Feature.Services.CodeCompletion.PostfixTemplates
+open JetBrains.ReSharper.Feature.Services.PostfixTemplates.Contexts
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Services.Cs.CodeCompletion
+open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Tree
+open JetBrains.Util
+
+[<Language(typeof<FSharpLanguage>)>]
+type FSharpPostfixTemplateContextFactory() =
+    interface IPostfixTemplateContextFactory with
+        member x.GetReparseStrings() = EmptyArray.Instance
+
+        member x.TryCreate(treeNode, executionContext) =
+            FSharpPostfixTemplateContext(treeNode, executionContext) :> _
+
+
+and FSharpPostfixExpressionContext(postfixContext, expression) =
+    inherit PostfixExpressionContext(postfixContext, expression)
+
+
+and FSharpPostfixTemplateContext(treeNode: ITreeNode, executionContext: PostfixTemplateExecutionContext) as this =
+    inherit PostfixTemplateContext(treeNode, executionContext)
+
+    override x.Language = FSharpLanguage.Instance :> _
+
+    override x.GetAllExpressionContexts() =
+        [| FSharpPostfixExpressionContext(this, treeNode) :> PostfixExpressionContext |] :> _
+
+
+[<Language(typeof<FSharpLanguage>)>]
+type FSharpPostfixTemplatesProvider(templatesManager, sessionExecutor, usageStatistics) =
+    inherit PostfixTemplatesItemProviderBase<FSharpCodeCompletionContext, FSharpPostfixTemplateContext>(
+        templatesManager, sessionExecutor, usageStatistics)
+
+    override x.TryCreatePostfixContext(fsCompletionContext) =
+        let context = fsCompletionContext.BasicContext
+        let settings = context.ContextBoundSettingsStore
+        let executionContext = PostfixTemplateExecutionContext(context.Solution, context.TextControl, settings, "__")
+
+        FSharpPostfixTemplateContext(fsCompletionContext.TokenBeforeCaret, executionContext)
