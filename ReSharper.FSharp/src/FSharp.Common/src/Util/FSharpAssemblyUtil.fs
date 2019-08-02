719f39b167244bbba715fa49720700c33054f917
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-08-02 19:38:46 +0300

    Add IsFSharpAssembly helper

---
+++ ReSharper.FSharp/src/FSharp.Common/src/Util/FSharpAssemblyUtil.fs
@@ -0,0 +1,30 @@
+[<AutoOpen>]
+module JetBrains.ReSharper.Plugins.FSharp.Util.FSharpAssemblyUtil
+
+open JetBrains.ProjectModel
+open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Modules
+open JetBrains.ReSharper.Resources.Shell
+open JetBrains.Util
+
+[<CompiledName("InterfaceDataVersionAttrTypeName")>]
+let interfaceDataVersionAttrTypeName = clrTypeName "Microsoft.FSharp.Core.FSharpInterfaceDataVersionAttribute"
+
+let isFSharpAssemblyKey = Key("IsFSharpAssembly")
+
+[<Extension; CompiledName("IsFSharpAssembly")>]
+let isFSharpAssembly (psiModule: IPsiModule) =
+    match psiModule.ContainingProjectModule with
+    | :? IProject -> false
+    | _ ->
+
+    match psiModule.GetData(isFSharpAssemblyKey) with
+    | null ->
+        use cookie = ReadLockCookie.Create()
+        let attrs = psiModule.GetPsiServices().Symbols.GetModuleAttributes(psiModule)
+        let isFSharpAssembly = attrs.HasAttributeInstance(interfaceDataVersionAttrTypeName, false)
+
+        psiModule.PutData(isFSharpAssemblyKey, if isFSharpAssembly then BooleanBoxes.True else BooleanBoxes.False)
+        isFSharpAssembly
+
+    | value -> value == BooleanBoxes.True
---
+++ ReSharper.FSharp/test/src/FSharp.Tests/IsFSharpAssemblyTest.fs
@@ -0,0 +1,25 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
+
+open JetBrains.ProjectModel
+open JetBrains.ReSharper.Plugins.FSharp.Util
+open JetBrains.ReSharper.Plugins.FSharp.Tests.Common
+open JetBrains.ReSharper.Psi.Modules
+open JetBrains.ReSharper.TestFramework
+open NUnit.Framework
+
+[<FSharpTest>]
+type IsFSharpAssemblyTest() =
+    inherit BaseTestWithSingleProject()
+
+    member x.DoTest(moduleName: string, expected: bool) =
+        x.WithSingleProject([], fun lifetime solution (project: IProject) ->
+            let modules = project.GetSolution().PsiModules().GetModules()
+            match modules |> Seq.tryFind (fun m -> m.Name = moduleName) with
+            | None -> failwith "Could not get module"
+            | Some psiModule -> Assert.AreEqual(expected, isFSharpAssembly psiModule))
+
+    [<Test>]
+    member x.``mscorlib``() = x.DoTest("mscorlib", false)
+
+    [<Test; TestPackages("FSharp.Core")>]
+    member x.``FSharpCore``() = x.DoTest("FSharp.Core", true)
