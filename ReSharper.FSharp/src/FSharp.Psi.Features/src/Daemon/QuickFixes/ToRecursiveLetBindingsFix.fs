f84d852b3cc9caa5d3fbb34176eede7daed70dd3
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-08-08 19:51:37 +0300

    Fix ToRecursiveLetBindingsFix

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/QuickFixes/ToRecursiveLetBindingsFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/QuickFixes/ToRecursiveLetBindingsFix.fs
@@ -1,7 +1,5 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
-
 open JetBrains.ReSharper.Feature.Services.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
@@ -15,5 +13,5 @@ type ToRecursiveLetBindingsFix(error: LetAndForNonRecBindingsError) =
     override x.IsAvailable _ = isValid letBindings
 
     override x.ExecutePsiTransaction(_, _) =
-        letBindings.SetIsRecursive(letBindings.IsPhysical())
+        letBindings.SetIsRecursive(true)
         null
