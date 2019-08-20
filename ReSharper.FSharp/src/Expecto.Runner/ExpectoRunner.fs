ffe16706463edd5381f6338d1d6a6489456355d9
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-08-20 15:41:41 +0300

    Launch expecto out of process, move runner to unsigned project

--- ReSharper.FSharp/src/Expecto.Runner/ExpectoRunner.fs
+++ ReSharper.FSharp/src/Expecto.Runner/ExpectoRunner.fs
@@ -2,77 +2,83 @@
 
 open System
 open System.IO
-open System.Xml
+open System.Reflection
+open global.Expecto
+open global.Expecto.Impl
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.UnitTesting.Expecto.Tasks
 open JetBrains.ReSharper.TaskRunnerFramework
 
-let [<Literal>] expectoId = "Expecto"
-
 [<AllowNullLiteral>]
-type TestRunner() =
+type ExpectoTestRunner() =
     inherit MarshalByRefObject()
 
     member x.Run(server: IRemoteTaskServer, configuration: TaskExecutorConfiguration, logger: SimpleLogger,
-                 assemblyTaskNode: TaskExecutionNode,  shadowCopyCookie: IShadowCopyCookie) =
+                 assemblyTaskNode: TaskExecutionNode,  shadowCopyCookie: IShadowCopyCookie, task: ExpectoAssemblyTask) =
+        TaskExecutor.Configuration <- configuration
+        TaskExecutor.Logger <- logger
+
+        let assemblyPath = task.Path
+        let assembly = Assembly.LoadFrom(assemblyPath)
+        match testFromAssembly assembly with
+        | None -> ()
+        | Some test ->
+
+        let testSummary _ testSummary =
+            for executionNode in assemblyTaskNode.Children do
+                server.TaskDuration(executionNode.RemoteTask, testSummary.duration)
+                server.TaskFinished(executionNode.RemoteTask, "", TaskResult.Success)
+
+            server.TaskDuration(assemblyTaskNode.RemoteTask, testSummary.duration)
+            server.TaskFinished(assemblyTaskNode.RemoteTask, "", TaskResult.Success)
+            async.Zero()
+
+        let beforeRun _ =
+            async.Zero()
+
+        let beforeEach _ =
+            async.Zero()
+
+        let info _ =
+            async.Zero()
+
+        let passed _ _ =
+            async.Zero()
+
+        let ignored _ _ =
+            async.Zero()
+
+        let failed _ _ _ =
+            async.Zero()
+
+        let exn _ _ _ =
+            async.Zero()
+
+        let testPrinters =
+            { beforeRun = beforeRun
+              beforeEach = beforeEach
+              info = info 
+              summary = testSummary
+              passed = passed
+              ignored = ignored
+              failed = failed
+              exn = exn }
+
+        let tests = Expecto.Test.toTestCodeList test
+        let conf = { defaultConfig with printer = testPrinters }
+
+//        server.TaskDiscovered(DiscoveryFinishedTask(expectoId))
+        
+//        for executionNode in assemblyTaskNode.Children do
+//            server.TaskDiscovered(executionNode.RemoteTask)
+//            server.TaskStarting(executionNode.RemoteTask)
+
+        Expecto.Tests.runTests conf test |> ignore
+
+        let _ =
+            test,
+            tests
         ()
 
-and [<Serializable>]
-    ExpectoAssemblyTask(path: string) =
-    inherit RemoteTask(expectoId)
-
-    static let [<Literal>] attributePathId = "Path"
-    
-    new (xmlElement: XmlElement) =
-        let path = RemoteTask.GetXmlAttribute(xmlElement, attributePathId)
-        ExpectoAssemblyTask(path)
-
-    member x.Path = path
-
-    override x.SaveXml(xmlElement) =
-        base.SaveXml(xmlElement)
-        RemoteTask.SetXmlAttribute(xmlElement, attributePathId, path)
-
-    override x.IsMeaningfulTask = false
-
-    override x.Equals (other: obj) =
-        match other with
-        | :? ExpectoAssemblyTask as task -> x.Equals(task)
-        | _ -> false
-
-    override x.Equals(other: RemoteTask) =
-        match other with
-        | :? ExpectoAssemblyTask as task -> task.Path = path
-        | _ -> false
-
-    override x.GetHashCode() = path.GetHashCode()
-
-and [<Serializable>]
-    ExpectTestElementTask(elementId: string) =
-    inherit RemoteTask(expectoId)
-
-    static let [<Literal>] attributeElementId = "ElementId"
-    
-    new (xmlElement: XmlElement) =
-        let path = RemoteTask.GetXmlAttribute(xmlElement, attributeElementId)
-        ExpectTestElementTask(path)
-
-    override x.SaveXml(xmlElement) =
-        base.SaveXml(xmlElement)
-        RemoteTask.SetXmlAttribute(xmlElement, attributeElementId, elementId)
-
-    member x.TestId = elementId
-    
-    override x.IsMeaningfulTask = true
-    override x.Equals(other: RemoteTask) =
-        match other with
-        | :? ExpectTestElementTask as task -> task.TestId = elementId
-        | _ -> false
-
-    override x.Equals (other: obj) =
-        match other with
-        | :? ExpectTestElementTask as task -> x.Equals(task)
-        | _ -> false
-
-    override x.GetHashCode() = elementId.GetHashCode()
 
 and ExpectoTaskRunner(server) =
     inherit RecursiveRemoteTaskRunner(server)
@@ -85,36 +91,35 @@ and ExpectoTaskRunner(server) =
         let assemblyCodeBase = Path.GetDirectoryName(assemblyLocation)
         let assemblyConfig = Path.GetFullPath(assemblyLocation) + ".config"
 
-        use our = new AssemblyLoader()
-        our.RegisterAssemblyOf<AssemblyLoader>()
-        our.RegisterAssemblyOf<TestRunner>()
-        
+        use ourAssemblyLoader = new AssemblyLoader()
+        ourAssemblyLoader.RegisterAssemblyOf<AssemblyLoader>()
+        ourAssemblyLoader.RegisterAssemblyOf<ExpectoTestRunner>()
+
+        let assemblyDir = Path.GetDirectoryName(assemblyLocation)
+
         use appConfig = AppConfig.Change(assemblyConfig)
         use shadowCopyCookie = ShadowCopy.SetupFor(assemblyCodeBase)
         use domainCookie = AppDomainBuilder.Setup(assemblyLocation, assemblyConfig, shadowCopyCookie)
-        use assemblyLoader = domainCookie.CreateAndUnwrapFrom<AssemblyLoader>()
-        assemblyLoader.RegisterAssemblyOf<AssemblyLoader>()
-        assemblyLoader.RegisterAssemblyOf<TestRunner>()
-          
-          //ES: There is a difference in how Mono and .NET resolves assemblies.
-          //    Whenever one loads an assembly using Assembly.LoadFrom method,
-          //    in true .NET its dependencies will also be looked for in the folder
-          //    one loaded it from. Mono, however, does not check that folder.
-        assemblyLoader.RegisterPathOf<TestRunner>()
-          
-          //ES: There are yet undetermined cases when test runner classes or
-          //    classes from a test assembly may leak into our domain. Registering
-          //    test runner root and test assembly root helps us resolving them. 
-        our.RegisterPathOf<TestRunner>()
-        our.RegisterPath(shadowCopyCookie.TargetLocation)
+        use domainAssemblyLoader = domainCookie.CreateAndUnwrapFrom<AssemblyLoader>()
 
-        runner <- domainCookie.CreateAndUnwrapFrom<TestRunner>()
-        runner.Run(x.Server, TaskExecutor.Configuration, TaskExecutor.Logger, node, shadowCopyCookie)
+        domainAssemblyLoader.RegisterAssemblyOf<AssemblyLoader>()
+        domainAssemblyLoader.RegisterAssemblyOf<ExpectoTestRunner>()
+        domainAssemblyLoader.RegisterPathOf<ExpectoTestRunner>()
 
-        ()
+        ourAssemblyLoader.RegisterPathOf<ExpectoTestRunner>()
+        ourAssemblyLoader.RegisterPath(shadowCopyCookie.TargetLocation)
+        ourAssemblyLoader.RegisterPath(assemblyDir)
+        domainAssemblyLoader.RegisterPath(assemblyDir)
+
+        // todo: remove
+        ourAssemblyLoader.RegisterPathOf<AssemblyLoader>()
+
+        runner <- domainCookie.CreateAndUnwrapFrom<ExpectoTestRunner>()
+        runner.Run(x.Server, TaskExecutor.Configuration, TaskExecutor.Logger, node, shadowCopyCookie, assemblyTask)
 
     static member val RunnerInfo =
         let location = typeof<RemoteTask>.Assembly.Location
         let dir = Path.GetDirectoryName(location)
+        
         RemoteTaskRunnerInfo(expectoId, typeof<ExpectoTaskRunner>, [| dir |])
 
