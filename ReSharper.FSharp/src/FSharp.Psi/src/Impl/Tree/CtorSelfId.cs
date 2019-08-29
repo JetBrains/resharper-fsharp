5cbce6ee49629d06ffdeb22f4598c54a10fb10a4
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-08-29 21:21:43 +0300

    Fix ctor self id type

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/Tree/CtorSelfId.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/Tree/CtorSelfId.cs
@@ -1,9 +1,16 @@
 using JetBrains.ReSharper.Plugins.FSharp.Psi.Tree;
+using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 {
   internal partial class CtorSelfId
   {
     public override IFSharpIdentifier NameIdentifier => (IFSharpIdentifier) Identifier;
+
+    public override IType Type =>
+      // FCS returns FSharpRef<TContainingType> for self identifiers.
+      GetContainingType() is ITypeElement typeElement
+        ? TypeFactory.CreateType(typeElement)
+        : TypeFactory.CreateUnknownType(Module);
   }
 }
