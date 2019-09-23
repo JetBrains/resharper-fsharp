c6a91ecfd1da997f46d15e8e7283ddfcecc441a9
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-09-23 17:54:09 +0300

    Don't create separate open statement import references

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/Tree/OpenStatement.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.ReSharper.Psi.Tree;
-
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
-{
-  internal partial class OpenStatement
-  {
-    public override ITokenNode IdentifierToken => ReferenceName?.Identifier as ITokenNode;
-  }
-}
