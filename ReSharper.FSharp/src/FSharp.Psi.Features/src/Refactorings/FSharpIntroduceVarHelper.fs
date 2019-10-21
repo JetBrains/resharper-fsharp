17938591f1dc86e91e1eeb13d51c655057f4c1c5
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-10-21 21:01:52 +0300

    Add basic name suggestions to Introduce Variable

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Refactorings/FSharpIntroduceVarHelper.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Refactorings/FSharpIntroduceVarHelper.fs
@@ -8,6 +8,10 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.ExtensionsAPI.Tree
+open JetBrains.ReSharper.Psi.Naming.Elements
+open JetBrains.ReSharper.Psi.Naming.Extentions
+open JetBrains.ReSharper.Psi.Naming.Impl
+open JetBrains.ReSharper.Psi.Naming.Settings
 open JetBrains.ReSharper.Psi.Pointers
 open JetBrains.ReSharper.Psi.Tree
 open JetBrains.ReSharper.Refactorings.IntroduceVariable
@@ -26,16 +30,37 @@ type FSharpIntroduceVariable(workflow, solution, driver) =
 
         let lineEnding = expr.GetLineEnding()
         let elementFactory = expr.CreateElementFactory()
+
+        let language = expr.Language
+        let sourceFile = expr.GetSourceFile()
+
+        let namingManager = solution.GetPsiServices().Naming
+        let namesCollection =
+            namingManager.Suggestion.CreateEmptyCollection(PluralityKinds.Unknown, language, true, sourceFile)
+
+        let entryOptions = EntryOptions(subrootPolicy = SubrootPolicy.Decompose, prefixPolicy = PredefinedPrefixPolicy.Remove)
+        namesCollection.Add(expr, entryOptions)
+
+        let settingsStore = expr.GetSettingsStoreWithEditorConfig()
+        let elementKind = NamedElementKinds.Locals
+        let descriptor = ElementKindOfElementType.LOCAL_VARIABLE
+        let namingRule =
+            namingManager.Policy.GetDefaultRule(sourceFile, language, settingsStore, elementKind, descriptor)
+
+        let suggestionOptions = SuggestionOptions(null, DefaultName = "foo")
+        let names = namesCollection.Prepare(namingRule, ScopeKind.Common, suggestionOptions).AllNames()
+        let name = if names.Count > 0 then names.[0] else "x"
+        
         use writeCookie = WriteLockCookie.Create(expr.IsPhysical())
 
         let replacedUsages =
             data.Usages
             |> Array.ofSeq
             |> Array.map (fun usage ->
-                let ref = elementFactory.CreateReferenceExpr("x")
+                let ref = elementFactory.CreateReferenceExpr(name)
                 ModificationUtil.ReplaceChild(usage, ref).As<ITreeNode>().CreateTreeElementPointer())
 
-        let letOrUseExpr = elementFactory.CreateLetBindingExpr("x", expr)
+        let letOrUseExpr = elementFactory.CreateLetBindingExpr(name, expr)
 
         letOrUseExpr.SetInExpression(parentExpr) |> ignore
         let anchor = ModificationUtil.AddChildAfter(letOrUseExpr.Bindings.[0], NewLine(lineEnding))
@@ -44,15 +69,19 @@ type FSharpIntroduceVariable(workflow, solution, driver) =
         let letOrUseExpr = ModificationUtil.ReplaceChild(parentExpr, letOrUseExpr) :> ITreeNode
 
         let nodes =
-            replacedUsages
-            |> Array.map (fun pointer -> pointer.GetTreeNode())
-            |> Array.append [| letOrUseExpr.As<ILet>().Bindings.[0].HeadPattern |]
-        
-        let nameExpression = NameSuggestionsExpression(["xxx"; "yyy"])
+            let replacedNodes =
+                replacedUsages |> Array.map (fun pointer -> pointer.GetTreeNode())
+
+            [| letOrUseExpr.As<ILet>().Bindings.[0].HeadPattern :> ITreeNode |]
+            |> Array.append replacedNodes 
+
+        let nameExpression = NameSuggestionsExpression(names)
         let hotspotsRegistry = HotspotsRegistry(solution.GetPsiServices())
         hotspotsRegistry.Register(nodes, nameExpression)
-        
-        IntroduceVariableResult(hotspotsRegistry, letOrUseExpr.CreateTreeElementPointer())
+
+        let expr = letOrUseExpr :?> ILetLikeExpr
+
+        IntroduceVariableResult(hotspotsRegistry, expr.Bindings.[0].HeadPattern.As<ITreeNode>().CreateTreeElementPointer())
 
 
 type FSharpIntroduceVarHelper() =
