7f0574e8da183deb0852b4d2f4c4930d89fb4b89
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-10-24 17:17:15 +0300

    Fix for API changes (DefaultQuickDefinitionLanguageService)

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/LanguageService/FSharpQuickDefinitionService.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/LanguageService/FSharpQuickDefinitionService.fs
@@ -5,16 +5,17 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Util
 
 [<Language(typeof<FSharpLanguage>)>]
 type FSharpQuickDefinitionService() =
     inherit DefaultQuickDefinitionLanguageService()
 
-    override x.GetPresentableNode(node) =
+    override x.GetPresentableTreeRange(node) =
         match node with
         | :? ISynPat as pat ->
             let pat = skipIntermediatePatParents pat
             let binding = BindingNavigator.GetByHeadPattern(pat)
-            LetNavigator.GetByBinding(binding) :> _
+            TreeRange(LetNavigator.GetByBinding(binding)) :> _
 
-        | _ -> base.GetPresentableNode(node)
+        | _ -> base.GetPresentableTreeRange(node)
