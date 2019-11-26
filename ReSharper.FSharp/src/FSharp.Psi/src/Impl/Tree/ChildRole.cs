4213c36db54a5456713d9fa73e2b5baf4665725e
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2019-11-26 21:16:03 +0300

    Add common child roles for separators

---
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/Tree/ChildRole.cs
@@ -0,0 +1,13 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
+{
+  public static class ChildRole
+  {
+    public const short NONE = 0;
+
+    public const short FSHARP_COMMA = 1;
+    public const short FSHARP_AND = 2;
+    public const short FSHARP_SEMI = 3;
+
+    public const short LAST = 100;
+  }
+}
