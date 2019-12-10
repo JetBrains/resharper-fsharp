a297e7a2f7548329e27fc813526fbeb6f17197f8
Roma Mendaliev <reacheight8@gmail.com>
2019-12-10 13:20:41 +0300

    Change deletion of not last clauses

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
@@ -1,14 +1,20 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings.CommonErrors
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
+open JetBrains.ReSharper.Psi.ExtensionsAPI.Tree
+open JetBrains.ReSharper.Psi.Tree
 open JetBrains.ReSharper.Resources.Shell
 
 type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
     inherit FSharpQuickFixBase()
 
     let matchClause = warning.MatchClause
+
+    let isNotMatchClause (node: ITreeNode) =
+        node.NodeType != ElementType.MATCH_CLAUSE.As<NodeType>()
 
     override x.Text = "Remove never matching rule"
     override x.IsAvailable _ = isValid matchClause
@@ -23,6 +29,11 @@ type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
                 let last = getLastMatchingNodeAfter isInlineSpaceOrComment first
                 deleteChildRange first last
 
-        let first = getFirstMatchingNodeBefore isWhitespace matchClause
-        let last = getLastMatchingNodeAfter isInlineSpaceOrComment matchClause
-        deleteChildRange first last
+            let first = getFirstMatchingNodeBefore isWhitespace matchClause
+            let last = getLastMatchingNodeAfter isInlineSpaceOrComment matchClause
+            deleteChildRange first last
+
+        else
+            let first = matchClause
+            let last = getLastMatchingNodeAfter isNotMatchClause matchClause
+            deleteChildRange first last
