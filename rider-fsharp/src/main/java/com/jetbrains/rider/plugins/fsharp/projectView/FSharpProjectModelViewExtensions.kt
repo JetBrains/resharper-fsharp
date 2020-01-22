7b6c6f113bf330fb2f55f627d81c1c6160e86158
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-01-22 16:17:27 +0300

    Fix for API changes (ProjectModelViewExtensions)

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/projectView/FSharpProjectModelViewExtensions.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/projectView/FSharpProjectModelViewExtensions.kt
@@ -1,11 +1,14 @@
 package com.jetbrains.rider.plugins.fsharp.projectView
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.rider.projectView.ProjectModelViewExtensions
+import com.jetbrains.rider.projectView.ProjectModelViewHost
 import com.jetbrains.rider.projectView.nodes.ProjectModelNode
 
 class FSharpProjectModelViewExtensions(project: Project) : ProjectModelViewExtensions(project) {
-    override fun chooseBestProjectModelNode(nodes: List<ProjectModelNode>): ProjectModelNode? {
+    override fun getBestParentProjectModelNode(virtualFile: VirtualFile): ProjectModelNode? {
+        val nodes = ProjectModelViewHost.getInstance(project).getItemsByVirtualFile(virtualFile).toList()
         if (nodes.isEmpty() || !nodes[0].isFromFSharpProject()) return null
         return nodes.maxBy { it.getSortKey() }
     }
