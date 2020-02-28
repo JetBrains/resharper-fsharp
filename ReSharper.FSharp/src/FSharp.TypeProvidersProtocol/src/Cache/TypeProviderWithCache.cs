ecbf908da6ab6e13f1888fe6678ef32ab7fbae46
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-02-28 20:08:34 +0300

    wip

--- ReSharper.FSharp/src/FSharp.TypeProvidersProtocol/src/Cache/TypeProviderWithCache.cs
+++ ReSharper.FSharp/src/FSharp.TypeProvidersProtocol/src/Cache/TypeProviderWithCache.cs
@@ -3,44 +3,57 @@ using System.Reflection;
 using Microsoft.FSharp.Core.CompilerServices;
 using Microsoft.FSharp.Quotations;
 
-namespace JetBrains.ReSharper.Plugins.FSharp.Cache
+namespace JetBrains.ReSharper.Plugins.FSharp.TypeProvidersProtocol.Cache
 {
-  public class TypeProviderWithCache: ITypeProvider
+  public class TypeProviderWithCache : ITypeProvider
   {
     private readonly ITypeProvider myTypeProvider;
+    private IProvidedNamespace[] myProvidedNamespaces;
 
     public TypeProviderWithCache(ITypeProvider typeProvider)
     {
       myTypeProvider = typeProvider;
+      myTypeProvider.Invalidate += TypeProviderOnInvalidate;
     }
+
     public void Dispose()
     {
       myTypeProvider.Dispose();
     }
 
-    public IProvidedNamespace[] GetNamespaces()
-    {
-      throw new NotImplementedException();
-    }
+    public IProvidedNamespace[] GetNamespaces() =>
+      myProvidedNamespaces ?? (myProvidedNamespaces = myTypeProvider.GetNamespaces());
 
     public ParameterInfo[] GetStaticParameters(Type typeWithoutArguments)
     {
-      throw new NotImplementedException();
+      return myTypeProvider.GetStaticParameters(typeWithoutArguments);
     }
 
-    public Type ApplyStaticArguments(Type typeWithoutArguments, string[] typePathWithArguments, object[] staticArguments)
+    public Type ApplyStaticArguments(Type typeWithoutArguments, string[] typePathWithArguments,
+      object[] staticArguments)
     {
       return myTypeProvider.ApplyStaticArguments(typeWithoutArguments, typePathWithArguments, staticArguments);
     }
 
     public FSharpExpr GetInvokerExpression(MethodBase syntheticMethodBase, FSharpExpr[] parameters)
     {
-      throw new NotImplementedException();
+      return myTypeProvider.GetInvokerExpression(syntheticMethodBase, parameters);
     }
 
     public byte[] GetGeneratedAssemblyContents(Assembly assembly)
     {
-      throw new NotImplementedException();
+      return myTypeProvider.GetGeneratedAssemblyContents(assembly);
+    }
+
+    private void TypeProviderOnInvalidate(object sender, EventArgs e)
+    {
+      ClearCaches();
+      Invalidate?.Invoke(sender, e);
+    }
+
+    private void ClearCaches()
+    {
+      myProvidedNamespaces = null;
     }
 
     public event EventHandler Invalidate;
