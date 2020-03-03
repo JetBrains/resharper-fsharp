a96c8f9c31d622b3359dfb6d576fcc273bb580a9
Roma Mendaliev <reacheight8@gmail.com>
2020-03-03 19:14:49 +0300

    Add indenting rules for bindings and nested module declarations

--- ReSharper.FSharp/src/FSharp.Psi/src/CodeFormatter/FSharpCodeFormatterInfoProvider.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/CodeFormatter/FSharpCodeFormatterInfoProvider.cs
@@ -1,7 +1,10 @@
+using System.Linq;
 using JetBrains.Application.Settings;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Plugins.FSharp.Psi;
+using JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 using JetBrains.ReSharper.Psi.Impl.CodeStyle;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
@@ -12,8 +15,32 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
   {
     public FSharpFormatterInfoProvider(ISettingsSchema settingsSchema) : base(settingsSchema)
     {
+      var bindingAndModuleDeclIndentingRulesParameters = new[]
+      {
+        ("NestedModuleDeclaration", ElementType.NESTED_MODULE_DECLARATION, NestedModuleDeclaration.MODULE_MEMBER),
+        ("TopBinding", ElementType.TOP_BINDING, TopBinding.CHAMELEON_EXPR),
+        ("LocalBinding", ElementType.LOCAL_BINDING, LocalBinding.EXPR),
+      };
+
+      lock (this)
+      {
+        bindingAndModuleDeclIndentingRulesParameters
+          .ToList()
+          .ForEach(DescribeSimpleIndentingRule);
+      }
     }
 
     public override ProjectFileType MainProjectFileType => FSharpProjectFileType.Instance;
+
+    private void DescribeSimpleIndentingRule((string name, CompositeNodeType parentType, short childRole) parameters)
+    {
+      Describe<IndentingRule>()
+        .Name(parameters.name + "Indent")
+        .Where(
+          Parent().HasType(parameters.parentType),
+          Node().HasRole(parameters.childRole))
+        .Return(IndentType.External)
+        .Build();
+    }
   }
 }
