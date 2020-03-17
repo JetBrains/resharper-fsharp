4679074f209f4d47d69d7a52c407355a6d33b369
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-03-17 20:59:13 +0300

    Run problem analyzers inside bindings/members/do expressions in parallel

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpErrorsStage.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpErrorsStage.fs
@@ -1,6 +1,7 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Stages
 
 open System
+open System.Collections.Generic
 open JetBrains.Application.Settings
 open JetBrains.ReSharper.Daemon.Stages.Dispatcher
 open JetBrains.ReSharper.Feature.Services.Daemon
@@ -33,34 +34,58 @@ and FSharpErrorStageProcess
         analyzerDispatcher.Run(element, consumer)
 
     override x.Execute(committer) =
-        // todo: schedule separate processors for different top level nodes
         let consumer = FilteringHighlightingConsumer(daemonProcess.SourceFile, fsFile, settings)
-        fsFile.ProcessThisAndDescendants(Processor(x, consumer))
+        let globalProcessor = GlobalProcessor(x, consumer)
+        fsFile.ProcessThisAndDescendants(globalProcessor)
+
+        let fibers = daemonProcess.CreateFibers()
+        for node in globalProcessor.MemberDeclarations do
+            fibers.EnqueueJob(fun _ -> node.ProcessThisAndDescendants(Processor(x, consumer)))
+        fibers.Dispose()
+
         committer.Invoke(DaemonStageResult(consumer.Highlightings))
 
 
-and Processor(daemonProcess: FSharpDaemonStageProcessBase, consumer: IHighlightingConsumer) =
+and private Processor(daemonProcess: FSharpDaemonStageProcessBase, consumer: IHighlightingConsumer) =
     abstract InteriorShouldBeProcessed: ITreeNode -> bool
     default x.InteriorShouldBeProcessed _ = true
+
+    abstract ProcessAfterInterior: ITreeNode -> unit
+    default x.ProcessAfterInterior(element) =
+        match element with
+        | :? FSharpToken as token ->
+            if not (token.GetTokenType().IsWhitespace) then
+                token.Accept(daemonProcess, consumer)
+
+        | :? IFSharpTreeNode as fsTreeNode ->
+            fsTreeNode.Accept(daemonProcess, consumer)
+
+        | _ -> failwithf "Unreachable: %O" element
 
     interface IRecursiveElementProcessor with
         member x.InteriorShouldBeProcessed(element) =
             x.InteriorShouldBeProcessed(element)
+
+        member x.ProcessAfterInterior(element) = x.ProcessAfterInterior(element)
+        member x.ProcessBeforeInterior _ = ()
 
         member x.ProcessingIsFinished =
             match daemonProcess.DaemonProcess.InterruptFlag with
             | true -> OperationCanceledException() |> raise
             | _ -> false
 
-        member x.ProcessBeforeInterior _ = ()
+and private GlobalProcessor(daemonProcessor, consumer) =
+    inherit Processor(daemonProcessor, consumer)
 
-        member x.ProcessAfterInterior(element) =
-            match element with
-            | :? FSharpToken as token ->
-                if not (token.GetTokenType().IsWhitespace) then
-                    token.Accept(daemonProcess, consumer)
+    let shouldProcess (node: ITreeNode) =
+        not (node :? IBinding || node :? IMemberDeclaration || node :? IDo)
 
-            | :? IFSharpTreeNode as fsTreeNode ->
-                fsTreeNode.Accept(daemonProcess, consumer)
+    member val MemberDeclarations: JetHashSet<ITreeNode> = JetHashSet()
 
-            | _ -> daemonProcess.VisitNode(element, consumer)
+    override x.InteriorShouldBeProcessed(node) = shouldProcess node
+
+    override x.ProcessAfterInterior(node) =
+        if shouldProcess node then
+            base.ProcessAfterInterior(node)
+        else
+            x.MemberDeclarations.Add(node) |> ignore
