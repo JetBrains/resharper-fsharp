8e8baf49310d62e312857f091fd93b17c06d8642
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-04-03 15:10:24 +0300

    wip

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ReplaceWithAssignmentExpressionFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ReplaceWithAssignmentExpressionFix.fs
@@ -1,5 +1,6 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
+open FSharp.Compiler.SourceCodeServices
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings.CommonErrors
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Parsing
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
@@ -7,22 +8,42 @@ open JetBrains.ReSharper.Resources.Shell
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 open JetBrains.ReSharper.Psi.ExtensionsAPI
 open JetBrains.ReSharper.Plugins.FSharp.Util
+open JetBrains.ReSharper.Psi
 
 type ReplaceWithAssignmentExpressionFix(warning: UnitTypeExpectedWarning) =
     inherit FSharpQuickFixBase()
    
     let expr = warning.Expr.As<IBinaryAppExpr>()
-    override x.IsAvailable _ =        
-        if not (isValid expr) then false else
-            
-        // Now we can’t find out if the operator is '=', so we define it bypassing the grammar rules
-        // TODO: rewrite after https://youtrack.jetbrains.com/issue/RIDER-41848 fix
+    override x.IsAvailable _ =
+        if not (isValid expr) ||
+            // Now we can’t find out if the operator is '=', so we define it bypassing the grammar rules
+            // TODO: rewrite after https://youtrack.jetbrains.com/issue/RIDER-41848 fix
+            not (getTokenType expr.Operator.FirstChild == FSharpTokenType.EQUALS) then false else
+
         match expr.LeftArgument with
-        | :? IReferenceExpr
-        | :? IIndexerExpr -> getTokenType expr.Operator.FirstChild == FSharpTokenType.EQUALS
+        | :? IReferenceExpr as ref ->
+            let declaredElement = ref.Reference.Resolve().DeclaredElement
+            
+            match ref.Reference.GetFSharpSymbol() with
+            | :? FSharpField as field ->
+                field.IsMutable ||
+                match field.DeclaringEntity with
+                | Some (entity) -> entity.IsFSharpRecord && not (declaredElement :? ICompiledElement)
+                | None -> false
+
+            | :? FSharpMemberOrFunctionOrValue as memberOrFunctionOrValue ->
+                if memberOrFunctionOrValue.IsMember then memberOrFunctionOrValue.IsMutable else
+                if memberOrFunctionOrValue.FullType.IsFunctionType then false else
+                match declaredElement.GetDeclarations().[0] with
+                | :? IReferencePat as pat -> pat.Binding <> null
+                | _ -> false
+
+            | _ -> false
+
+        | :? IIndexerExpr -> true
         | _ -> false
         
-    override x.Text = "Replace with assignment expression"
+    override x.Text = "Replace with '<-' assignment"
     
     override x.ExecutePsiTransaction _ =
         use writeCookie = WriteLockCookie.Create(expr.IsPhysical())
