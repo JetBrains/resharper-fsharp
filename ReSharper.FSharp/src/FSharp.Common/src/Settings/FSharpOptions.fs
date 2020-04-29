79db5895fa6a5a75877bc34a1396753967cc1080
Saul Rennison <saul@users.noreply.github.com>
2020-04-29 16:39:42 +0100

    Add code insight to |> chains (#106)
    * Update InferredTypeCodeVisionProvider.fs
    
    * Split into PipeChainCodeVisionProvider
    
    * Create Multi line.fs.gold
    
    * Hook into the tooltip for the |> operator
    
    * Update Multi line.fs.gold
    
    * Add TypeHintsAdornmentProvider
    
    * Update TypeHintsAdornmentProvider.fs
    
    * Update TypeHintsAdornmentProvider.fs
    
    * Markups
    
    * Update TypeHintsAdornmentProvider.fs
    
    * Add more tests
    
    * Add new lines
    
    * Add setting
    
    * Update TypeHintAdornmentStage.fs
    
    * Add option to 'Hide when |> is on same line as argument'
    
    * Invalidate highlighting on type hint setting change
    
    * Update TypeHintAdornmentStage.fs
    
    * Update TypeHintAdornmentStage.fs
    
    * Use {HideSameLine:All} in tests
    
    * Reorganisation
    
    * Delete ReSharper.FSharp.sln.DotSettings.user
    
    * Code style
    
    * More code style changes
    
    * Markups

--- ReSharper.FSharp/src/FSharp.Common/src/Settings/FSharpOptions.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Settings/FSharpOptions.fs
@@ -1,12 +1,16 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Settings
 
+open System
 open System.Reflection
+open JetBrains.Application
 open JetBrains.Application.Settings
 open JetBrains.Application.UI.Options
+open JetBrains.Application.UI.Options.OptionsDialog.SimpleOptions
 open JetBrains.Lifetimes
 open JetBrains.ProjectModel
 open JetBrains.ProjectModel.DataContext
 open JetBrains.ProjectModel.Resources
+open JetBrains.ReSharper.Feature.Services.Daemon
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModel
 open JetBrains.UI.RichText
 
@@ -57,19 +61,60 @@ type FSharpScriptOptionsProvider(lifetime: Lifetime, settings: IContextBoundSett
     member val CustomDefines = settings.GetValueProperty(lifetime, fun s -> s.CustomDefines)
 
 
+
+module FSharpTypeHintOptions =
+    let [<Literal>] pipeReturnTypes = "Show return type hints in |> chains"
+
+    let [<Literal>] hideSameLinePipe = "Hide when |> is on same line as argument"
+
+
+[<SettingsKey(typeof<FSharpOptions>, "FSharpTypeHintOptions")>]
+type FSharpTypeHintOptions =
+    { [<SettingsEntry(true, FSharpTypeHintOptions.pipeReturnTypes); DefaultValue>]
+      mutable ShowPipeReturnTypes: bool
+
+      [<SettingsEntry(true, FSharpTypeHintOptions.hideSameLinePipe); DefaultValue>]
+      mutable HideSameLine: bool }
+
+
 [<OptionsPage("FSharpOptionsPage", "F#", typeof<ProjectModelThemedIcons.Fsharp>)>]
 type FSharpOptionsPage
         (lifetime: Lifetime, optionsPageContext, settings) as this =
     inherit FSharpOptionsPageBase(lifetime, optionsPageContext, settings)
 
-    do ignoreAll {
+    do
         this.AddHeader("Imports")
-        this.AddBoolOption((fun key -> key.EnableOutOfScopeCompletion), RichText(outOfScopeCompletion), null)
-        this.AddBoolOption((fun key -> key.TopLevelOpenCompletion), RichText(topLevelOpenCompletion), null)
+        this.AddBoolOption((fun key -> key.EnableOutOfScopeCompletion), RichText(outOfScopeCompletion), null) |> ignore
+        this.AddBoolOption((fun key -> key.TopLevelOpenCompletion), RichText(topLevelOpenCompletion), null) |> ignore
 
         this.AddHeader("Script editing")
-        this.AddComboEnum((fun key -> key.LanguageVersion), FSharpScriptOptions.languageVersion, FSharpLanguageVersion.toString)
+        this.AddComboEnum((fun key -> key.LanguageVersion), FSharpScriptOptions.languageVersion, FSharpLanguageVersion.toString) |> ignore
+
+        this.AddHeader("Type hints")
+        let showPipeReturnTypes = this.AddBoolOption((fun key -> key.ShowPipeReturnTypes), RichText(FSharpTypeHintOptions.pipeReturnTypes), null)
+        do
+            use _x = this.Indent()
+            [
+                this.AddBoolOption((fun key -> key.HideSameLine), RichText(FSharpTypeHintOptions.hideSameLinePipe), null)
+            ]
+            |> Seq.iter (fun checkbox ->
+                this.AddBinding(checkbox, BindingStyle.IsEnabledProperty, (fun key -> key.ShowPipeReturnTypes), id)
+            )
         
         this.AddHeader("FSharp.Compiler.Service options")
-        this.AddBoolOption((fun key -> key.BackgroundTypeCheck), RichText(backgroundTypeCheck), null)
-    }
+        this.AddBoolOption((fun key -> key.BackgroundTypeCheck), RichText(backgroundTypeCheck), null) |> ignore
+
+
+[<ShellComponent>]
+type FSharpTypeHintOptionsStore(lifetime: Lifetime, settingsStore: ISettingsStore, highlightingSettingsManager: HighlightingSettingsManager) =
+    do
+        let settingsKey = settingsStore.Schema.GetKey<FSharpTypeHintOptions>()
+
+        settingsStore.Changed.Advise(lifetime, fun args ->
+            let typeHintOptionChanged =
+                args.ChangedEntries
+                |> Seq.exists (fun changedEntry -> changedEntry.Parent = settingsKey)
+
+            if typeHintOptionChanged then
+                highlightingSettingsManager.SettingsChanged.Fire(Nullable<_>(false))
+        )
