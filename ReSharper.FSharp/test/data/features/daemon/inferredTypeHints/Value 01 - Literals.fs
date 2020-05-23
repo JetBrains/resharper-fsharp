91e6d5db6cf69c6222022a03d10908616e5dcd38
Saul Rennison <saul.rennison@gmail.com>
2020-05-23 19:35:12 +0100

    Add object expression and cast expression to tests

--- ReSharper.FSharp/test/data/features/daemon/inferredTypeHints/Value 01 - Literals.fs
+++ ReSharper.FSharp/test/data/features/daemon/inferredTypeHints/Value 01 - Literals.fs
@@ -4,4 +4,6 @@ let run () =
     let z = [1;2;3]
     let u = [|1;2;3|]
     let ts = 1, "hi", 10.
+    let disp = { new System.IDisposable with override __.Dispose () = () }
+    let dispB = disp :> obj
     y + ""
