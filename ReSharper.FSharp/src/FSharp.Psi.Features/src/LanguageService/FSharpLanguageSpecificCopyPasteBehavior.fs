0cbd166d65f1cfa244bfe2caca0732d31f4a97e2
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-05-28 12:47:08 +0300

    Disable smart copy assist handlers

---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/LanguageService/FSharpLanguageSpecificCopyPasteBehavior.fs
@@ -0,0 +1,10 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.LanguageService
+
+open JetBrains.ReSharper.Feature.Services.Clipboard
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Psi
+
+[<Language(typeof<FSharpLanguage>)>]
+type FSharpLanguageSpecificCopyPasteBehavior() =
+    interface ILanguageSpecificCopyPasteBehavior with
+        member x.AllowSmartCopyPaste _ = false
