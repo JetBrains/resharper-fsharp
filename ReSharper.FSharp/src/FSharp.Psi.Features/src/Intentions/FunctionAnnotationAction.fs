928112f476021fd593d04c039fea93dc63ab4d2a
Matt Constable <bonsty@gmail.com>
2020-06-02 08:43:51 +0100

    Action now works on parameter-less bindings.

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Intentions/FunctionAnnotationAction.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Intentions/FunctionAnnotationAction.fs
@@ -16,48 +16,19 @@ open JetBrains.Application.Settings
 [<ContextAction(Name = "AnnotateFunction", Group = "F#", Description = "Annotate function with parameter types and return type")>]
 type FunctionAnnotationAction(dataProvider: FSharpContextActionDataProvider) =
     inherit ContextActionBase()
-
-    let [<Literal>] opName = "FunctionAnnotationAction"
-    override x.Text = "Annotate function with parameter types and return type"
     
-    override x.IsAvailable _ =
-        let binding = dataProvider.GetSelectedElement<IBinding>()
-        if isNull binding then false else
-
-        match binding.HeadPattern.As<IParametersOwnerPat>() with
-        | null -> false
-        | namedPat ->
-        isNotNull namedPat.Identifier
-        
-    override x.ExecutePsiTransaction(_, _) =
-        let binding = dataProvider.GetSelectedElement<IBinding>()
-        use _writeCookie = WriteLockCookie.Create(binding.IsPhysical())
-        use _disableFormatter = new DisableCodeFormatter()
-        let settingsStore = binding.FSharpFile.GetSettingsStore()
-        let spaceBeforeColon = settingsStore.GetValue(fun (key : FSharpFormatSettingsKey) -> key.SpaceBeforeColon)
-        
-        match binding.HeadPattern.As<INamedPat>() with
-        | null -> null
-        | namedPat ->
-        let fsharpSymbolUse = namedPat.GetFSharpSymbolUse() 
-        match box(fsharpSymbolUse.Symbol) with
-        | null -> null
-        | methodSymbol ->
-        let fSharpFunction =
-            match methodSymbol with
-            | :? FSharpMemberOrFunctionOrValue as x -> x
-            | _ -> failwith "Expected function here"
-            
-        match namedPat.As<IParametersOwnerPat>() with
-        | null -> null
-        | parameterOwner ->
+    let annotateBindingParameters
+        (parameterOwner : IParametersOwnerPat)
+        (factory : IFSharpElementFactory)
+        (fSharpFunction : FSharpMemberOrFunctionOrValue)
+        (fsharpSymbolUse : FSharpSymbolUse)
+        (spaceBeforeColon : bool) =
         let typedTreeParameters =
             parameterOwner.Parameters
             |> Seq.zip fSharpFunction.CurriedParameterGroups
             |> Seq.toList
             
         // Annotate function parameters
-        let factory = namedPat.CreateElementFactory()
         for fSharpTypes, parameter in typedTreeParameters do
             let paramName =
                 match parameter with
@@ -86,21 +57,65 @@ type FunctionAnnotationAction(dataProvider: FSharpContextActionDataProvider) =
                 PsiModificationUtil.replaceWithCopy parameter parenPat
             | None -> ()
             
-        // Annotate function return type
-        if binding.ReturnTypeInfo |> isNull then
-            let returnTypeString =
-                fsharpSymbolUse.DisplayContext.WithShortTypeNames(true)
-                |> fSharpFunction.ReturnParameter.Type.Format
+    let annotateBindingReturnType
+        (namedPat : INamedPat)
+        (factory : IFSharpElementFactory)
+        (fSharpFunction : FSharpMemberOrFunctionOrValue)
+        (fsharpSymbolUse : FSharpSymbolUse)
+        (spaceBeforeColon : bool) =
+        let returnTypeString =
+            fsharpSymbolUse.DisplayContext.WithShortTypeNames(true)
+            |> fSharpFunction.ReturnParameter.Type.Format
+        
+        let afterWhitespace =
+            if spaceBeforeColon then
+                ModificationUtil.AddChildAfter(namedPat.LastChild, Whitespace(" ")) :> ITreeNode
+            else
+                namedPat.LastChild
+        let namedType =
+            returnTypeString
+            |> factory.CreateTypeUsage
+            |> factory.CreateReturnTypeInfo
+        ModificationUtil.AddChildAfter(afterWhitespace, namedType) |> ignore
+
+    let [<Literal>] opName = "FunctionAnnotationAction"
+    override x.Text = "Annotate function with parameter types and return type"
+    
+    override x.IsAvailable _ =
+        let binding = dataProvider.GetSelectedElement<IBinding>()
+        if isNull binding then false else
+
+        match binding.HeadPattern with
+        | :? IAsPat -> false
+        | :? INamedPat as namedPat -> isNotNull namedPat.Identifier
+        | _ -> false
+        
+    override x.ExecutePsiTransaction(_, _) =
+        let binding = dataProvider.GetSelectedElement<IBinding>()
+        use _writeCookie = WriteLockCookie.Create(binding.IsPhysical())
+        use _disableFormatter = new DisableCodeFormatter()
+        let settingsStore = binding.FSharpFile.GetSettingsStore()
+        let spaceBeforeColon = settingsStore.GetValue(fun (key : FSharpFormatSettingsKey) -> key.SpaceBeforeColon)
+        
+        match binding.HeadPattern.As<INamedPat>() with
+        | null -> null
+        | namedPat ->
+        let fsharpSymbolUse = namedPat.GetFSharpSymbolUse() 
+        match box(fsharpSymbolUse.Symbol) with
+        | null -> null
+        | methodSymbol ->
+        let fSharpFunction =
+            match methodSymbol with
+            | :? FSharpMemberOrFunctionOrValue as x -> x
+            | _ -> failwith "Expected function here"
             
-            let afterWhitespace =
-                if spaceBeforeColon then
-                    ModificationUtil.AddChildAfter(namedPat.LastChild, Whitespace(" ")) :> ITreeNode
-                else
-                    namedPat.LastChild
-            let namedType =
-                returnTypeString
-                |> factory.CreateTypeUsage
-                |> factory.CreateReturnTypeInfo
-            ModificationUtil.AddChildAfter(afterWhitespace, namedType) |> ignore
+        let factory = namedPat.CreateElementFactory()
+        let parametersOwner = namedPat.As<IParametersOwnerPat>()
+        // Not all bindings have parameters, e.g. `let x = 1`
+        if isNotNull parametersOwner then
+            annotateBindingParameters parametersOwner factory fSharpFunction fsharpSymbolUse spaceBeforeColon
+            
+        if binding.ReturnTypeInfo |> isNull then
+            annotateBindingReturnType namedPat factory fSharpFunction fsharpSymbolUse spaceBeforeColon
 
         null
