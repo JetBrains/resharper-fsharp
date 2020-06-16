6535b3215f650df6acc253524a6489a13fc696f2
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-06-16 17:36:33 +0300

    Inline var: add initial prototype

---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Refactorings/FSharpInlineVariable.fs
@@ -0,0 +1,95 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Refactorings
+
+open System
+open System.Collections.Generic
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open JetBrains.ReSharper.Psi.ExtensionsAPI.Tree
+open JetBrains.ReSharper.Psi.Resolve
+open JetBrains.ReSharper.Psi.Tree
+open JetBrains.ReSharper.Refactorings.Inline
+open JetBrains.ReSharper.Refactorings.InlineVar
+open JetBrains.ReSharper.Refactorings.InlineVar.RDAnalysis
+open JetBrains.ReSharper.Resources.Shell
+open JetBrains.Util
+
+type FSharpInlineHelper(driver) =
+    inherit InlineHelper(driver)
+
+    override x.GetContainingStatement(treeNode) = treeNode
+
+    override x.ReplaceTypeParameter(_, _, _) = raise (NotImplementedException())
+    override x.GetParameter2Argument(_, _) = raise (NotImplementedException())
+    override x.GetArgument2Infos(_, _, _) = raise (NotImplementedException())
+    override x.CanHaveSideEffects(_: IArgument): bool = raise (NotImplementedException())
+    override x.CanHaveSideEffects(_: IExpression): bool = raise (NotImplementedException())
+    override x.GetQualifierExpression _ = raise (NotImplementedException())
+    override x.InsertReturnValueTempVariable(_, _, _, _) = raise (NotImplementedException())
+    override x.InsertTempForQualifier(_, _, _, _, _) = raise (NotImplementedException())
+    override x.InsertTempVariableForAssignedValue(_, _, _) = raise (NotImplementedException())
+    override x.InsertTempForArgument(_, _, _, _, _, _) = raise (NotImplementedException())
+    override x.RemoveCastFromElement _ = raise (NotImplementedException())
+    override x.GetArgumentOwner(_, _) = raise (NotImplementedException())
+    override x.AllNotQualifiableReferences _ = raise (NotImplementedException())
+
+type FSharpInlineVariable(workflow, solution, driver) =
+    inherit InlineVarBase(workflow, solution, driver)
+
+    override x.InlineHelper = FSharpInlineHelper(driver) :> _
+
+    override x.ProcessReferenceWithContext(reference, _, info) =
+        let treeNode = reference.GetTreeNode()
+        use cookie = WriteLockCookie.Create(treeNode.IsPhysical())
+        let expr = info.InlinedMethodInfo.Expression :?> IFSharpExpression
+        let newExpr = ModificationUtil.ReplaceChild(treeNode, expr.Copy())
+        addParensIfNeeded newExpr |> ignore
+ 
+    override x.Ignore _ = false
+    override x.RemoveVariableDeclaration(decl) =
+        let refPat = decl.As<ILocalReferencePat>()
+        let binding = BindingNavigator.GetByHeadPattern(refPat.IgnoreParentParens())
+        let letExpr = LetOrUseExprNavigator.GetByBinding(binding)
+
+        use cookie = WriteLockCookie.Create(letExpr.IsPhysical())
+        replaceWithCopy letExpr letExpr.InExpression
+
+    override x.RemoveAssignment _ = ()
+
+
+type FSharpInlineVarAnalyser(workflow) =
+    inherit InlineVarAnalyserBase(workflow)
+
+    let mutable inlineExpr = null
+    let mutable inlineReferences = null
+
+    override val InlineAll = true with get, set
+
+    override x.References = inlineReferences
+    override x.Expression = inlineExpr
+    override x.AssignmentExpression = null
+
+    override x.Run(declaredElement, _, references) =
+        let refPat = declaredElement.As<ILocalReferencePat>()
+        if isNull refPat then Pair(false, "") else
+
+        let binding = BindingNavigator.GetByHeadPattern(refPat.IgnoreParentParens())
+        if isNull binding || isNull binding.Expression then Pair(false, "") else
+
+        let letExpr = LetOrUseExprNavigator.GetByBinding(binding)
+        if isNull letExpr || letExpr.Bindings.Count <> 1 then Pair(false, "") else
+
+        let hasWriteReferences (references: IList<IReference>) =
+            references |> Seq.exists (fun reference ->
+                let treeNode = reference.GetTreeNode()
+                let refExpr = treeNode.As<IReferenceExpr>()
+                if isNull refExpr then false else
+                isNotNull (SetExprNavigator.GetByLeftExpression(refExpr.IgnoreParentParens())))
+
+        if hasWriteReferences references then Pair(false, "") else
+
+        inlineExpr <- binding.Expression
+        inlineReferences <- List(references)
+
+        Pair(true, "")
---
+++ ReSharper.FSharp/test/src/FSharp.Tests/Refactorings/InlineVarTest.fs
@@ -0,0 +1,21 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features.Refactorings
+
+open JetBrains.ReSharper.Plugins.FSharp.Tests
+open JetBrains.ReSharper.Refactorings.Test.Common
+open NUnit.Framework
+
+[<FSharpTest>]
+type InlineVarTest() =
+    inherit InlineVarTestBase()
+
+    override x.RelativeTestDataPath = "features/refactorings/inlineVar"
+
+    override x.DoTest(lifetime, project) =
+        use cookie = FSharpRegistryUtil.AllowExperimentalFeaturesCookie.Create()
+        base.DoTest(lifetime, project)
+
+    [<Test>] member x.``Simple 01``() = x.DoNamedTest()
+    [<Test>] member x.``Simple 02``() = x.DoNamedTest()
+    [<Test>] member x.``Simple 03 - App``() = x.DoNamedTest()
+
+    [<Test>] member x.``Not available - Set 01``() = x.DoNamedTest()
