35631568e3e3d7fff9cbf78c296dc4fac704abde
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-07-01 23:11:43 +0300

    tests

---
+++ ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/AddParensToApplicationTest.fs
@@ -0,0 +1,25 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
+
+open JetBrains.ReSharper.FeaturesTestFramework.Intentions
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
+open JetBrains.ReSharper.Plugins.FSharp.Tests
+open NUnit.Framework
+
+[<FSharpTest>]
+type AddParensToApplicationTest() =
+    inherit QuickFixTestBase<AddParensToApplicationFix>()
+
+    override x.RelativeTestDataPath = "features/quickFixes/addParensToApplication"
+
+    [<Test>] member x.``Single application``() = x.DoNamedTest()
+    [<Test>] member x.``Multiply applications``() = x.DoNamedTest()
+    [<Test>] member x.``Application inside application``() = x.DoNamedTest()
+    [<Test>] member x.``First arg is application``() = x.DoNamedTest()
+
+[<FSharpTest>]
+type AddParensToApplicationAvailabilityTest() =
+    inherit QuickFixAvailabilityTestBase()
+
+    override x.RelativeTestDataPath = "features/quickFixes/addParensToApplication"
+
+    [<Test>] member x.``Not enough arguments - not available``() = x.DoNamedTest()
