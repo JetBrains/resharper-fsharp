d13df330191208c25316e76b73e7662398894d1c
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-07-08 16:01:29 +0300

    added base

---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/AddToModuleExtensionAttributeFix.fs
@@ -0,0 +1,37 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
+
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
+open JetBrains.ReSharper.Psi.ExtensionsAPI
+open JetBrains.ReSharper.Resources.Shell
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open FSharpAttributesUtil
+
+type AddToModuleExtensionAttributeFix(warning: ExtensionMemberInNonExtensionTypeWarning) =
+    inherit FSharpQuickFixBase()
+
+    let mutable moduleName = null
+
+    do moduleName <- LetModuleDeclNavigator.GetByAttribute(warning.Attr).GetContainingTypeDeclaration()
+    override x.Text = sprintf "Add 'Extension' attribute to '%s' module" moduleName.DeclaredName
+
+    override x.IsAvailable _ =
+        match moduleName with
+        | :? INamedModuleDeclaration -> false
+        | _ -> isValid warning.Attr
+
+    override x.ExecutePsiTransaction _ =
+        use writeCookie = WriteLockCookie.Create(warning.Attr.IsPhysical())
+        use disableFormatter = new DisableCodeFormatter()
+
+        let attribute = warning.Attr.CreateElementFactory().CreateAttribute("Extension")
+        match moduleName with
+        | :? IDeclaredModuleDeclaration as m ->
+            if m.AttributeLists.IsEmpty then addAttributesList m true
+            addAttribute m.AttributeLists.[0] attribute
+        | :? IObjectTypeDeclaration as o ->
+            let attributeList = getAttributeList o
+            addAttribute attributeList attribute
+        | _ -> ()
--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ToAbstractFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ToAbstractFix.fs
@@ -4,7 +4,7 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open FSharpAttributesUtil
 open JetBrains.ReSharper.Psi.ExtensionsAPI
 open JetBrains.ReSharper.Resources.Shell
 
@@ -12,17 +12,6 @@ type ToAbstractFix(error: NoImplementationGivenError) =
     inherit FSharpQuickFixBase()
 
     let typeDecl = error.TypeDecl
-
-    let getAttributeList typeDecl =
-        let typeDeclarationGroup = TypeDeclarationGroupNavigator.GetByTypeDeclaration(typeDecl)
-        if typeDeclarationGroup.TypeDeclarations.[0] == typeDecl then
-            let attributeLists = typeDeclarationGroup.AttributeLists
-            if not attributeLists.IsEmpty then attributeLists.[0] else
-            FSharpAttributesUtil.addAttributesList typeDeclarationGroup true; typeDeclarationGroup.AttributeLists.[0]
-        else
-            let attributeLists = typeDecl.AttributeLists
-            if not attributeLists.IsEmpty then attributeLists.[0] else
-            FSharpAttributesUtil.addAttributesList typeDecl false; typeDecl.AttributeLists.[0]
 
     override x.Text = "To abstract"
 
@@ -35,4 +24,4 @@ type ToAbstractFix(error: NoImplementationGivenError) =
 
         let attributeList = getAttributeList typeDecl
         let attribute = typeDecl.CreateElementFactory().CreateAttribute("AbstractClass")
-        FSharpAttributesUtil.addAttribute attributeList attribute
+        addAttribute attributeList attribute
