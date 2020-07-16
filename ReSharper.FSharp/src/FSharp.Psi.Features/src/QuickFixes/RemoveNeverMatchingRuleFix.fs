f298b83e02b1c5303388af2a82f962a2e4205fd6
Roma Mendaliev <reacheight8@gmail.com>
2020-07-16 14:43:10 +0300

    Enable formatter in RemoveNeverMatchingRule

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
@@ -3,17 +3,12 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings.CommonErrors
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
-open JetBrains.ReSharper.Psi.ExtensionsAPI
-open JetBrains.ReSharper.Psi.Tree
 open JetBrains.ReSharper.Resources.Shell
 
 type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
     inherit FSharpScopedQuickFixBase()
 
     let matchClause = warning.MatchClause
-
-    let isNotMatchClause (node: ITreeNode) =
-        not (node :? IMatchClause)
 
     override x.Text = "Remove never matching rule"
     override x.IsAvailable _ = isValid matchClause
@@ -22,7 +17,7 @@ type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
 
     override x.ExecutePsiTransaction _ =
         use writeLock = WriteLockCookie.Create(matchClause.IsPhysical())
-        use disableFormatter = new DisableCodeFormatter()
+        use enableFormatter = FSharpRegistryUtil.AllowFormatterCookie.Create()
 
         if isLastChild matchClause then
             let matchClauseOwner = MatchClauseListOwnerNavigator.GetByClause(matchClause)
@@ -31,11 +26,5 @@ type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
                 let last = getLastMatchingNodeAfter isInlineSpaceOrComment first
                 deleteChildRange first last
 
-            let first = getFirstMatchingNodeBefore isWhitespace matchClause
-            let last = getLastMatchingNodeAfter isInlineSpaceOrComment matchClause
-            deleteChildRange first last
-
-        else
-            let first = matchClause
-            let last = getLastMatchingNodeAfter isNotMatchClause matchClause
-            deleteChildRange first last
+        let last = getLastMatchingNodeAfter isInlineSpaceOrComment matchClause
+        deleteChildRange matchClause last
