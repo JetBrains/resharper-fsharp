c0ab897a108dfa2b956acb42dcdc53092913036c
Roma Mendaliev <reacheight8@gmail.com>
2020-07-16 14:43:55 +0300

    Enable formatter in AddMatchAllClause

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/AddMatchAllClauseFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/AddMatchAllClauseFix.fs
@@ -5,7 +5,6 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
-open JetBrains.ReSharper.Psi.ExtensionsAPI
 open JetBrains.ReSharper.Resources.Shell
 
 [<RequireQualifiedAccess>]
@@ -31,20 +30,16 @@ type AddMatchAllClauseFix(expr: IMatchExpr, generatedExpr: GeneratedClauseExpr) 
     override x.ExecutePsiTransaction _ =
         use writeCookie = WriteLockCookie.Create(expr.IsPhysical())
         let factory = expr.CreateElementFactory()
-        use disableFormatter = new DisableCodeFormatter()
+        use enableFormatter = FSharpRegistryUtil.AllowFormatterCookie.Create()
 
-        let clauses = expr.Clauses
         let isSingleLineMatch = expr.IsSingleLine
 
         let addToNewLine = not isSingleLineMatch // todo: cover more cases
-        let indent = if addToNewLine then clauses.Last().Indent else expr.Indent
 
         let clause = 
             addNodesAfter expr.LastChild [
                 if addToNewLine then
                     NewLine(expr.GetLineEnding())
-                    if indent > 0 then
-                        Whitespace(indent)
                 else
                     Whitespace()
                 factory.CreateMatchClause()
