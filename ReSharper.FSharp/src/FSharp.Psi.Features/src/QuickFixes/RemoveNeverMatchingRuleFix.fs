796c3682bfd669a488aff9a98c8ee38afbbe4844
Roma Mendaliev <reacheight8@gmail.com>
2020-07-20 11:41:34 +0300

    Make RemoveNeverMatchingRuleFix scoped

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/RemoveNeverMatchingRuleFix.fs
@@ -1,30 +1,47 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
+open JetBrains.ReSharper.Feature.Services.Intentions.Scoped
+open JetBrains.ReSharper.Feature.Services.Intentions.Scoped.Actions
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings.CommonErrors
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Resources.Shell
 
 type RemoveNeverMatchingRuleFix(warning: RuleNeverMatchedWarning) =
-    inherit FSharpScopedQuickFixBase()
+    inherit FSharpQuickFixBase()
 
-    let matchClause = warning.MatchClause
-
-    override x.Text = "Remove never matching rule"
-    override x.IsAvailable _ = isValid matchClause
-
-    override x.TryGetContextTreeNode() = matchClause :> _
-
-    override x.ExecutePsiTransaction _ =
-        use writeLock = WriteLockCookie.Create(matchClause.IsPhysical())
-        use enableFormatter = FSharpRegistryUtil.AllowFormatterCookie.Create()
-
-        if isLastChild matchClause then
-            let matchClauseOwner = MatchClauseListOwnerNavigator.GetByClause(matchClause)
+    let removeMatchClause (clause : IMatchClause) =
+        if isLastChild clause then
+            let matchClauseOwner = MatchClauseListOwnerNavigator.GetByClause(clause)
             if isNotNull matchClauseOwner && isInlineSpaceOrComment matchClauseOwner.NextSibling then
                 let first = matchClauseOwner.NextSibling
                 let last = getLastMatchingNodeAfter isInlineSpaceOrComment first
                 deleteChildRange first last
 
-        let last = getLastMatchingNodeAfter isInlineSpaceOrComment matchClause
-        deleteChildRange matchClause last
+        let last = getLastMatchingNodeAfter isInlineSpaceOrComment clause
+        deleteChildRange clause last
+
+    override x.Text = "Remove never matching rule"
+    override x.IsAvailable _ = isValid warning.MatchClause
+
+    override x.ExecutePsiTransaction _ =
+        let clause = warning.MatchClause
+        use enableFormatter = FSharpRegistryUtil.AllowFormatterCookie.Create()
+        use writeLock = WriteLockCookie.Create(clause.IsPhysical())
+        removeMatchClause clause
+
+    interface IHighlightingsSetScopedAction with
+        member x.ScopedText = x.Text
+        member x.FileCollectorInfo = FileCollectorInfo.Default
+
+        member x.ExecuteAction(highlightingInfos, _, _) =
+            use enableFormatter = FSharpRegistryUtil.AllowFormatterCookie.Create()
+
+            for highlightingInfo in highlightingInfos do
+                match highlightingInfo.Highlighting.As<RuleNeverMatchedWarning>() with
+                | null -> ()
+                | warning ->
+                    let clause = warning.MatchClause
+                    use writeLock = WriteLockCookie.Create(clause.IsPhysical())
+                    removeMatchClause clause
+            null
