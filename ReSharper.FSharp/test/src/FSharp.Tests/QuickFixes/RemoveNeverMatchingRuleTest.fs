3dd9dc188d5cc747bbd3e9a66af64d2db7c6e99f
Roma Mendaliev <reacheight8@gmail.com>
2020-07-24 11:56:00 +0300

    Add tests for scoped never matching rule fix

--- ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/RemoveNeverMatchingRuleTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/RemoveNeverMatchingRuleTest.fs
@@ -19,3 +19,6 @@ type RemoveNeverMatchingRuleTest() =
     [<Test>] member x.``Match expr - Multiple rules 04 - First clause on a line``() = x.DoNamedTest()
     [<Test>] member x.``Match lambda expr - Single rule 01``() = x.DoNamedTest()
     [<Test>] member x.``Try with expr - Single rule 01``() = x.DoNamedTest()
+
+    [<Test; ExecuteScopedQuickFixInFile>] member x.``Scoped 01 - Multiple match exprs``() = x.DoNamedTest()
+    [<Test; ExecuteScopedQuickFixInFile>] member x.``Scoped 02 - Match expr and Try with expr``() = x.DoNamedTest()
