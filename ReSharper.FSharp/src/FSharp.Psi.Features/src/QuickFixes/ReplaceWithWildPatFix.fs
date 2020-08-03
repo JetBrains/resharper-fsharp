a9cc0828155407ef83a7ffd3a862d67819d75517
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-08-03 19:29:57 +0300

    added scope apply

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ReplaceWithWildPatFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/ReplaceWithWildPatFix.fs
@@ -1,5 +1,8 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
+open JetBrains.ReSharper.Feature.Services.Intentions.Scoped
+open JetBrains.ReSharper.Feature.Services.Intentions.Scoped.Actions
+open JetBrains.ReSharper.Feature.Services.Intentions.Scoped.Scopes
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
@@ -10,7 +13,34 @@ open JetBrains.ReSharper.Resources.Shell
 type ReplaceWithWildPatFix(pat: INamedPat) =
     inherit FSharpQuickFixBase()
 
+    let [<Literal>] fixText = "Replace with '_'"
     let pat = pat.As<IReferencePat>()
+
+    let replaceWithWildPat (pat: IReferencePat) =
+        replace pat (pat.GetFSharpLanguageService().CreateElementFactory(pat.GetPsiModule()).CreateWildPat())
+
+    let getPatOwner (pat: INamedPat) =
+        let pat = pat.IgnoreParentParens()
+        if isNotNull (AttribPatNavigator.GetByPattern(pat)) then None else
+
+        let typedPat = TypedPatNavigator.GetByPattern(pat).IgnoreParentParens()
+        if isNotNull (AttribPatNavigator.GetByPattern(typedPat)) then None else
+
+        Some(skipIntermediatePatParents pat |> getParent)
+        
+    let isAvailable (pat: IReferencePat) =
+        isValid pat &&
+        let parent = getPatOwner pat
+        match parent with
+        | None -> false
+        | Some node ->
+            node :? IBinding ||
+            node :? IMatchClause ||
+            node :? ILambdaExpr ||
+            node :? IMemberParamsDeclaration &&
+            (node.Parent :? IMemberDeclaration || node.Parent :? IMemberConstructorDeclaration)
+
+    let patOwner = getPatOwner pat
 
     new (warning: UnusedValueWarning) =
         ReplaceWithWildPatFix(warning.Pat)
@@ -18,25 +48,35 @@ type ReplaceWithWildPatFix(pat: INamedPat) =
     new (error: VarBoundTwiceError) =
         ReplaceWithWildPatFix(error.Pat)
 
-    override x.Text = "Replace with '_'"
+    override x.Text = fixText
 
-    override x.IsAvailable _ =
-        isValid pat &&
-
-        let pat = pat.IgnoreParentParens()
-        if isNotNull (AttribPatNavigator.GetByPattern(pat)) then false else
-
-        let typedPat = TypedPatNavigator.GetByPattern(pat).IgnoreParentParens()
-        if isNotNull (AttribPatNavigator.GetByPattern(typedPat)) then false else
-
-        let node = skipIntermediatePatParents pat |> getParent
-        node :? IBinding ||
-        node :? IMatchClause ||
-        node :? ILambdaParametersList ||
-        node :? IMemberParamsDeclaration &&
-                (node.Parent :? IMemberDeclaration || node.Parent :? IMemberConstructorDeclaration)
+    override x.IsAvailable _ = isAvailable pat
 
     override x.ExecutePsiTransaction _ =
         use writeLock = WriteLockCookie.Create(pat.IsPhysical())
         use disableFormatter = new DisableCodeFormatter()
-        replace pat (pat.GetFSharpLanguageService().CreateElementFactory(pat.GetPsiModule()).CreateWildPat())
+        replaceWithWildPat pat
+
+    interface IHighlightingsSetScopedAction with
+        member x.ScopedText = fixText
+        member x.FileCollectorInfo =
+            match patOwner with
+            | None -> FileCollectorInfo.Default
+            | Some node -> 
+                FileCollectorInfo.WithThisAndContainingLocalScopes(LocalScope(node, "signature", "signature"))
+
+        member x.ExecuteAction(highlightingInfos, _, _) =
+            use writeLock = WriteLockCookie.Create(true)
+            use disableFormatter = new DisableCodeFormatter()
+
+            for highlightingInfo in highlightingInfos do
+                match highlightingInfo.Highlighting.As<UnusedValueWarning>() with
+                | null -> ()
+                | warning ->
+                    let pat = warning.Pat.As<IReferencePat>()
+                    let isAvailable =
+                        match pat with
+                        | null -> false
+                        | pat -> isAvailable pat
+                    if isAvailable then replaceWithWildPat pat
+            null
