90550a2cac2cdd1d37a28a1e1b0093c7838ee7f6
Alex Berezhnykh <berejnih.alex2011@yandex.ru>
2020-08-10 19:41:22 +0300

    added FSharpQuickFixTestBase

--- ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/NamespaceToModuleTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/NamespaceToModuleTest.fs
@@ -1,17 +1,12 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
 
-open JetBrains.ReSharper.FeaturesTestFramework.Intentions
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
-open JetBrains.ReSharper.Plugins.FSharp.Tests
 open NUnit.Framework
 
-[<FSharpTest>]
 type NamespaceToModuleTest() =
-    inherit QuickFixTestBase<NamespaceToModuleFix>()
+    inherit FSharpQuickFixTestBase<NamespaceToModuleFix>()
 
     override x.RelativeTestDataPath = "features/quickFixes/namespaceToModule"
-    
-    override x.OnQuickFixNotAvailable(_, _) = Assert.Fail(ErrorText.NotAvailable);
 
     [<Test>] member x.``Simple``() = x.DoNamedTest()
     [<Test; NotAvailable>] member x.``Global namespace``() = x.DoNamedTest()
