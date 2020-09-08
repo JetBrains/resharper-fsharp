89a508ba5297fe3e89cd8475d888af5d410c79e4
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-09-08 19:16:10 +0300

    Use formatter settings in ListConsPatAnalyzer

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/SimplifyListConsPatFix.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/QuickFixes/SimplifyListConsPatFix.fs
@@ -1,10 +1,13 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
+open JetBrains.Application.Settings
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings.Errors
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
+open JetBrains.ReSharper.Psi.Tree
 open JetBrains.ReSharper.Psi.ExtensionsAPI.Tree
 open JetBrains.ReSharper.Resources.Shell
 
@@ -18,11 +21,14 @@ type SimplifyListConsPatFix(warning: ConsWithEmptyListPatWarning) =
     override x.IsAvailable _ =
         isValid pat && isValid pat.HeadPattern &&
 
-        let tailPat = pat.TailPattern.As<IListPat>()
-        isValid tailPat && isNotNull tailPat.LeftBracket
+        let listPat = pat.TailPattern.As<IListPat>()
+        isValid listPat && isNotNull listPat.LeftBracket && isNotNull listPat.RightBracket
 
     override x.ExecutePsiTransaction _ =
         use writeLock = WriteLockCookie.Create(pat.IsPhysical())
+
+        let settings = pat.GetSettingsStoreWithEditorConfig()
+        let addSpaces = settings.GetValue(fun (key: FSharpFormatSettingsKey) -> key.SpaceAroundDelimiter)
 
         let listPat = pat.TailPattern :?> IListPat
 
@@ -30,6 +36,21 @@ type SimplifyListConsPatFix(warning: ConsWithEmptyListPatWarning) =
         | :? Whitespace as ws -> ModificationUtil.DeleteChild(ws)
         | _ -> ()
 
-        ModificationUtil.AddChildAfter(listPat.LeftBracket, pat.HeadPattern) |> ignore
+        let innerListPat = ModificationUtil.AddChildAfter(listPat.LeftBracket, pat.HeadPattern)
+
+        let nextSibling = innerListPat.NextSibling
+        if nextSibling != listPat.RightBracket && not (nextSibling :? NewLine) then
+            ModificationUtil.AddChildAfter(innerListPat, Whitespace()) |> ignore
+
+        let rightBracketPrevSibling = listPat.RightBracket.PrevSibling
+
+        if addSpaces then
+            ModificationUtil.AddChildBefore(innerListPat, Whitespace()) |> ignore
+            if not (rightBracketPrevSibling :? Whitespace) && not (rightBracketPrevSibling :? NewLine) then
+                ModificationUtil.AddChildBefore(listPat.RightBracket, Whitespace()) |> ignore
+        else
+            if rightBracketPrevSibling :? Whitespace then
+                ModificationUtil.DeleteChild(rightBracketPrevSibling)
+
         ModificationUtil.AddChildBefore(pat, listPat) |> ignore
         ModificationUtil.DeleteChild(pat)
--- ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/SimplifyListConsPatTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/SimplifyListConsPatTest.fs
@@ -1,6 +1,8 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
 
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
+open JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
+open JetBrains.ReSharper.TestFramework
 open NUnit.Framework
 
 type SimplifyListConsPatTest() =
@@ -8,6 +10,17 @@ type SimplifyListConsPatTest() =
 
     override x.RelativeTestDataPath = "features/quickFixes/simplifyListConsPat"
 
+    [<Test>] member x.``Test - Comment 01``() = x.DoNamedTest()
+    [<Test>] member x.``Test - Comment 02 - Add space``() = x.DoNamedTest()
+
+    [<TestSetting(typeof<FSharpFormatSettingsKey>, "SpaceAroundDelimiter", "false")>]
+    [<Test>] member x.``Test - Comment 03 - Remove space``() = x.DoNamedTest()
+
+    [<TestSetting(typeof<FSharpFormatSettingsKey>, "SpaceAroundDelimiter", "false")>]
+    [<Test>] member x.``Test - No space 01``() = x.DoNamedTest()
+
+    [<TestSetting(typeof<FSharpFormatSettingsKey>, "SpaceAroundDelimiter", "false")>]
+    [<Test>] member x.``Test - No space 02``() = x.DoNamedTest()
+
     [<Test>] member x.``Test 01``() = x.DoNamedTest()
     [<Test>] member x.``Test 02 - Space``() = x.DoNamedTest()
-    [<Test>] member x.``Test 03 - Comment``() = x.DoNamedTest()
