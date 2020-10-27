7bee0cd5edbf25efb36b7cf61657543b1330bd97
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-10-27 23:12:29 +0300

    Make FSharpQuickDocRequest component

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/QuickDoc/FSharpQuickDocRequest.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/QuickDoc/FSharpQuickDocRequest.fs
@@ -1,7 +1,9 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.QuickDock
 
+open JetBrains.ProjectModel
 open JetBrains.ReSharper.Host.Features.Daemon.Tooltips.Request
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 
+[<SolutionComponent>]
 type FSharpQuickDocRequest() =
     inherit HighlightingBasedQuickDocTooltipRequest<FSharpIdentifierHighlighting>()
