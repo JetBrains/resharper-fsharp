cd9b0b479158e260338080ba46b0d2104bf3eaa9
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2020-11-23 15:21:56 +0300

    Workaround wrong FCS symbols for optional method parameter declarations

--- ReSharper.FSharp/test/src/FSharp.Tests/Parsing/FSharpParserTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Parsing/FSharpParserTest.fs
@@ -368,10 +368,11 @@ type FSharpParserTest() =
 
     [<Test>] member x.``Type member - Member 01 - Wild self id``() = x.DoNamedTest()
 
+    [<Test>] member x.``Type member - Member - Method - Parameters - Optional 01``() = x.DoNamedTest()
+    [<Test>] member x.``Type member - Member - Method - Parameters - Optional 02``() = x.DoNamedTest()
+    [<Test>] member x.``Type member - Member - Method - Parameters - Optional 03 - Private``() = x.DoNamedTest()
     [<Test>] member x.``Type member - Member - Method - Parameters 01``() = x.DoNamedTest()
     [<Test>] member x.``Type member - Member - Method - Parameters 02``() = x.DoNamedTest()
-    [<Test>] member x.``Type member - Member - Method - Parameters 03 - Optional``() = x.DoNamedTest()
-    [<Test>] member x.``Type member - Member - Method - Parameters 04 - Optional``() = x.DoNamedTest()
 
     [<Test>] member x.``Type member - Member - Method - Curried Parameters 01``() = x.DoNamedTest()
 
