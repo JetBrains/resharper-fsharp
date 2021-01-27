c5920048a66b3a06f79c7b75440c2bb63f360df2
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-01-27 23:30:16 +0300

    Add test for Highlight usage in file
    Covers fix in the product

---
+++ ReSharper.FSharp/test/src/FSharp.Tests/Actions/HighlightUsagesTest.fs
@@ -0,0 +1,15 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
+
+open JetBrains.ReSharper.FeaturesTestFramework.Highlighting
+open JetBrains.ReSharper.Plugins.FSharp.Tests
+open NUnit.Framework
+
+[<FSharpTest>]
+type HighlightUsagesTest() =
+    inherit HighlightActionTestBase()
+
+    override x.RelativeTestDataPath = "actions/highlightUsages"
+    override this.HighlightActionName = "HighlightUsages"
+
+    [<Test>] member x.``Let declaration 01``() = x.DoNamedTest()
+    [<Test>] member x.``Let declaration 02 - Union case repr``() = x.DoNamedTest()
