e7c392874f84ee276274fdc2a105a8c83bd3404c
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-03-07 17:26:30 +0300

    UseWildSelfIdFix: disable formatter in object expressions (#239)

---
+++ ReSharper.FSharp/test/src/FSharp.Tests/QuickFixes/UseWildSelfIdTest.fs
@@ -0,0 +1,14 @@
+﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features
+
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
+open JetBrains.ReSharper.Plugins.FSharp.Tests
+open NUnit.Framework
+
+[<FSharpTest>]
+type UseWildSelfIdTest() =
+    inherit FSharpQuickFixTestBase<UseWildSelfIdFix>()
+
+    override x.RelativeTestDataPath = "features/quickFixes/useWildSelfId"
+
+    [<Test>] member x.``Simple 01``() = x.DoNamedTest()
+    [<Test>] member x.``Object expression 01``() = x.DoNamedTest()
