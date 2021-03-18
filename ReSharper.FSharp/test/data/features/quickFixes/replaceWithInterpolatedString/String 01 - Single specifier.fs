25dd9bc542d01ec744e75c61afa4f2f271eebbff
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-03-18 16:48:34 +0300

    To interpolated string: initial work on removing specifiers

--- ReSharper.FSharp/test/data/features/quickFixes/replaceWithInterpolatedString/String 01 - Single specifier.fs
+++ ReSharper.FSharp/test/data/features/quickFixes/replaceWithInterpolatedString/String 01 - Single specifier.fs
@@ -1,3 +1,7 @@
 module Module
 
 let s = sprintf {caret}"%d" 10
+let _ = sprintf "%d" s.Length
+
+let i = 10
+let _ = sprintf "%d" i
--- ReSharper.FSharp/test/data/features/quickFixes/replaceWithInterpolatedString/String 02 - Many specifiers.fs
+++ ReSharper.FSharp/test/data/features/quickFixes/replaceWithInterpolatedString/String 02 - Many specifiers.fs
@@ -2,3 +2,4 @@ module Module
 
 let foo = "foo"
 let s = sprintf {caret}"%d %s" 10 foo
+let _ = sprintf "%d %s" s.Length foo
