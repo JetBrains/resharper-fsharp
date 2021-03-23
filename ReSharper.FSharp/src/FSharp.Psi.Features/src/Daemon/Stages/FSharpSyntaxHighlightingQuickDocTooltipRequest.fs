b5a28e8fc3652f6d143901b219d95ae7e0d3a9c7
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-03-23 20:08:22 +0300

    Tooltips: fix quick doc request component

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpSyntaxHighlightingQuickDocTooltipRequest.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpSyntaxHighlightingQuickDocTooltipRequest.fs
@@ -1,15 +1,14 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Stages
 
-open JetBrains.Application.DataContext
 open JetBrains.ProjectModel
 open JetBrains.ReSharper.Host.Features.Daemon.Tooltips.Request
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.TextControl.DocumentMarkup
 
 [<SolutionComponent>]
-type FSharpSyntaxHighlightingQuickDocTooltipRequest =
+type FSharpSyntaxHighlightingQuickDocTooltipRequest() =
     interface IRiderQuickDocTooltipRequest with
-        member x.ShouldShowToolTipAsQuickDoc(highlighter: IHighlighter, context: IDataContext) =
+        member x.ShouldShowToolTipAsQuickDoc(highlighter: IHighlighter, _) =
             if highlighter.UserData :? FSharpIdentifierHighlighting then
                 RiderTooltipAction.SHOW_AS_QUICK_DOC
             else
