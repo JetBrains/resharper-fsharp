ab4c6114e87cda7a96415d136577272255497d8c
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2021-03-23 17:34:31 +0300

    Tooltips: move quick doc tooltip request into F# plugin (#247)

---
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpSyntaxHighlightingQuickDocTooltipRequest.fs
@@ -0,0 +1,18 @@
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Stages
+
+open JetBrains.Application.DataContext
+open JetBrains.ProjectModel
+open JetBrains.ReSharper.Host.Features.Daemon.Tooltips.Request
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
+open JetBrains.TextControl.DocumentMarkup
+
+[<SolutionComponent>]
+type FSharpSyntaxHighlightingQuickDocTooltipRequest =
+    interface IRiderQuickDocTooltipRequest with
+        member x.ShouldShowToolTipAsQuickDoc(highlighter: IHighlighter, context: IDataContext) =
+            if highlighter.UserData :? FSharpIdentifierHighlighting then
+                RiderTooltipAction.SHOW_AS_QUICK_DOC
+            else
+                RiderTooltipAction.UNSURE
+
+        member x.Priority = 2000
