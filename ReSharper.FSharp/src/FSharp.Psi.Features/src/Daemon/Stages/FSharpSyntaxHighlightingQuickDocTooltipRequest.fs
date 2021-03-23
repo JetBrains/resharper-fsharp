b279d182958feb67538367ff15c07a343d8ffbcf
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2021-03-23 16:40:13 +0300

    Add an attribute

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpSyntaxHighlightingQuickDocTooltipRequest.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Daemon/Stages/FSharpSyntaxHighlightingQuickDocTooltipRequest.fs
@@ -1,10 +1,12 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Stages
 
 open JetBrains.Application.DataContext
+open JetBrains.ProjectModel
 open JetBrains.ReSharper.Host.Features.Daemon.Tooltips.Request
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.TextControl.DocumentMarkup
 
+[<SolutionComponent>]
 type FSharpSyntaxHighlightingQuickDocTooltipRequest =
     interface IRiderQuickDocTooltipRequest with
         member x.ShouldShowToolTipAsQuickDoc(highlighter: IHighlighter, context: IDataContext) =
