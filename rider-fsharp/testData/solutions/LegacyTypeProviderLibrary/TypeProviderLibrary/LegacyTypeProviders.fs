fa803d90205694d4acfd12050620d702c876f605
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-04-20 21:15:33 +0300

    add test

---
+++ rider-fsharp/testData/solutions/LegacyTypeProviderLibrary/TypeProviderLibrary/LegacyTypeProviders.fs
@@ -0,0 +1,18 @@
+﻿module Library
+
+open FSharp.Text.RegexProvider
+open FSharp.Data
+
+type PhoneRegex = Regex< @"(?<AreaCode>^\d{3})-(?<PhoneNumber>\d{3}-\d{4}$)" >
+let _: string = PhoneRegex().TypedMatch("425-123-2345").AreaCode.Value
+
+
+type Xml = XmlProvider<"Sample.lxml",
+                       SampleIsList = true,
+                       Global = true,
+                       InferTypesFromValues = false>
+let _: Xml.Excitation =
+    Xml.Excitation(name = Some "",
+                   ``type`` = "",
+                   excite = Some "",
+                   primitives = Some (Xml.Primitives(Xml.Box(xElement = null))))
