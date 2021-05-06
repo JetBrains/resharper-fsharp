6a8e0ac4de9a95c02c09d1c0144cc5be3d3d4bef
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-05-06 16:53:02 +0300

    Formatter: blank lines in binding/type groups

---
+++ ReSharper.FSharp/test/data/features/service/codeFormatter/Blank lines - Module members 02 - Type groups.fs
@@ -0,0 +1,18 @@
+module Module
+
+type A1 = int
+type B1 = int
+type C1 = int
+and D1 = int
+type E1 = int
+type F1 = int
+
+type A2 =
+    int
+and B2 = int
+and C2 = int
+
+type A3 = int
+and B3 = int
+and C3 =
+    int
