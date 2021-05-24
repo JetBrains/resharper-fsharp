76ff4d9141728a0b680f6bc03c633f5c5385cd44
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-05-24 19:37:32 +0300

    IntelliJ/lexer: disable incremental lexing in string interpolations
    Fixes RIDER-55349

--- rider-fsharp/src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/fsharp/lexer/FSharpLexer.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/fsharp/lexer/FSharpLexer.kt
@@ -1,5 +1,20 @@
 package com.jetbrains.rider.ideaInterop.fileTypes.fsharp.lexer
 
 import com.intellij.lexer.FlexAdapter
+import com.intellij.lexer.RestartableLexer
+import com.intellij.lexer.TokenIterator
+import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.lexer._FSharpLexer.YYINITIAL
 
-class FSharpLexer : FlexAdapter(_FSharpLexer())
+@Suppress("UnstableApiUsage")
+class FSharpLexer : FlexAdapter(_FSharpLexer()), RestartableLexer {
+    override fun getStartState() = YYINITIAL
+
+    override fun isRestartableState(state: Int): Boolean {
+        val flex = flex
+        return flex is _FSharpLexer && flex.isRestartableState
+    }
+
+    override fun start(
+        buffer: CharSequence, startOffset: Int, endOffset: Int, initialState: Int, tokenIterator: TokenIterator?
+    ) = super.start(buffer, startOffset, endOffset, initialState)
+}
