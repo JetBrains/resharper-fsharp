4f7164a10be8124f70ee11cd73043a10a51d2090
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-05-25 04:18:53 +0300

    fix

--- ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/Cache/TypeProvidersContext.cs
+++ ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/Cache/TypeProvidersContext.cs
@@ -1,5 +1,5 @@
 using JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol.Utils;
-using static FSharp.Compiler.ExtensionTyping;
+using JetBrains.Rider.FSharp.TypeProviders.Protocol.Client;
 using IProvidedCustomAttributeProvider =
   JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol.Utils.IProvidedCustomAttributeProvider;
 
@@ -13,17 +13,32 @@ namespace JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol.Cache
       ProvidedCustomAttributeProvider = new ProvidedCustomAttributeProvider(connection);
       ProvidedTypesCache = new ProvidedTypesCache(this);
       ProvidedAssembliesCache = new ProvidedAssembliesCache(this);
+      GenericProvidedTypesCache =
+        new DependedProvidedTypesCache<string, MakeGenericTypeArgs>(this, ProvidedTypeProtocol.MakeGenericType);
+      AppliedProvidedTypesCache =
+        new DependedProvidedTypesCache<string, ApplyStaticArgumentsParameters>(this,
+          ProvidedTypeProtocol.ApplyStaticArguments);
+      ArrayProvidedTypesCache =
+        new DependedProvidedTypesCache<int, MakeArrayTypeArgs>(this, ProvidedTypeProtocol.MakeArrayType);
     }
 
     public TypeProvidersConnection Connection { get; }
-    public IProvidedEntitiesCache<ProvidedType, int> ProvidedTypesCache { get; }
-    public IProvidedEntitiesCache<ProvidedAssembly, int> ProvidedAssembliesCache { get; }
+    public ProvidedTypesCache ProvidedTypesCache { get; }
+    public ProvidedAssembliesCache ProvidedAssembliesCache { get; }
+    public DependedProvidedTypesCache<string, MakeGenericTypeArgs> GenericProvidedTypesCache { get; }
+    public DependedProvidedTypesCache<string, ApplyStaticArgumentsParameters> AppliedProvidedTypesCache { get; }
+    public DependedProvidedTypesCache<int, MakeArrayTypeArgs> ArrayProvidedTypesCache { get; }
     public IProvidedCustomAttributeProvider ProvidedCustomAttributeProvider { get; }
+
+    private RdProvidedTypeProcessModel ProvidedTypeProtocol => Connection.ProtocolModel.RdProvidedTypeProcessModel;
 
     public void Dispose(int typeProviderId)
     {
       ProvidedTypesCache.Remove(typeProviderId);
       ProvidedAssembliesCache.Remove(typeProviderId);
+      GenericProvidedTypesCache.Remove(typeProviderId);
+      AppliedProvidedTypesCache.Remove(typeProviderId);
+      ArrayProvidedTypesCache.Remove(typeProviderId);
     }
 
     public string Dump() =>
