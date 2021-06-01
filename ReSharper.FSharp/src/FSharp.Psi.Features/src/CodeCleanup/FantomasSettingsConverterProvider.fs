9ccb0c6e1d8f58823b879cb57ba8ed9ab0e959a6
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-06-01 18:59:19 +0300

    fix indentation

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/CodeCleanup/FantomasSettingsConverterProvider.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/CodeCleanup/FantomasSettingsConverterProvider.fs
@@ -11,24 +11,24 @@ open JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
 open JetBrains.ReSharper.Psi.EditorConfig
 
 type FantomasSettingsConverter(fantomasSettingsEntry: SettingsIndexedEntry) =
-  let fSharpPrefix = "fsharp_"
+    let fSharpPrefix = "fsharp_"
 
-  let run (context: SettingsConvertContext<'a>) _ =
-      let context = context.As<SettingsConvertContext<string>>()
-      Assertion.AssertNotNull(context, "Context expected to be SettingsConvertContext<string>")
+    let run (context: SettingsConvertContext<'a>) _ =
+        let context = context.As<SettingsConvertContext<string>>()
+        Assertion.AssertNotNull(context, "Context expected to be SettingsConvertContext<string>")
 
-      for entry in context.SourceData.FindEntriesByPrefix(fSharpPrefix) do
-        let setting = SettingIndex(fantomasSettingsEntry, entry.Key)
-        context.Target.SetValue(setting, entry.Value)
+        for entry in context.SourceData.FindEntriesByPrefix(fSharpPrefix) do
+            let setting = SettingIndex(fantomasSettingsEntry, entry.Key)
+            context.Target.SetValue(setting, entry.Value)
 
-      true
+        true
 
-  interface IEditorConfigConverter with
-    member val Category = "F#" with get, set
-    member x.IsPropertySupported(property) = property.StartsWith(fSharpPrefix, StringComparison.Ordinal)
-    member x.Convert(context) = run context null
-    member x.ConvertAndCheck(context) = run context context
-    member x.ReverseConvert _ = failwith "Not implemented"
+    interface IEditorConfigConverter with
+        member val Category = "F#" with get, set
+        member x.IsPropertySupported(property) = property.StartsWith(fSharpPrefix, StringComparison.Ordinal)
+        member x.Convert(context) = run context null
+        member x.ConvertAndCheck(context) = run context context
+        member x.ReverseConvert _ = failwith "Not implemented"
 
 
 [<ShellComponent>]
@@ -36,8 +36,8 @@ type FantomasSettingsConverterProvider(lifetime: Lifetime, schema: SettingsSchem
     let items = CollectionEvents<IEditorConfigConverter>(lifetime, $"{nameof FantomasSettingsConverterProvider}.Items")
 
     do
-      let fantomasSettingsEntry = schema.GetIndexedEntry<_, _>(fun (key: FSharpFormatSettingsKey) -> key.FantomasSettings)
-      items.Add(FantomasSettingsConverter(fantomasSettingsEntry))
+        let fantomasSettingsEntry = schema.GetIndexedEntry<_, _>(fun (key: FSharpFormatSettingsKey) -> key.FantomasSettings)
+        items.Add(FantomasSettingsConverter(fantomasSettingsEntry))
 
     interface IProvider<IEditorConfigConverter> with
         member this.Items = items :> _
