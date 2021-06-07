93f3ea89e35cce91cacb4fabf13ebff4fff007db
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-06-07 14:44:23 +0300

    Fix Release build

--- ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/Cache/IProvidedEntitiesCache.cs
+++ ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/Cache/IProvidedEntitiesCache.cs
@@ -73,8 +73,8 @@ namespace JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol.Cache
           foreach (var (key, i) in group)
           {
             if (!KeyHasValue(key)) continue;
-            Assertion.Assert(Entities.TryGetValue(key, out var providedEntity),
-              "Possible concurrent Remove(typeProviderId) call");
+            var hasValue = Entities.TryGetValue(key, out var providedEntity);
+            Assertion.Assert(hasValue, "Possible concurrent Remove(typeProviderId) call");
             entities[i] = providedEntity;
           }
 
