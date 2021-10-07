b16c7ee78803fbb2f7312c4717cc5326516534ea
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2021-10-07 11:34:14 +0300

    Daemon/fcs: support FSharpDiagnosticSeverity.Info (#315)

--- ReSharper.FSharp/test/src/FSharp.Tests/Daemon/FSharpHighlightingTestBase.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Daemon/FSharpHighlightingTestBase.fs
@@ -2,10 +2,15 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Features.Daemon
 
 open JetBrains.ReSharper.FeaturesTestFramework.Daemon
 open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 open JetBrains.ReSharper.Plugins.FSharp.Tests
 
 [<FSharpTest; AbstractClass>]
 type FSharpHighlightingTestBase() =
     inherit HighlightingTestBase()
+
+    override x.HighlightingPredicate(highlighting, sourceFile, settingsStore) =
+        base.HighlightingPredicate(highlighting, sourceFile, settingsStore) ||
+        highlighting :? InfoHighlighting
 
     override x.CompilerIdsLanguage = FSharpLanguage.Instance :> _
