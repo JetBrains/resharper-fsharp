f34b277b3578b45a71d6a8d5a22bd0cd4d830642
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2021-11-15 19:03:38 +0300

    Completion/rules/union case: invalidate file after using pinned results

--- ReSharper.FSharp/src/FSharp.ProjectModelBase/src/PinTypeCheckResultsCookie.cs
+++ ReSharper.FSharp/src/FSharp.ProjectModelBase/src/PinTypeCheckResultsCookie.cs
@@ -1,17 +1,23 @@
 using System;
 using FSharp.Compiler.CodeAnalysis;
 using JetBrains.Diagnostics;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Files;
 using Microsoft.FSharp.Core;
 
 namespace JetBrains.ReSharper.Plugins.FSharp
 {
+  // todo: limit to the UI thread? add an assert? may need to invalidate other files in future as well
   public class PinTypeCheckResultsCookie : IDisposable
   {
+    private readonly IPsiSourceFile mySourceFile;
     private readonly IDisposable myProhibitTypeCheckCookie;
 
-    public PinTypeCheckResultsCookie(FSharpParseFileResults parseResults, FSharpCheckFileResults checkResults,
-      bool prohibitTypeCheck)
+    public PinTypeCheckResultsCookie(IPsiSourceFile sourceFile, FSharpParseFileResults parseResults,
+      FSharpCheckFileResults checkResults, bool prohibitTypeCheck)
     {
+      mySourceFile = sourceFile;
       PinnedResults = Tuple.Create(parseResults, checkResults);
       if (prohibitTypeCheck)
         myProhibitTypeCheckCookie = ProhibitTypeCheckCookie.Create();
@@ -30,6 +36,10 @@ namespace JetBrains.ReSharper.Plugins.FSharp
 
       PinnedResults = null;
       myProhibitTypeCheckCookie?.Dispose();
+
+      var file = mySourceFile.GetPrimaryPsiFile();
+      if (file != null)
+        mySourceFile.GetSolution().GetComponent<IPsiFiles>().PsiChanged(file, PsiChangedElementType.InvalidateCached);
     }
   }
 }
