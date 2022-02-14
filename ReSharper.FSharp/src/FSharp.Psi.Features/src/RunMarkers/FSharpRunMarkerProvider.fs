JetBrains/resharper-fsharp

commit 3b22fbb17e9fd7d6a2a93a880272b3b4e922d177

Psi: remove top-level parameters owner patterns
Splits topParametersOwnerPat into:
  referencePattern
  typeParameterOfMethodList
  parametersPatternDeclaration*

ReSharper.FSharp/src/FSharp.Psi.Features/src/RunMarkers/FSharpRunMarkerProvider.fs
@@ -33,12 +33,15 @@ let isEntryPoint (binding: ITopBinding) =
     | :? IArrayType as parameterType -> parameterType.ElementType.IsString()
     | _ -> false
 
-let isApplicableMethod (parametersOwner: IParametersOwnerPat) =
-    let pattern = parametersOwner.ParametersEnumerable.SingleItem
+let isApplicableMethod (binding: IBindingImplementation) =
+    let pattern = binding.ParametersPatternsEnumerable.SingleItem.Pattern
     let unitPattern = pattern.IgnoreInnerParens().As<IUnitPat>()
     if isNull unitPattern then false else
 
-    let method = parametersOwner.DeclaredElement.As<IMethod>()
+    let refPat = binding.HeadPattern.As<IReferencePat>()
+    if isNull refPat then false else
+
+    let method = refPat.DeclaredElement.As<IMethod>()
     isNotNull method && RunMarkerUtil.IsSuitableStaticMethod(method)
 
 [<Language(typeof<FSharpLanguage>)>]
@@ -66,7 +69,7 @@ type FSharpRunMarkerProvider() =
                 let letBindings = LetBindingsDeclarationNavigator.GetByBinding(binding)
                 if letBindings.IsInline || isNull (ModuleDeclarationNavigator.GetByMember(letBindings)) then () else
 
-                if showMarkerOnStaticMethods && isApplicableMethod parametersOwner then
+                if showMarkerOnStaticMethods && isApplicableMethod binding then
                     addHighlighting binding RunMarkerAttributeIds.RUN_METHOD_MARKER_ID
 
                 if showMarkerOnEntryPoint && isEntryPoint binding then

commit 1384973a658cf98e5f4383df0dcddd959f475389

Daemon/run: fix providing entry point run markers
Regression from 3b22fbb17e9fd7d6a2a93a880272b3b4e922d177

ReSharper.FSharp/src/FSharp.Psi.Features/src/RunMarkers/FSharpRunMarkerProvider.fs
@@ -62,8 +62,7 @@ type FSharpRunMarkerProvider() =
                 consumer.AddHighlighting(highlighting, range)
 
             for binding in CachedDeclarationsCollector.Run<ITopBinding>(fsFile) do
-                let parametersOwner = binding.HeadPattern.As<IParametersOwnerPat>()
-                if isNull parametersOwner then () else
+                if binding.ParametersDeclarationsEnumerable.IsEmpty() then () else
 
                 let letBindings = LetBindingsDeclarationNavigator.GetByBinding(binding)
                 if binding.IsInline || isNull (ModuleDeclarationNavigator.GetByMember(letBindings)) then () else