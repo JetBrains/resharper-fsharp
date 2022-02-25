70eade0de017f70e8c5067f9fac8c78027c61bf5
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-02-25 19:47:38 +0300

    WIP
    # Conflicts:
    #	ReSharper.FSharp/src/FSharp.Psi/src/Impl/DocComments/FSharpDocCommentXmlPsi.cs
    #	ReSharper.FSharp/src/FSharp.Psi/src/Impl/Tree/FSharpComment.cs

---
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/DocComments/IFSharpDocCommentBlock.cs
@@ -0,0 +1,8 @@
+using JetBrains.ReSharper.Psi.Xml.XmlDocComments;
+
+namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.DocComments
+{
+  public partial interface IFSharpDocCommentBlock : IDocCommentBlockWithPsi<IDocCommentXmlPsi, XmlDocBlock>
+  {
+  }
+}
