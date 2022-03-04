b08958ecccd0c3cb7ba841b2c4d03d05ceb6ba35
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-03-04 20:07:17 +0300

    wip

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/DocComments/IFSharpDocCommentBlock.cs
+++
@@ -1,8 +0,0 @@
-using JetBrains.ReSharper.Psi.Xml.XmlDocComments;
-
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.DocComments
-{
-  public partial interface IFSharpDocCommentBlock : IDocCommentBlockWithPsi<IDocCommentXmlPsi, XmlDocBlock>
-  {
-  }
-}
