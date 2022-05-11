5a5609d2ec129b833f0874474ce109749a09a2c1
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-05-11 21:18:07 +0300

    wip

---
+++ rider-fsharp/testData/solutions/YamlProviderCSharp/YamlProviderLibrary/Library.fs
@@ -0,0 +1,29 @@
+﻿module YamlProviderLibrary
+
+open FSharp.Configuration
+
+type GeneratedConfig = YamlConfig<YamlText = """ 
+Level1:
+  Level12:
+    Level13:
+      -
+        name: Alex
+        age: 22
+      -
+        name: Eugene
+        age: 25
+Level2:
+  Level21: 2""">
+
+let configInstance = GeneratedConfig()
+  
+let funcWithNestedProvidedType (x: GeneratedConfig.Level1_Type) = () 
+let funcWithNestedProvidedTypeArray (x: GeneratedConfig.Level1_Type[]) = () 
+
+
+
+
+
+
+
+
