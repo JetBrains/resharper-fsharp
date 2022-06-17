3288357f347e1b7cabfb12106c9f0035889ecf3d
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-06-17 13:21:29 +0300

    wip

--- ReSharper.FSharp/src/FSharp.Psi/src/Impl/Cache2/GenerativeMembersConverter.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Impl/Cache2/GenerativeMembersConverter.cs
@@ -23,7 +23,6 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Cache2
     }
 
     public XmlNode GetXmlDoc() => Type.GetXmlDoc(TypeElement);
-    public IList<ITypeParameter> AllTypeParameters => EmptyList<ITypeParameter>.InstanceList;
 
     // Operators for provided types are compiled into methods
     public IEnumerable<IOperator> Operators => EmptyList<IOperator>.InstanceList;
