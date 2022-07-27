a974f63776f3d680b6105107df9d256bbe3f6bf9
Mikhail Filippov <mikhail@filippov.me>
2022-07-27 15:55:41 +0300

    Fix gradle project model for protocol

---
+++ rider-fsharp/protocol/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+}
+
+val rdLibDirectory: () -> File by rootProject.extra
+
+repositories {
+    mavenCentral()
+    maven { setUrl("https://cache-redirector.jetbrains.com/maven-central") }
+    flatDir {
+        dir(rdLibDirectory())
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        sourceSets {
+            main {
+                java {
+                    srcDir("src/kotlin/model")
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation(group = "", name = "rd-gen")
+    implementation(group = "", name = "rider-model")
+}
