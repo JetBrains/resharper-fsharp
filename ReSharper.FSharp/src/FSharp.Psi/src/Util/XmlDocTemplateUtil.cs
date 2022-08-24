f787c3f1ff52b70fd22047fe784515d5c4ac150d
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-08-24 21:57:51 +0300

    wip

--- ReSharper.FSharp/src/FSharp.Psi/src/Util/XmlDocTemplateUtil.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Util/XmlDocTemplateUtil.cs
@@ -1,18 +1,16 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using JetBrains.ReSharper.Plugins.FSharp.Psi.Impl;
-using JetBrains.ReSharper.Plugins.FSharp.Psi.Tree;
+using JetBrains.ReSharper.Plugins.FSharp.Psi.Resolve;
 using JetBrains.ReSharper.Psi.ExtensionsAPI;
 using JetBrains.ReSharper.Psi.Tree;
-using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 {
   public static class XmlDocTemplateUtil
   {
-    public static (string, int caretOffset) GetDocTemplate(IDocCommentBlock docCommentBlock, Func<int, string> linePrefix)
+    public static (string, int caretOffset) GetDocTemplate(IDocCommentBlock docCommentBlock,
+      Func<int, string> linePrefix)
     {
       var text = new StringBuilder();
       var line = 1;
@@ -21,74 +19,16 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Util
       text.Append($"{linePrefix(line++)}");
 
       var caretOffset = text.Length;
-      
+
       text.Append($"{linePrefix(line++)}</summary>");
 
-      foreach (var parameter in GetParametersGroupNames(docCommentBlock.Parent).SelectMany(t => t))
+      foreach (var parameter in FSharpParameterUtil.GetParametersGroupNames(docCommentBlock.Parent)
+                 .SelectMany(t => t)
+                 .Where(t => t != SharedImplUtil.MISSING_DECLARATION_NAME))
+
         text.Append($"{linePrefix(line++)}<param name=\"{parameter}\"></param>");
 
       return (text.ToString(), caretOffset);
     }
-
-    private static IEnumerable<string[]> GetParametersGroupNames(ITreeNode node) =>
-      node switch
-      {
-        IBinding binding => binding.Expression is ILambdaExpr lambda
-          ? binding.ParameterPatterns.SelectMany(GetParameterNames).Union(GetLambdaArgs(lambda))
-          : binding.ParameterPatterns.SelectMany(GetParameterNames),
-        IBindingSignature bindingSignature => GetParameterNames(bindingSignature.ReturnTypeInfo.ReturnType),
-        IMemberDeclaration member => member.ParameterPatterns.SelectMany(GetParameterNames)
-          .Union(member.AccessorDeclarations.SelectMany(t =>
-            t.ParameterPatterns.SelectMany(GetParameterNames))),
-        IConstructorSignature constructorSignature => GetParameterNames(constructorSignature.ReturnTypeInfo.ReturnType),
-        IConstructorDeclaration constructorDeclaration => GetParameterNames(constructorDeclaration.ParameterPatterns),
-        IAbstractMemberDeclaration abstractMember => GetParameterNames(abstractMember.ReturnTypeInfo.ReturnType),
-        IMemberSignature memberSignature => GetParameterNames(((IMemberSignatureOrDeclaration)memberSignature)
-          .ReturnTypeInfo.ReturnType),
-        IUnionCaseDeclaration ucDecl => ucDecl.Fields.Select(t => t.SourceName)
-          .Where(t => t != SharedImplUtil.MISSING_DECLARATION_NAME),
-        _ => EmptyList<string>.Enumerable
-      };
-
-    private static IEnumerable<string[]> GetLambdaArgs(ILambdaExpr expr)
-    {
-      var lambdaParams = expr.Patterns;
-      var parameters = lambdaParams.SelectMany(GetParameterNames);
-      if (expr.Expression is ILambdaExpr innerLambda && !lambdaParams.Any(t => t.IgnoreInnerParens() is IAsPat))
-        parameters = parameters.Union(GetLambdaArgs(innerLambda));
-      return parameters;
-    }
-
-    private static IEnumerable<string[]> GetParameterNames(IFSharpPattern pattern)
-    {
-      IEnumerable<string> GetParameterNamesInternal(IFSharpPattern pattern, bool isTopLevelParameter)
-      {
-        pattern = pattern.IgnoreInnerParens();
-        return pattern switch
-        {
-          ILocalReferencePat local => new[] { local.SourceName },
-          IOptionalValPat opt => GetParameterNamesInternal(opt.Pattern, isTopLevelParameter),
-          ITypedPat typed => GetParameterNamesInternal(typed.Pattern, false),
-          IAttribPat attrib => GetParameterNamesInternal(attrib.Pattern, false),
-          IAsPat asPat => GetParameterNamesInternal(asPat.RightPattern, false),
-          ITuplePat tuplePat when isTopLevelParameter => tuplePat.PatternsEnumerable.SelectMany(t =>
-            GetParameterNamesInternal(t, false)),
-          _ => EmptyList<string>.Enumerable
-        };
-      }
-
-      return GetParameterNamesInternal(pattern, true);
-    }
-
-    private static IEnumerable<string[]> GetParameterNames(ITypeUsage pattern) =>
-      pattern switch
-      {
-        IParenTypeUsage parenUsage => GetParameterNames(parenUsage.InnerTypeUsage),
-        IParameterSignatureTypeUsage { Identifier: not null } local => new[] { local.Identifier.Name },
-        IFunctionTypeUsage funPat => GetParameterNames(funPat.ArgumentTypeUsage)
-          .Union(GetParameterNames(funPat.ReturnTypeUsage)),
-        ITupleTypeUsage tuplePat => tuplePat.Items.SelectMany(GetParameterNames),
-        _ => EmptyList<string[]>.Enumerable
-      };
   }
 }
