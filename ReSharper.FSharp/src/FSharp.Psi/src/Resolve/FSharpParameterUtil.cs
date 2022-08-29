4c4a39536216f166bf458fe77ecd220320389b9f
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-08-29 23:01:28 +0300

    фывфыв

--- ReSharper.FSharp/src/FSharp.Psi/src/Resolve/FSharpParameterUtil.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Resolve/FSharpParameterUtil.cs
@@ -99,7 +99,8 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Resolve
     private static bool IsSimplePattern(IFSharpPattern pattern) => pattern.IgnoreInnerParens() switch
     {
       ILocalReferencePat or IAttribPat or ITypedPat or IUnitPat or IWildPat => true,
-      ITuplePat tuplePat => tuplePat.PatternsEnumerable.All(t => t.IgnoreInnerParens() is ILocalReferencePat),
+      ITuplePat tuplePat => tuplePat.PatternsEnumerable.All(t =>
+        t.IgnoreInnerParens() is ILocalReferencePat or IAttribPat or ITypedPat or IWildPat),
       _ => false
     };
 
