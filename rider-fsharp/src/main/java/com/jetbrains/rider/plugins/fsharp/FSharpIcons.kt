930b7b5a703d627d55fbe7159f0c25d7900fa48d
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2022-10-12 16:21:45 +0200

    Fix F# file icons

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/FSharpIcons.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/FSharpIcons.kt
@@ -3,7 +3,7 @@ package com.jetbrains.rider.plugins.fsharp
 import com.intellij.openapi.util.IconLoader
 
 object FSharpIcons {
-    val FSharp = IconLoader.getIcon("/icons/Fsharp.png")
-    val FSharpScript = IconLoader.getIcon("/icons/FsharpScript.png")
-    val FSharpConsole = IconLoader.getIcon("/icons/fsharpConsole.png")
+    val FSharp = IconLoader.getIcon("/icons/Fsharp.svg", FSharpIcons::class.java)
+    val FSharpScript = IconLoader.getIcon("/icons/FsharpScript.svg", FSharpIcons::class.java)
+    val FSharpConsole = IconLoader.getIcon("/icons/fsharpConsole.png", FSharpIcons::class.java)
 }
