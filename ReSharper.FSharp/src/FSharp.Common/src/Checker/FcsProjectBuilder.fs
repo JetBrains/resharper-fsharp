928c39edacd2b46ec57f1af6bd6892e3c76b8f90
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2022-10-23 02:29:26 +0300

    WIP

--- ReSharper.FSharp/src/FSharp.Common/src/Checker/FcsProjectBuilder.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/Checker/FcsProjectBuilder.fs
@@ -81,6 +81,9 @@ type FcsProjectBuilder(lifetime: Lifetime, checkerService: FcsCheckerService, it
     let unusedValuesWarns =
         [| "--warnon:1182" |]
 
+    let xmlDocsNoWarns =
+        [| "--nowarn:3390" |]
+
     let getOutputType (outputType: ProjectOutputType) =
         match outputType with
         | ProjectOutputType.CONSOLE_EXE -> "exe"
@@ -136,6 +139,7 @@ type FcsProjectBuilder(lifetime: Lifetime, checkerService: FcsCheckerService, it
 
         otherOptions.AddRange(defaultOptions)
         otherOptions.AddRange(unusedValuesWarns)
+        otherOptions.AddRange(xmlDocsNoWarns)
 
         match projectProperties.ActiveConfigurations.TryGetConfiguration(targetFrameworkId) with
         | :? IManagedProjectConfiguration as cfg ->
