42a3dc10e38206b71068a7b59dd7d5f025e331d3
rwx788 <rodion.iafarov@jetbrains.com>
2022-10-26 11:27:14 +0200

    Update test expectations in the template tests
    This change related to the updated of the dotnet 7 SDK. Also it was
    possible to use same gold file on mac and linux.
    Linux fails to run tests on netcoreapp and has to be investigated
    separately, this didn't work on netcoreapp2.1 before. Same test works
    fine on Windows and macOS.
    
    See [RIDER-81473](https://youtrack.jetbrains.com/issue/RIDER-81473).

--- rider-fsharp/src/test/kotlin/templates/FSharpTemplatesTestCore31.kt
+++ rider-fsharp/src/test/kotlin/templates/FSharpTemplatesTestCore31.kt
@@ -2,11 +2,14 @@ package templates
 
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.CoreVersion
+import com.jetbrains.rider.test.enums.PlatformType
 import com.jetbrains.rider.test.enums.ToolsetVersion
 import org.testng.annotations.Test
 
 @Test
-@TestEnvironment(toolset = ToolsetVersion.TOOLSET_16_CORE, coreVersion = CoreVersion.DOT_NET_CORE_3_1)
+@TestEnvironment(toolset = ToolsetVersion.TOOLSET_16_CORE,
+    coreVersion = CoreVersion.DOT_NET_CORE_3_1,
+    platform = [PlatformType.WINDOWS_ALL, PlatformType.MAC_OS_ALL])
 class FSharpTemplatesTestCore31 : FSharpTemplatesTestCore()
 {
     @Test
