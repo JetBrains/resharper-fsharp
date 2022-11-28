34003b3182e28295ae0fb052aa62e4ffe5ddd8fc
Konstantin343 <47460832+Konstantin343@users.noreply.github.com>
2022-11-28 17:07:36 +0200

    Add test cases for RIDER-69084, RIDER-69562, RIDER-75644 (#452)

--- rider-fsharp/src/test/kotlin/templates/FSharpTemplatesTestCore7.kt
+++ rider-fsharp/src/test/kotlin/templates/FSharpTemplatesTestCore7.kt
@@ -3,15 +3,17 @@ package templates
 import com.jetbrains.rider.test.annotations.TestEnvironment
 import com.jetbrains.rider.test.enums.CoreVersion
 import com.jetbrains.rider.test.enums.ToolsetVersion
+import io.qameta.allure.Issue
 import org.testng.annotations.Test
 
 @Test
 @TestEnvironment(toolset = ToolsetVersion.TOOLSET_17_CORE, coreVersion = CoreVersion.DOT_NET_7)
 class FSharpTemplatesTestCore7 : FSharpTemplatesTestCore()
 {
-    @Test()
+    @Test
     fun classlibNetCoreAppTemplate() = super.classlibNetCoreAppTemplate(targetFramework = "net7.0")
 
     @Test
+    @Issue("RIDER-75644")
     fun consoleAppCoreTemplate() = super.consoleAppCoreTemplate(expectedOutput = "Hello from F#", breakpointLine = 2)
 }
