738cc2e8ae4187a9b7f13d26af4a480b82127a33
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-01-12 17:24:34 +0300

    Type Providers: use read-lock inside FCS

--- ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/TypeProvidersExternalProcessFactory.cs
+++ ReSharper.FSharp/src/FSharp.TypeProviders.Protocol/src/TypeProvidersExternalProcessFactory.cs
@@ -1,19 +1,16 @@
 ﻿using JetBrains.Annotations;
 using JetBrains.Application.Processes;
 using JetBrains.Application.Threading;
+using JetBrains.Diagnostics;
 using JetBrains.Lifetimes;
 using JetBrains.Platform.MsBuildHost;
 using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.Build;
+using JetBrains.ProjectModel.Properties;
 using JetBrains.Util;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol
 {
-  public interface IFSharpProjectsRequiringFrameworkCache
-  {
-    bool Contains(string projectOutputPath);
-  }
-
-
   [SolutionComponent]
   public class TypeProvidersExternalProcessFactory
   {
@@ -21,20 +18,23 @@ namespace JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol
     [NotNull] private readonly ILogger myLogger;
     [NotNull] private readonly IShellLocks myShellLocks;
     [NotNull] private readonly ISolutionToolset myToolset;
-    [NotNull] private readonly IFSharpProjectsRequiringFrameworkCache myProjectsRequiringFrameworkCache;
+    [NotNull] private readonly OutputAssemblies myOutputAssemblies;
+    [NotNull] private readonly IShellLocks myLocks;
 
     public TypeProvidersExternalProcessFactory(
       [NotNull] ISolutionProcessStartInfoPatcher solutionProcessStartInfoPatcher,
       [NotNull] ILogger logger,
       [NotNull] IShellLocks shellLocks,
       [NotNull] ISolutionToolset toolset,
-      [NotNull] IFSharpProjectsRequiringFrameworkCache projectsRequiringFrameworkCache)
+      [NotNull] OutputAssemblies outputAssemblies,
+      [NotNull] IShellLocks locks)
     {
       mySolutionProcessStartInfoPatcher = solutionProcessStartInfoPatcher;
       myLogger = logger;
       myShellLocks = shellLocks;
       myToolset = toolset;
-      myProjectsRequiringFrameworkCache = projectsRequiringFrameworkCache;
+      myOutputAssemblies = outputAssemblies;
+      myLocks = locks;
     }
 
     public TypeProvidersExternalProcess Create(Lifetime lifetime,
@@ -59,8 +59,20 @@ namespace JetBrains.ReSharper.Plugins.FSharp.TypeProviders.Protocol
         ? JetProcessRuntimeType.DotNetCore
         : JetProcessRuntimeType.FullFramework;
 
-      if (requestingProjectOutputPath != null && myProjectsRequiringFrameworkCache.Contains(requestingProjectOutputPath))
-        runtimeType = JetProcessRuntimeType.FullFramework;
+      if (requestingProjectOutputPath != null)
+      {
+        var path = VirtualFileSystemPath.Parse(requestingProjectOutputPath, InteractionContext.SolutionContext);
+
+        FSharpAsyncUtil.UsingReadLockInsideFcs(myLocks, () =>
+        {
+          var project = myOutputAssemblies.TryGetProjectByOutputAssemblyLocation(path).NotNull();
+
+          foreach (var configuration in project.ProjectProperties.GetActiveConfigurations<IProjectConfiguration>())
+            if (configuration.PropertiesCollection.TryGetValue("FscToolExe", out var fscTool) &&
+                fscTool is "fsc.exe" or "fsharpc")
+              runtimeType = JetProcessRuntimeType.FullFramework;
+        });
+      }
 
       var mutator = MsBuildConnectionFactory.GetEnvironmentVariablesMutator(buildTool);
 
