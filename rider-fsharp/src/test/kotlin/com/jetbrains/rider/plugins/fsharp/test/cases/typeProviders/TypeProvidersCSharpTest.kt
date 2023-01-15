fd8cb60be75ed3bf5e7aec4754bb91922debbfeb
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-01-15 21:49:17 +0300

    Fix

--- rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/typeProviders/TypeProvidersCSharpTest.kt
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/typeProviders/TypeProvidersCSharpTest.kt
@@ -65,7 +65,7 @@ class TypeProvidersCSharpTest : BaseTestWithSolution() {
 
             // change method call from "ApiCoursesGet" to "ApiCoursesGet1"
             typeFromOffset("1", 195)
-            waitForDaemon()
+            waitForNextDaemon()
 
             executeWithGold(File(testGoldFile.path + "_after")) {
                 dumpSevereHighlighters(it)
