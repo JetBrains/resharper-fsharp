e6674645d235e928d576ec91b08d0d92898ea389
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-03-27 18:18:31 +0200

    Tooltips: restore the obsolete provider as a marker component
    The backend requires any implementation assigned to a highlighting type, otherwise tooltip calculation takes too long

--- ReSharper.FSharp/src/FSharp.Psi/src/Features/Daemon/Highlightings/IdentifierHighlighting.cs
+++ ReSharper.FSharp/src/FSharp.Psi/src/Features/Daemon/Highlightings/IdentifierHighlighting.cs
@@ -1,8 +1,16 @@
-﻿using JetBrains.DocumentModel;
+﻿using JetBrains.Annotations;
+using JetBrains.DocumentModel;
+using JetBrains.Lifetimes;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Daemon;
 using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Feature.Services.Descriptions;
+using JetBrains.ReSharper.Feature.Services.UI;
+using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
 {
+  [DaemonTooltipProvider(typeof(FSharpIdentifierTooltipProvider))]
   [StaticSeverityHighlighting(Severity.INFO, typeof(HighlightingGroupIds.IdentifierHighlightings),
     OverlapResolve = OverlapResolveKind.NONE, ShowToolTipInStatusBar = false)]
   public class FSharpIdentifierHighlighting : ICustomAttributeIdHighlighting, IHighlightingWithFeatureStatisticsKey
@@ -22,5 +30,16 @@ namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
     public DocumentRange CalculateRange() => myRange;
 
     public int? GetStatisticsKey() => null;
+  }
+
+  [SolutionComponent]
+  internal class FSharpIdentifierTooltipProvider : IdentifierTooltipProvider<FSharpLanguage>
+  {
+    public FSharpIdentifierTooltipProvider(Lifetime lifetime, ISolution solution,
+      IDeclaredElementDescriptionPresenter presenter, DeclaredElementPresenterTextStylesService textStylesService,
+      [CanBeNull] DeclaredElementPresenterTextStyles textStyles = null)
+      : base(lifetime, solution, presenter, textStylesService, textStyles)
+    {
+    }
   }
 }
