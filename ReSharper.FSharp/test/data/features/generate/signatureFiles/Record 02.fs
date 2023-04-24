e5f5898bd00abe6b3f0047fa0db8d3c9c9762d4f
dawe <dawedawe@posteo.de>
2023-04-24 11:28:31 +0200

    add test for record with static member

---
+++ ReSharper.FSharp/test/data/features/generate/signatureFiles/Record 02.fs
@@ -0,0 +1,7 @@
+// ${KIND:SignatureFile}
+// ${SELECT0:Generate signature file title}
+module Foo
+
+type Bar = { A:int; B: int }
+           static member Add x y = x + y
+{caret}
--- ReSharper.FSharp/test/src/FSharp.Tests/Generate/FsharpGenerateSignatureTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Generate/FsharpGenerateSignatureTest.fs
@@ -31,6 +31,7 @@ type FsharpGenerateSignatureTest() =
     [<Test>] member x.``Namespace structure 01`` () = x.DoNamedTest()
     [<Test>] member x.``TypeAbbreviation 01`` () = x.DoNamedTest()
     [<Test>] member x.``Record 01`` () = x.DoNamedTest()
+    [<Test>] member x.``Record 02`` () = x.DoNamedTest()
     [<Test>] member x.``Union 01`` () = x.DoNamedTest()
     [<Test>] member x.``Nested module 01`` () = x.DoNamedTest()
     [<Test>] member x.``Recursive types 01`` () = x.DoNamedTest()
