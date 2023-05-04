05ec8b6382be4ab27acc9b78dbd284594adc91ec
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-05-04 14:18:28 +0300

    use ILanguageFSharpZone

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/CodeCleanup/ZoneMarker.fs
+++
@@ -1,8 +0,0 @@
-namespace JetBrains.ReSharper.Plugins.FSharp.Services.Formatter
-
-open JetBrains.Application.BuildScript.Application.Zones
-open JetBrains.TextControl
-
-[<ZoneMarker>]
-type ZoneMarker() =
-    interface IRequire<ITextControlsZone>
--- ReSharper.FSharp/src/FSharp.Psi.Features/src/ZoneMarker.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/ZoneMarker.fs
@@ -1,4 +1,4 @@
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.LanguageService
+namespace JetBrains.ReSharper.Plugins.FSharp
 
 open JetBrains.Application.BuildScript.Application.Zones
 open JetBrains.ReSharper.Plugins.FSharp
