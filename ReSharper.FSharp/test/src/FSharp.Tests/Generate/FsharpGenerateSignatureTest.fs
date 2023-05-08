62a893806ebd20d98873b0e22fe217d5f26f0e87
nojaf <florian.verdonck@outlook.com>
2023-05-08 12:05:49 +0200

    Implicit constructor with parameters.

--- ReSharper.FSharp/test/src/FSharp.Tests/Generate/FsharpGenerateSignatureTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Generate/FsharpGenerateSignatureTest.fs
@@ -44,3 +44,4 @@ type FsharpGenerateSignatureTest() =
     [<Test>] member x.``Exception 02`` () = x.DoNamedTest()
     [<Test>] member x.``Delegate 01`` () = x.DoNamedTest()
     [<Test>] member x.``Implicit Constructor 01`` () = x.DoNamedTest()
+    [<Test>] member x.``Implicit Constructor 02`` () = x.DoNamedTest()
