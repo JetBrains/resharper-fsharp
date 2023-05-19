6737434768a5c182bb3a4507532ab3aeae3257a6
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-05-19 12:32:49 +0200

    Fix for API change (BreadcrumbsHelperBase)

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/LanguageService/FSharpBreadcrumbsHelper.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/LanguageService/FSharpBreadcrumbsHelper.fs
@@ -1,10 +1,10 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.LanguageService
 
 open JetBrains.ProjectModel
+open JetBrains.ReSharper.Feature.Services.Breadcrumbs
 open JetBrains.ReSharper.Plugins.FSharp
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Psi.Tree
-open JetBrains.Rider.Backend.Features.Documents
 
 [<ProjectFileType(typeof<FSharpProjectFileType>)>]
 type FSharpBreadcrumbsHelper() =
