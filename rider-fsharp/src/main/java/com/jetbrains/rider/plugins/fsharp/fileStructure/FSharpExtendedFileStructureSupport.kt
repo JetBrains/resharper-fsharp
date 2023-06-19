ea23e22de4164022953a064cd3f9e8cc3bc18bd2
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-06-19 14:55:23 +0200

    Navigation/file member: disable sorting

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/fileStructure/FSharpExtendedFileStructureSupport.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/fileStructure/FSharpExtendedFileStructureSupport.kt
@@ -1,5 +1,9 @@
 package com.jetbrains.rider.plugins.fsharp.fileStructure
 
+import com.intellij.ide.util.treeView.smartTree.Sorter
 import com.jetbrains.rider.fileStructure.RiderExtendedFileStructure
+import com.jetbrains.rider.fileStructure.*
 
-class FSharpExtendedFileStructureSupport : RiderExtendedFileStructure
+class FSharpExtendedFileStructureSupport : RiderExtendedFileStructureBase() {
+  override val sorters: Array<Sorter> get() = emptyArray()
+}
