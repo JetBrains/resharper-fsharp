0e40d58c5cb89aad19747c355041768497e48c3d
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-06-20 17:14:49 +0300

    review fixes

--- rider-fsharp/src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/fsharp/FSharpFileType.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/ideaInterop/fileTypes/fsharp/FSharpFileType.kt
@@ -8,7 +8,7 @@ object FSharpFileType : RiderLanguageFileTypeBase(FSharpLanguage) {
   override fun getName() = "F#"
   override fun getDisplayName() = "F#"
   override fun getDefaultExtension() = "fs"
-  override fun getDescription() = FSharpBundle.message("FSharpFileType.label.fsharp.file")
+  override fun getDescription() = FSharpBundle.message("FSharpFileType.label")
   override fun getIcon() = FSharpIcons.FSharp
 }
 
@@ -16,6 +16,6 @@ object FSharpScriptFileType : RiderLanguageFileTypeBase(FSharpLanguage) {
   override fun getName() = "F# Script"
   override fun getDisplayName() = "F# Script"
   override fun getDefaultExtension() = "fsx"
-  override fun getDescription() = FSharpBundle.message("FSharpScriptFileType.label.fsharp.file")
+  override fun getDescription() = FSharpBundle.message("FSharpScriptFileType.label")
   override fun getIcon() = FSharpIcons.FSharpScript
 }
