c2461c29f66ba951f75ebf4ed3b9c2cb9aebf3e5
dawe <dawedawe@posteo.de>
2023-07-10 17:28:37 +0200

    add test for complex expressions fom lang preview

--- ReSharper.FSharp/test/src/FSharp.Intentions.Tests/src/QuickFixes/UpdateLiteralConstantFixTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Intentions.Tests/src/QuickFixes/UpdateLiteralConstantFixTest.fs
@@ -1,5 +1,6 @@
 ﻿namespace JetBrains.ReSharper.Plugins.FSharp.Tests.Intentions.QuickFixes
 
+open JetBrains.ReSharper.Plugins.FSharp.ProjectModel
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Tests
 open NUnit.Framework
@@ -17,6 +18,8 @@ type UpdateLiteralConstantFixTest() =
     [<Test>] member x.``Update Literal Constant - 05`` () = x.DoNamedTestWithSignature()
     [<Test>] member x.``Update Literal Constant - 06`` () = x.DoNamedTestWithSignature()
     [<Test>] member x.``Update Literal Constant - 07`` () = x.DoNamedTestWithSignature()
+    [<FSharpLanguageLevel(FSharpLanguageLevel.Preview)>]
+    [<Test>] member x.``Update Literal Constant - 08`` () = x.DoNamedTestWithSignature()
     [<Test; NotAvailable>] member x.``Do not update Literal Constant - 01`` () = x.DoNamedTestWithSignature()
     [<Test; NotAvailable>] member x.``Do not update Literal Constant - 02`` () = x.DoNamedTestWithSignature()
     [<Test; NotAvailable>] member x.``Do not update Literal Constant - 03`` () = x.DoNamedTestWithSignature()
