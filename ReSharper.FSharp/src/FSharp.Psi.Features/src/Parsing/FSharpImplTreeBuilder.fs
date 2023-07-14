143d76ce21d93f5a442ffaff689dc5718144a122
dawe <dawedawe@posteo.de>
2023-07-15 00:32:09 +0200

    use range instead of expr.Range

--- ReSharper.FSharp/src/FSharp.Psi.Features/src/Parsing/FSharpImplTreeBuilder.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Features/src/Parsing/FSharpImplTreeBuilder.fs
@@ -804,8 +804,8 @@ type FSharpExpressionTreeBuilder(lexer, document, lifetime, path, projectedOffse
 
             match synConst with
             | SynConst.Measure _ ->
-                x.AdvanceToTokenOrRangeEnd(FSharpTokenType.LESS, expr.Range)
-                x.MarkAndDone(expr.Range, ElementType.TYPE_MEASURE)
+                x.AdvanceToTokenOrRangeEnd(FSharpTokenType.LESS, range)
+                x.MarkAndDone(range, ElementType.TYPE_MEASURE)
             | _ -> ()
 
             x.Done(range, mark, x.GetConstElementType(synConst))
