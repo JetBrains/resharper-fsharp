219c219dfc11f1215c3ea398b4faa466c4ef4115
Rodion Iafarov <rodion.iafarov@jetbrains.com>
2023-07-19 17:30:07 +0200

    Add F# net 8.0 template tests

---
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/templates/net80/ProjectTemplates.Net80.kt
@@ -0,0 +1,76 @@
+package com.jetbrains.rider.plugins.fsharp.test.cases.templates.net80
+
+import com.jetbrains.rider.test.annotations.Mute
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.base.templates.sdk.ClassLibProjectTemplateTestBase
+import com.jetbrains.rider.test.base.templates.sdk.ConsoleAppProjectTemplateTestBase
+import com.jetbrains.rider.test.base.templates.sdk.XUnitProjectTemplateTestBase
+import com.jetbrains.rider.test.enums.PlatformType
+import com.jetbrains.rider.test.env.enums.BuildTool
+import com.jetbrains.rider.test.env.enums.SdkVersion
+import com.jetbrains.rider.test.scriptingApi.ProjectTemplateIds
+import com.jetbrains.rider.test.scriptingApi.TemplateIdWithVersion
+
+@Suppress("unused")
+@Mute("Unable to load project and obtain project information from MsBuild.", [PlatformType.LINUX_ARM64])
+@TestEnvironment(sdkVersion = SdkVersion.DOT_NET_8, buildTool = BuildTool.SDK )
+object Net80 {
+  class ClassLibProjectTemplateTest : ClassLibProjectTemplateTestBase() {
+    override val templateId: TemplateIdWithVersion
+      get() = ProjectTemplateIds.currentSdk.fsharp_classLibrary
+    override val expectedNumOfAnalyzedFiles: Int = 1
+    override val expectedNumOfSkippedFiles: Int = 0
+    override val targetFramework: String = "net8.0"
+    override val buildFilesIgnoreList: Set<Regex> = setOf(
+      Regex("ClassLibrary/bin/Debug/net8\\.0/ClassLibrary\\.deps\\.json"),
+      Regex("ClassLibrary/obj/Debug/net8\\.0/ref(int)?/.*")
+    )
+
+    init {
+      addMute(Mute("RIDER-79065: No SWEA for F#"), ::swea)
+    }
+  }
+
+  class ConsoleAppProjectTemplateTest : ConsoleAppProjectTemplateTestBase() {
+    override val templateId: TemplateIdWithVersion
+      get() = ProjectTemplateIds.currentSdk.fsharp_consoleApplication
+    override val expectedNumOfAnalyzedFiles: Int = 3
+    override val expectedNumOfSkippedFiles: Int = 0
+    override val breakpointLine: Int = 2
+    override val expectedOutput: String = "Hello from F#"
+    override val debugFileName: String = "Program.fs"
+    override val buildFilesIgnoreList: Set<Regex> = setOf(
+      Regex("ConsoleApplication/bin/Debug/net8\\.0/FSharp\\.Core\\.dll"),
+      Regex("ConsoleApplication/bin/Debug/net8\\.0/.*/FSharp\\.Core\\.resources\\.dll"),
+      Regex("ConsoleApplication/(bin|obj)/Debug/net8\\.0/ConsoleApplication\\.(fsproj\\.CopyComplete|runtimeconfig\\.json|deps\\.json)"),
+      Regex("ConsoleApplication/obj/Debug/net8\\.0/ref(int)?/.*")
+    )
+
+    init {
+      addMute(Mute("RIDER-79065: No SWEA for F#"), ::swea)
+    }
+  }
+
+  class XUnitProjectTemplateTest : XUnitProjectTemplateTestBase() {
+    override val templateId: TemplateIdWithVersion
+      get() = ProjectTemplateIds.currentSdk.fsharp_xUnit
+    override val expectedNumOfAnalyzedFiles: Int = 1
+    override val expectedNumOfSkippedFiles: Int = 0
+    override val sessionElements: Int = 3
+    override val debugFileName: String = "Tests.fs"
+    override val breakpointLine: Int = 8
+    override val buildFilesIgnoreList: Set<Regex> = setOf(
+      Regex("UnitTestProject/bin/Debug/net8\\.0/FSharp\\.Core\\.dll"),
+      Regex("UnitTestProject/bin/Debug/net8\\.0/.*/.*\\.dll"), // Localization folders, like cs/de/es
+      Regex("UnitTestProject/bin/Debug/net8\\.0/Microsoft\\.(TestPlatform|VisualStudio).*\\.dll"),
+      Regex("UnitTestProject/bin/Debug/net8\\.0/(Newtonsoft\\.Json|NuGet\\.Frameworks|testhost|xunit\\.).*\\.(dll|exe)"),
+      Regex("UnitTestProject/(bin|obj)/Debug/net8\\.0/UnitTestProject\\.(fsproj\\.CopyComplete|runtimeconfig\\.json|deps\\.json)"),
+      Regex("UnitTestProject/obj/Debug/net8\\.0/ref(int)?/.*")
+    )
+
+    init {
+      addMute(Mute("No run configuration"), testMethod = XUnitProjectTemplateTestBase::runConfiguration)
+      addMute(Mute("RIDER-79065: No SWEA for F#"), ::swea)
+    }
+  }
+}
