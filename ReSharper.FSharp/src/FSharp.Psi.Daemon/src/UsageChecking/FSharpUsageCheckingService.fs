a64504946cc33b57f06b73914d1effa62f1ef2a0
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-07-25 18:58:40 +0200

    Daemon/usage checking: prevent usage checking analysis

--- ReSharper.FSharp/src/FSharp.Psi.Daemon/src/UsageChecking/FSharpUsageCheckingService.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Daemon/src/UsageChecking/FSharpUsageCheckingService.fs
@@ -16,3 +16,26 @@ type FSharpUsageCheckingServices(lifetime, suppressors) =
     let _ = FSharpLanguage.Instance // workaround to create assembly reference (Microsoft/visualfsharp#3522)
 
     override x.CreateUnusedLocalDeclarationAnalyzer(_, _, _) = null
+
+
+type FSharpCollectUsagesPsiFileProcessor(collectUsagesStageProcess, daemonProcess, settingsStore,
+        collectUsagesStagePersistentData, fibers, scopeProcessorFactory, usageCheckingServiceManager,
+        globalFileStructureBuilder, swaExtensionProviders) =
+    inherit CommonCollectUsagesPsiFileProcessor(collectUsagesStageProcess, daemonProcess, settingsStore,
+        collectUsagesStagePersistentData, fibers, scopeProcessorFactory, usageCheckingServiceManager,
+        globalFileStructureBuilder, swaExtensionProviders)
+
+    override this.InteriorShouldBeProcessed(_, _) = false
+    override this.ProcessBeforeInterior(_, _) = ()
+
+
+[<Language(typeof<FSharpLanguage>)>]
+type FSharpCollectUsagesPsiFileProcessorFactory() =
+    inherit CollectUsagesPsiFileProcessorFactory()
+
+    override this.CreatePsiFileProcessor(collectUsagesStageProcess, daemonProcess, settingsStore,
+            collectUsagesStagePersistentData, fibers, scopeProcessorFactory, usageCheckingServiceManager,
+            globalFileStructureBuilder, swaExtensionProviders) =
+        FSharpCollectUsagesPsiFileProcessor(collectUsagesStageProcess, daemonProcess, settingsStore,
+            collectUsagesStagePersistentData, fibers, scopeProcessorFactory, usageCheckingServiceManager,
+            globalFileStructureBuilder, swaExtensionProviders)
