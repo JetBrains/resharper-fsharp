41f5d83133d0507f76526240feecaa0c3c5b7306
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-09-18 12:08:03 +0200

    Plugins: Fix zone validation by adding missing Module Zone requirements in JetBrains.ReSharper.Plugins.FSharp.Psi.Intentions

--- ReSharper.FSharp/src/FSharp.Psi.Intentions/src/Intentions/IfToElifAction.fs
+++ ReSharper.FSharp/src/FSharp.Psi.Intentions/src/Intentions/IfToElifAction.fs
@@ -1,7 +1,10 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Intentions
 
 open System
+open JetBrains.Application.BuildScript.Application.Zones
+open JetBrains.ProjectModel
 open JetBrains.ReSharper.Feature.Services.ContextActions
+open JetBrains.ReSharper.Plugins.FSharp
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Parsing
@@ -14,6 +17,7 @@ open JetBrains.TextControl
 open JetBrains.Util
 
 [<ContextAction(Name = "IfToElif", Group = "F#", Description = "Converts `if` expression to 'elif'")>]
+[<ZoneMarker(typeof<ILanguageFSharpZone>, typeof<IProjectModelZone>, typeof<ITextControlsZone>, typeof<PsiFeaturesImplZone>)>]
 type IfToElifAction(dataProvider: FSharpContextActionDataProvider) =
     inherit FSharpContextActionBase(dataProvider)
 
