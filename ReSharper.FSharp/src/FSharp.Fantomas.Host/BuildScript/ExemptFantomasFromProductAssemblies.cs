9875f760461fe283c2e221e18fb35b8579b64529
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-09-27 15:11:13 +0200

    Plugins: Exempt FSharp.Fantomas.Host and FSharp.TypeProviders.Host from product assemblies to fix type resolving errors as they target .NET Core
    Adds ExemptFantomasFromProductAssemblies./ExemptTypeProvidersFromProductAssemblies.MarkMyselfNonProduct() BuildStep in
    Plugins/resharper-fsharp/ReSharper.FSharp/src/FSharp.Fantomas.Host,FSharp.TypeProviders.Host subplatforms respectively.

---
+++ ReSharper.FSharp/src/FSharp.Fantomas.Host/BuildScript/ExemptFantomasFromProductAssemblies.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Application.BuildScript.Compile;
+using JetBrains.Application.BuildScript.Solution;
+using JetBrains.Build;
+
+namespace JetBrains.ReSharper.Plugins.FSharp.Fantomas.Host.BuildScript;
+
+public class ExemptFantomasFromProductAssemblies
+{
+    [BuildStep]
+    public static SubplatformWithNonProductAssemblies[] MarkMyselfNonProduct(AllAssembliesOnEverything allass)
+    {
+        return SubplatformWithNonProductAssemblies.MarkCaller<ExemptFantomasFromProductAssemblies>(allass);
+    }
+}
