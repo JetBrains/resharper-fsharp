615a2240f2e2f63b1159b4e8bb78967416ee9dec
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-10-01 21:09:56 +0200

    Plugins: Add FantomasBundledVersionAttribute as assembly information in JetBrains.ReSharper.Plugins.FSharp.Psi.Features

---
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/AssemblyInfo.fs
@@ -0,0 +1,4 @@
+﻿module JetBrains.ReSharper.Plugins.FSharp.Psi.Features.AssemblyInfo
+
+[<assembly: JetBrains.ReSharper.Plugins.FSharp.Fantomas.Protocol.FantomasBundledVersionAttribute("6.0.5")>]
+do ()
