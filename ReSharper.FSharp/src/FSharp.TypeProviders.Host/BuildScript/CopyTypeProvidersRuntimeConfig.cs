860e59b72fa20f2e46c640520312ec085105a72a
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-10-02 17:33:53 +0200

    Plugins: Copy tploader.{unix,win}.runtimeconfig.json files by adding CopyTypeProvidersRuntimeConfig.Run() BuildStep in Plugins/resharper-fsharp/ReSharper.FSharp/src/FSharp.TypeProviders.Host subplatform

---
+++ ReSharper.FSharp/src/FSharp.TypeProviders.Host/BuildScript/CopyTypeProvidersRuntimeConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using JetBrains.Application.BuildScript;
+using JetBrains.Application.BuildScript.Compile;
+using JetBrains.Application.BuildScript.Solution;
+using JetBrains.Build;
+using JetBrains.Util.Storage;
+
+namespace JetBrains.ReSharper.Plugins.FSharp.BuildScript;
+
+public class CopyTypeProvidersRuntimeConfig
+{
+    [BuildStep]
+    public static SubplatformFileForPackagingFast[] Run(AllAssembliesOnEverything allass, ProductHomeDirArtifact homedir)
+    {
+        if (allass.FindSubplatformByClass<CopyTypeProvidersRuntimeConfig>() is SubplatformOnSources subplatform)
+        {
+            return new[]
+            {
+                CopyFileToOutputRequest("tploader.unix.runtimeconfig.json"),
+                CopyFileToOutputRequest("tploader.win.runtimeconfig.json")
+            };
+
+            SubplatformFileForPackagingFast CopyFileToOutputRequest(string fileName)
+            {
+                return new SubplatformFileForPackagingFast(
+                    subplatform.Name,
+                    ImmutableFileItem.CreateFromDisk(homedir.ProductHomeDir / subplatform.Name.RelativePath / "FSharp.TypeProviders.Host.NetCore" / fileName));
+            }
+        }
+
+        return Array.Empty<SubplatformFileForPackagingFast>();
+    }
+}
