98b24b29cd1e621549f9d52aabeca7d07f381f2f
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-10-02 17:52:43 +0200

    Plugins: Install application .exe.config file by adding RequestAppConfigInstall.Run() BuildStep in Plugins/resharper-fsharp/ReSharper.FSharp/src/FSharp.TypeProviders.Host subplatform

---
+++ ReSharper.FSharp/src/FSharp.TypeProviders.Host/BuildScript/RequestAppConfigInstall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Application.BuildScript.Install;
+using JetBrains.Application.BuildScript.Solution;
+using JetBrains.Build;
+
+namespace JetBrains.ReSharper.Plugins.FSharp.BuildScript;
+
+public class RequestAppConfigInstall
+{
+    [BuildStep]
+    public static InstallAppConfig[] Run(AllAssembliesOnEverything allass)
+    {
+        return allass.FindSubplatformByClass<RequestAppConfigInstall>() is SubplatformOnSources subplatform ? new[] {new InstallAppConfig(subplatform.Name, "FSharp.TypeProviders.Host/FSharp.TypeProviders.Host.csproj")} : Array.Empty<InstallAppConfig>();
+    }
+}
