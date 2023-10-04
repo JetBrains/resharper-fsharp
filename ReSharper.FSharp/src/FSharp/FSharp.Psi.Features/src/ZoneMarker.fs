a9160da1ce7dc5e0ae1ad237f3c1f748c561fcbf
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-10-04 18:40:15 +0300

    ZoneMarker: cleanup (#566)

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/ZoneMarker.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/ZoneMarker.fs
@@ -6,3 +6,13 @@ open JetBrains.ReSharper.Plugins.FSharp
 [<ZoneMarker>]
 type ZoneMarker() =
     interface IRequire<IFSharpPluginZone>
+
+
+namespace JetBrains.ReSharper.Plugins.FSharp.Services
+
+open JetBrains.Application.BuildScript.Application.Zones
+open JetBrains.ReSharper.Plugins.FSharp
+
+[<ZoneMarker>]
+type ZoneMarker() =
+    interface IRequire<IFSharpPluginZone>
--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/ZoneMarkerServices.fs
+++
@@ -1,8 +0,0 @@
-namespace JetBrains.ReSharper.Plugins.FSharp.Services
-
-open JetBrains.Application.BuildScript.Application.Zones
-open JetBrains.ReSharper.Plugins.FSharp
-
-[<ZoneMarker>]
-type ZoneMarker() =
-    interface IRequire<IFSharpPluginZone>
