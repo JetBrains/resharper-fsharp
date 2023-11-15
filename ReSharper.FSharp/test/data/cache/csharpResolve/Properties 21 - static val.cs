9ed9c856c0a24132eaa2ca9e1732182aabcd8fa6
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2023-11-15 13:36:40 +0100

    Psi: static let in more types

---
+++ ReSharper.FSharp/test/data/cache/csharpResolve/Properties 21 - static val.cs
@@ -0,0 +1,11 @@
+public class Class1
+{
+  public Class1()
+  {
+    int i1 = U.P1;
+    int i2 = U.P2;
+
+    U.P1 = i1;
+    U.P2 = i2;
+  }
+}
--- ReSharper.FSharp/test/src/FSharp.Tests/Cache/CSharpResolveTest.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests/Cache/CSharpResolveTest.fs
@@ -103,6 +103,7 @@ type CSharpResolveTest() =
     [<Test>] member x.``Properties 18 - Explicit accessors - Compiled name``() = x.DoNamedTest()
     [<Test>] member x.``Properties 19 - Simple property with getter``() = x.DoNamedTest()
     [<Test>] member x.``Properties 20 - Simple property with accessors``() = x.DoNamedTest()
+    [<Test>] member x.``Properties 21 - static val``() = x.DoNamedTest()
 
     [<Test>] member x.``Module bindings 01 - Simple``() = x.DoNamedTest()
     [<Test>] member x.``Module bindings 02 - Records``() = x.DoNamedTest()
