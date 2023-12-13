d4200a9c7c6ada9b93568aa721e51c3a23329ef4
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-12-13 21:50:01 +0300

    wip

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Daemon/src/Analyzers/LambdaAnalyzer.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Daemon/src/Analyzers/LambdaAnalyzer.fs
@@ -90,9 +90,6 @@ type LambdaAnalyzer() =
 
         compareArgsRec expr 0 null
 
-    let resolvesToPredefinedFunction (context: ILambdaExpr) funName =
-        resolvesToPredefinedFunction context.RArrow funName "LambdaAnalyzer"
-
     let hasExplicitConversion (lambda: ILambdaExpr) =
         let expr = lambda.Expression.IgnoreInnerParens()
 
@@ -209,6 +206,10 @@ type LambdaAnalyzer() =
                 |> Seq.exists (fun x -> x.IsOptionalArg)
             if hasOptionalArg then null else ctor arg
         | _ -> ctor arg
+
+    let tryCreateWarningForBuiltInFun (ctor: ILambdaExpr * string -> #IHighlighting) (lambda: ILambdaExpr, funName: string as arg) isFSharp6Supported =
+        if not (resolvesToPredefinedFunction lambda.RArrow funName "LambdaAnalyzer") then null else
+        tryCreateWarning ctor arg isFSharp6Supported
 
     let rec containsForcedCalculations (expression: IFSharpExpression) =
         let mutable containsForcedCalculations = false
@@ -341,18 +342,15 @@ type LambdaAnalyzer() =
                     if isFunctionInApp expr &funExpr &arg then
                         RedundantApplicationWarning(funExpr, arg) :> _
                     else
-                        if not (resolvesToPredefinedFunction lambda "id") then null else
-                        tryCreateWarning LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda,  "id") isFSharp60Supported :> _
+                        tryCreateWarningForBuiltInFun LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda,  "id") isFSharp60Supported :> _
                 else
                     match pat with
                     | :? ITuplePat as pat when not pat.IsStruct && pat.PatternsEnumerable.CountIs(2) ->
                         let tuplePats = pat.Patterns
                         if compareArg tuplePats[0] expr then
-                            if not (resolvesToPredefinedFunction lambda "fst") then null else
-                            tryCreateWarning LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda, "fst") isFSharp60Supported :> _
+                            tryCreateWarningForBuiltInFun LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda, "fst") isFSharp60Supported :> _
                         elif compareArg tuplePats[1] expr then
-                            if not (resolvesToPredefinedFunction lambda "snd") then null else
-                            tryCreateWarning LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda, "snd") isFSharp60Supported :> _
+                            tryCreateWarningForBuiltInFun LambdaCanBeReplacedWithBuiltinFunctionWarning (lambda, "snd") isFSharp60Supported :> _
                         else null
                     | :? ILocalReferencePat as pat when isFSharp80Supported ->
                         let referenceExpr = getRootRefExprIfCanBeConvertedToDotLambda pat lambda
