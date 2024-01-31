eb3e17bb72e9b9ad2af67e639eda88cf68bf8ea6
Mark.Tkachenko <mark.tkachenko@jetbrains.com>
2024-01-31 15:36:11 +0100

    fix RIDER-104282
    GitOrigin-RevId: d58bb37fa7a2593abf361424b6240f0fe9a4de2b

---
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/AI/FSharpCodeSetNodesFactory.fs
@@ -0,0 +1,28 @@
+module JetBrains.ReSharper.Plugins.FSharp.Psi.Features.AI.FSharpCodeSetNodesFactory
+
+open System.Text
+open JetBrains.Application
+open JetBrains.ReSharper.Feature.Services.AI.CodeSetsProviding
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
+open JetBrains.ReSharper.Psi.Tree
+open JetBrains.ReSharper.Feature.Services.AI.CSharp.CodeSetsProviding
+
+
+type FSharpGraphNode(graph:CodeSetsGraph, treeNode:ITreeNode) = 
+    inherit CodeSetNode(graph, treeNode)
+    let myFsharpNode = treeNode
+    override this.Print(printer:StringBuilder, printingInfo:CodeSetsGraphPrintingInfo) = 
+        myFsharpNode.Print(printer, graph, this, printingInfo)
+    override this.GetCost() =
+        myFsharpNode.GetCost(this.Graph.CodeSetNodesFactory, this.Graph.TokenCounter)
+
+[<ShellComponent>]
+type FSharpCodeSetNodesFactory() =
+    interface ICodeSetNodesFactory with
+        member _.CreateGraphNode(treeNode: ITreeNode, graph: CodeSetsGraph) = 
+            FSharpGraphNode(graph, treeNode) :> CodeSetNode
+
+        member _.CanCreateGraphNode(treeNode: ITreeNode) =
+            match treeNode with
+            | :? IFSharpDeclaration -> true
+            | _ -> false
