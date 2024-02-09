16ff7d5ff98b634c60c321cd210148401b18fd2a
Denis Mukhametianov <denis.mukhametianov@jetbrains.com>
2024-02-09 12:03:05 +0100

    [rider] fix spellchecking for shaders and uxml
    GitOrigin-RevId: 6e941eaf20be09038fe4637ae43f88e2ed04fc73

---
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/spellchecker/FSharpSpellcheckingStrategy.kt
@@ -0,0 +1,6 @@
+package com.jetbrains.rider.plugins.fsharp.spellchecker
+
+import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.FSharpLanguage
+import com.jetbrains.rider.spellChecker.BackendLanguageSpellcheckingStrategy
+
+class FSharpSpellcheckingStrategy : BackendLanguageSpellcheckingStrategy(FSharpLanguage)
