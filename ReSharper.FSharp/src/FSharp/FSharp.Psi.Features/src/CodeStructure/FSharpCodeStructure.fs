ce15486acdb5762d65a82d4c30964c1b513a4f69
Alexander Karaev <Alexander.Karaev@jetbrains.com>
2024-02-12 11:34:20 +0200

    [radler] CPP-37193: Rename `IRiderFeatureZone` -> `IRiderBackendFeatureZone`
    * Preparation for splitting Rider and CLion (Radler)
    
    GitOrigin-RevId: 66f5ab6d6b1e28e2f308644995e426291eef50dc

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeStructure/FSharpCodeStructure.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeStructure/FSharpCodeStructure.fs
@@ -25,7 +25,7 @@ open JetBrains.UI.RichText
 open JetBrains.Util
 
 [<SolutionComponent>]
-[<ZoneMarker(typeof<IResharperHostCoreFeatureZone>, typeof<IRiderProductEnvironmentZone>, typeof<IRiderFeatureZone>)>]
+[<ZoneMarker(typeof<IResharperHostCoreFeatureZone>, typeof<IRiderProductEnvironmentZone>, typeof<IRiderBackendFeatureZone>)>]
 type FSharpExtendedFileStructureLanguage() =
     let filters : IStructureTreeElementFilter array = [| StructureTreeElementFilter() |]
     interface IExtendedFileStructureLanguage with
--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/Fsi/FsiOptionsPage.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/Fsi/FsiOptionsPage.fs
@@ -145,7 +145,7 @@ type FsiOptionsPage(lifetime: Lifetime, optionsPageContext, settings, settingsSc
 
 
 [<ShellComponent>]
-[<ZoneMarker(typeof<IRiderProductEnvironmentZone>, typeof<IRiderFeatureZone>)>]
+[<ZoneMarker(typeof<IRiderProductEnvironmentZone>, typeof<IRiderBackendFeatureZone>)>]
 type FSharpSettingsCategoryProvider() =
     let [<Literal>] categoryKey = "F# Interactive settings"
 
