751877ec53a43ef2820efa7c75a4c9dbe6927531
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2024-02-13 15:36:36 +0100

    Completion/postfix: add 'with' objExpr template

---
+++ ReSharper.FSharp/test/data/features/completion/postfix/Obj - Class 02.fs
@@ -0,0 +1,9 @@
+﻿// ${COMPLETE_ITEM:with}
+module Module
+
+[<AbstractClass>]
+type Base() =
+    abstract M: int -> unit
+    abstract M: System.Text.StringBuilder -> unit
+
+Base().{caret}
---
+++ ReSharper.FSharp/test/data/features/completion/postfix/Obj - Interface - Generic 01.fs
@@ -0,0 +1,8 @@
+﻿// ${COMPLETE_ITEM:with}
+module Module
+
+type IBase<'T> =
+    abstract M: string -> unit
+    abstract M: 'T -> unit
+
+IBase<int>.{caret}
