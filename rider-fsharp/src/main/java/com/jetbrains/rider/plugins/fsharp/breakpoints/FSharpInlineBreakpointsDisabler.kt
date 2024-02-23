f55e3827dd56c0eb39c1a2a82247fe80046932e8
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2024-02-23 13:56:23 +0000

    pass VirtualFile instead of Document to InlineBreakpointsDisabler
    GitOrigin-RevId: 537841dc02161bf722692f3ea197ff877b37b16a

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/breakpoints/FSharpInlineBreakpointsDisabler.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/breakpoints/FSharpInlineBreakpointsDisabler.kt
@@ -1,13 +1,12 @@
 package com.jetbrains.rider.plugins.fsharp.breakpoints
 
 import com.intellij.lang.LanguageUtil
-import com.intellij.openapi.editor.Document
-import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.xdebugger.breakpoints.InlineBreakpointsDisabler
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.FSharpLanguage
 
 class FSharpInlineBreakpointsDisabler : InlineBreakpointsDisabler {
-  override fun areInlineBreakpointsDisabled(document: Document): Boolean {
-    return LanguageUtil.getFileLanguage(FileDocumentManager.getInstance().getFile(document)) == FSharpLanguage
+  override fun areInlineBreakpointsDisabled(virtualFile: VirtualFile?): Boolean {
+    return LanguageUtil.getFileLanguage(virtualFile) == FSharpLanguage
   }
 }
