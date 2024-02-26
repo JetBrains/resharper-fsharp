8d547f58c12d8750caf270c6292dc1c72ba54558
Arseny.Chernyaev <arseny.chernyaev@jetbrains.com>
2024-02-26 14:31:55 +0000

    add description to FSharpInlineBreakpointsDisabler
    GitOrigin-RevId: 251d99892c71bf593d391c4a106d77e33db7b9c3

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/breakpoints/FSharpInlineBreakpointsDisabler.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/breakpoints/FSharpInlineBreakpointsDisabler.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.xdebugger.breakpoints.InlineBreakpointsDisabler
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.FSharpLanguage
 
+// we have to disable inline breakpoints in F# due to the usability issues described in RIDER-106329
 class FSharpInlineBreakpointsDisabler : InlineBreakpointsDisabler {
   override fun areInlineBreakpointsDisabled(virtualFile: VirtualFile?): Boolean {
     return LanguageUtil.getFileLanguage(virtualFile) == FSharpLanguage
