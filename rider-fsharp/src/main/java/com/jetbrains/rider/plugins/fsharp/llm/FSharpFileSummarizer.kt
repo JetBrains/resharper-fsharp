e7751a683333a1caef1c06339b11198329286639
Nikolai.Palchikov <nikolai.palchikov@jetbrains.com>
2024-05-06 16:40:06 +0200

    LLM-2283 Refactor all the "unsafe" PrivacySafe usages inside llm.core
     1) all string literals and templates that are casted to PSString refactored with .privacyConst usages
     2) several facades for building PSString from platform-dependent informations introduced
    
    GitOrigin-RevId: 1e7a8faa3ea5f9e1d3ba9bb7fdfec939e8001729

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/llm/FSharpFileSummarizer.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/llm/FSharpFileSummarizer.kt
@@ -1,5 +1,7 @@
 package com.jetbrains.rider.plugins.fsharp.llm
 
+import com.intellij.ml.llm.privacy.PSString
+import com.intellij.ml.llm.privacy.trustedStringBuilders.privacySafe
 import com.intellij.ml.llm.smartChat.psiSummarization.LanguageSummaryProvider
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
@@ -13,7 +15,7 @@ import com.jetbrains.rider.projectView.solution
 import kotlinx.coroutines.runBlocking
 
 class FSharpFileSummarizer: LanguageSummaryProvider {
-  override fun generateSummary(psiElement: PsiElement): String? {
+  override fun generateSummary(psiElement: PsiElement): PSString? {
     val psiFile = psiElement.containingFile
     if (psiFile is FSharpFile) {
       val ids: RdDocumentId = PsiDocumentManager.getInstance(psiFile.project).getDocument(psiFile)?.getFirstDocumentId(psiFile.project)
@@ -21,7 +23,8 @@ class FSharpFileSummarizer: LanguageSummaryProvider {
 
       ThreadingAssertions.assertBackgroundThread()
       return runBlocking {
-        return@runBlocking psiFile.project.solution.aIChatModel.getFileSummary.startSuspending(GetFileSummaryParameters(ids)).summary
+        return@runBlocking psiFile.project.solution.aIChatModel.getFileSummary.startSuspending(
+          GetFileSummaryParameters(ids)).summary.privacySafe
       }
     }
     return null
