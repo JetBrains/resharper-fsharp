bc95873f8848ea724b542d514c29b5b9f467b894
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2024-05-31 16:43:01 +0300

    [rider] Add RiderTestDataMarker with testData paths in every module with Rider tests
    GitOrigin-RevId: 2d469bc2f9f9cbf5fb3b5649f4379c3e349344b9

---
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/RiderTestDataMarker.kt
@@ -0,0 +1,13 @@
+package com.jetbrains.rider.plugins.fsharp.test.cases
+
+import com.jetbrains.rider.test.framework.testData.IRiderTestDataMarker
+import java.io.File
+
+@Suppress("unused")
+object RiderTestDataMarker : IRiderTestDataMarker {
+  override val testDataFromRoot: File
+    get() = File("testData")
+
+  override val pluginDirectoryInUltimate: File
+    get() = File("dotnet/Plugins/resharper-fsharp/rider-fsharp")
+}
