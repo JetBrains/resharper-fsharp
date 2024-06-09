11791d3e4b2c5260cbba1dec8ec33846a3ee7ffc
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2024-06-09 16:09:49 +0200

    [rdct] Rename api after removing old rebinding in rider
    GitOrigin-RevId: 790bfdb4d7e88c44b5d725da1fd96449a212681b

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/llm/FSharpFileSummarizer.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/llm/FSharpFileSummarizer.kt
@@ -7,7 +7,7 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.util.concurrency.ThreadingAssertions
 import com.jetbrains.rd.ide.model.RdDocumentId
-import com.jetbrains.rdclient.document.getFirstDocumentId
+import com.jetbrains.rdclient.document.getDocumentId
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.psi.FSharpFile
 import com.jetbrains.rider.model.GetFileSummaryParameters
 import com.jetbrains.rider.model.aIChatModel
@@ -18,7 +18,7 @@ class FSharpFileSummarizer: LanguageSummaryProvider {
   override fun generateSummary(psiElement: PsiElement): PSString? {
     val psiFile = psiElement.containingFile
     if (psiFile is FSharpFile) {
-      val ids: RdDocumentId = PsiDocumentManager.getInstance(psiFile.project).getDocument(psiFile)?.getFirstDocumentId(psiFile.project)
+      val ids: RdDocumentId = PsiDocumentManager.getInstance(psiFile.project).getDocument(psiFile)?.getDocumentId(psiFile.project)
                               ?: return null
 
       ThreadingAssertions.assertBackgroundThread()
