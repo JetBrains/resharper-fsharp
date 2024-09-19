80154b9a50316c32acb1ce204814d4ba7efa7397
Andrey Dyatlov <andrey.dyatlov@jetbrains.com>
2024-09-20 01:53:26 +0200

    Fix compilation
    GitOrigin-RevId: 28bbccda9faaaca13803d86bdb454b1ea849ebb9

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/DisableWarning/DisableWarningActionProvider.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/DisableWarning/DisableWarningActionProvider.fs
@@ -21,7 +21,7 @@ type DisableWarningActionProvider(settingsManager: HighlightingSettingsManager) 
             if isNull severityId then EmptyList.Instance else
 
             let hasCompilerId =
-                not (Seq.isEmpty (settingsManager.GetCompilerIds(highlighting, FSharpLanguage.Instance)))
+                not (Seq.isEmpty (settingsManager.GetCompilerIds(highlighting, FSharpLanguage.Instance, sourceFile.PsiModule)))
             //TODO: suggest #nowarn
             if hasCompilerId then EmptyList.Instance else
 
