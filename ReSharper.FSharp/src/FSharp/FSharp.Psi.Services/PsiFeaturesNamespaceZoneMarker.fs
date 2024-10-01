899899c811444814eea3812c87db4c041655ba2d
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2024-10-01 16:36:07 +0300

    Cleanup zone market

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Services/PsiFeaturesNamespaceZoneMarker.fs
+++
@@ -1,8 +0,0 @@
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features
-
-open JetBrains.Application.BuildScript.Application.Zones
-open JetBrains.ReSharper.Plugins.FSharp
-
-[<ZoneMarker>]
-type ZoneMarker() =
-    interface IRequire<IFSharpPluginZone>
