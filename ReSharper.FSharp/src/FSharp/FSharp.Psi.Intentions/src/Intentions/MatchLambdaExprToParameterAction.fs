aba80b5862de40ec6fd6dff71c79468644bb2dda
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2024-10-31 13:58:13 +0100

    Intentions/match lambda to param: add missing hotspots

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/Intentions/MatchLambdaExprToParameterAction.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/Intentions/MatchLambdaExprToParameterAction.fs
@@ -1,7 +1,10 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Intentions
 
+open System
 open FSharp.Compiler.Symbols
+open JetBrains.ReSharper.Feature.Services.Bulbs
 open JetBrains.ReSharper.Feature.Services.ContextActions
+open JetBrains.ReSharper.Feature.Services.LiveTemplates.Hotspots
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
@@ -32,7 +35,7 @@ type MatchLambdaExprToParameterAction(dataProvider) =
 
         this.IsAtTreeNode(matchLambdaExpr.FunctionKeyword)
 
-    override this.ExecutePsiTransaction _ =
+    override this.ExecutePsiTransaction(_, _) =
         let matchLambdaExpr = dataProvider.GetSelectedElement<IMatchLambdaExpr>()
         let binding = BindingNavigator.GetByExpression(matchLambdaExpr)
 
@@ -41,13 +44,13 @@ type MatchLambdaExprToParameterAction(dataProvider) =
         use disableFormatter = new DisableCodeFormatter()
 
         let mfv = getMfv binding
-        if isNull mfv then () else
+        if isNull mfv then null else
 
         let parameterTypes = FcsTypeUtil.getFunctionTypeArgs true mfv.FullType
         let parameterPatternCount = binding.ParameterPatterns.Count
 
         match parameterTypes |> Seq.skip parameterPatternCount |> Seq.tryHead with
-        | None -> ()
+        | None -> null
         | Some nextParameterType ->
 
         let usedNames = FSharpNamingService.getBindingPatternsUsedNames binding
@@ -57,7 +60,7 @@ type MatchLambdaExprToParameterAction(dataProvider) =
             |> FSharpNamingService.addNamesForFcsType binding nextParameterType
             |> FSharpNamingService.prepareNamesCollection usedNames binding
 
-        let names = List.ofSeq names @ ["_"]
+        let names = List.ofSeq names
         let name = if names.Length > 1 then names[0] else "x"
 
         let parameterDecl =
@@ -81,12 +84,20 @@ type MatchLambdaExprToParameterAction(dataProvider) =
         let afterFunctionKeywordRange = TreeRange(matchLambdaExpr.FunctionKeyword.NextSibling, clauses[0].PrevSibling)
         ModificationUtil.AddChildRangeAfter(binding.EqualsToken |> getLastMatchingNodeAfter isWhitespaceOrComment, afterFunctionKeywordRange) |> ignore
 
-        
-
         let matchExpr = elementFactory.CreateExpr($"match {name} with | _ -> ()") :?> IMatchExpr
         let generatedClausesRange = TreeRange(matchExpr.WithKeyword.NextSibling, matchExpr.LastChild)
 
         let originalClausesRange = TreeRange(clauses[0] |> getFirstMatchingNodeBefore isInlineSpaceOrComment |> getThisOrPrevNewLine, clauses.Last())
 
         ModificationUtil.ReplaceChildRange(generatedClausesRange, originalClausesRange) |> ignore
-        ModificationUtil.ReplaceChild(matchLambdaExpr, matchExpr) |> ignore
+        let matchExpr = ModificationUtil.ReplaceChild(matchLambdaExpr, matchExpr)
+
+        let hotspotsRegistry = HotspotsRegistry(matchExpr.GetPsiServices())
+        let nodes: ITreeNode list = [parameterDecl; matchExpr.Expression]
+        hotspotsRegistry.Register(nodes, NameSuggestionsExpression(names))
+
+        Action<_>(fun texControl ->
+            let offset = parameterDecl.GetDocumentEndOffset()
+            BulbActionUtils.ExecuteHotspotSession(hotspotsRegistry, offset).Invoke(texControl)
+            ()
+        )
