e5f36cecbdeef2869485e61a830ab40cc2a38e7a
Nikita Popov <nikita.popov@jetbrains.com>
2024-11-16 13:19:24 +0100

    [dotnet] rework ProjectHasPsiService
    GitOrigin-RevId: be19a5cfa97e38ca750d069d7ac9ba5bcb51e0f2

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/LanguageService/FSharpClrLanguage.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/LanguageService/FSharpClrLanguage.fs
@@ -1,10 +1,11 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.LanguageService
 
 open JetBrains.Application
+open JetBrains.Application.Parts
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Feature.Services.ClrLanguages
 
-[<ShellComponent>]
+[<ShellComponent(Instantiation.DemandAnyThreadSafe)>]
 type FSharpClrLanguage() =
     interface IClrLanguagesKnown with
         member x.Language = FSharpLanguage.Instance :> _
