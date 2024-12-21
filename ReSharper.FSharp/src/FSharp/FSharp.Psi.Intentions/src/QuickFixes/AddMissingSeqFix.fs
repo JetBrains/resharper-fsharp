7b219afa266261239f792ae94e4f7b49db7f92b2
Edgar Gonzalez <egonzalez@totallymoney.com>
2024-12-21 21:59:01 +0000

    Add initial test

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/AddMissingSeqFix.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/AddMissingSeqFix.fs
@@ -6,24 +6,7 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Psi.ExtensionsAPI
 open JetBrains.ReSharper.Resources.Shell
-open System
-open System.Text.RegularExpressions
-open FSharp.Compiler.Symbols
-open JetBrains.ReSharper.Feature.Services.Navigation.CustomHighlighting
-open JetBrains.ReSharper.Feature.Services.Refactorings.WorkflowOccurrences
-open JetBrains.ReSharper.Plugins.FSharp.Psi
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.Highlightings
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util.FcsTypeUtil
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
-open JetBrains.ReSharper.Psi.ExtensionsAPI
 open JetBrains.ReSharper.Psi.ExtensionsAPI.Tree
-open JetBrains.ReSharper.Psi.Tree
-open JetBrains.ReSharper.Resources.Shell
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
-open JetBrains.ProjectModel
-open JetBrains.UI.RichText
 
 type AddMissingSeqFix(expr: IComputationExpr) =
     inherit FSharpQuickFixBase()
@@ -40,4 +23,12 @@ type AddMissingSeqFix(expr: IComputationExpr) =
         isValid expr
 
     override x.ExecutePsiTransaction _ =
-        ()
+        use writeCookie = WriteLockCookie.Create(expr.IsPhysical())
+        use disableFormatter = new DisableCodeFormatter()
+        
+        let factory = expr.Expression.CreateElementFactory()
+        let seqExpr = factory.CreateExpr("seq")
+        let newAppExpr = factory.CreateAppExpr(seqExpr, expr, true)
+        
+        ModificationUtil.ReplaceChild(expr, newAppExpr) |> ignore  
+        
