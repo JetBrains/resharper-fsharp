22f5bb6c96b4bf340b2ea55941154b0640d3522c
Leonid Shalupov <leonid@shalupov.com>
2025-02-03 16:19:43 +0100

    housekeeping: remove unused imports
    GitOrigin-RevId: be93bd341860bfc077fdbff5ce2ae91eebada31e

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/FSharpBundle.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/FSharpBundle.kt
@@ -4,7 +4,6 @@ import com.intellij.DynamicBundle
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.util.function.Supplier
 
 @NonNls
 private const val BUNDLE = "messages.FSharpBundle"
