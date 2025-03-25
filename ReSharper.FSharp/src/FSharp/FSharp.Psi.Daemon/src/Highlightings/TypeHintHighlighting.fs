5e7926230f481dde180f8d61001531db5958b725
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-03-25 17:04:40 +0300

    Inlay Hints: add 'copy inferred type' action (#761)

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Daemon/src/Highlightings/TypeHintHighlighting.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Daemon/src/Highlightings/TypeHintHighlighting.fs
@@ -3,12 +3,16 @@
 open System
 open JetBrains.Application.Parts
 open JetBrains.Application.Settings
+open JetBrains.Application.UI.Controls.BulbMenu.Anchors
+open JetBrains.Application.UI.Controls.BulbMenu.Items
 open JetBrains.DocumentModel
 open JetBrains.ProjectModel
 open JetBrains.ReSharper.Feature.Services.Daemon.Attributes
 open JetBrains.ReSharper.Feature.Services.Daemon
 open JetBrains.ReSharper.Feature.Services.InlayHints
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Daemon.Common.ActionUtils
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Daemon.Options
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Daemon.Resources
 open JetBrains.TextControl.DocumentMarkup.Adornments
 open JetBrains.UI.RichText
 
@@ -36,12 +40,19 @@ type TypeHintHighlighting(typeNameString: string, range: DocumentRange, pushToHi
         member x.TestOutput = text.Text
 
     member x.Text = text
+    member x.TypeText = typeNameString
     member x.PushToHintMode = pushToHintMode
     member x.BulbActionsProvider = bulbActionsProvider
     member x.IsValid() = not text.IsEmpty && range.IsEmpty
 
 and [<SolutionComponent(Instantiation.DemandAnyThreadSafe)>]
     TypeHintAdornmentProvider(settingsStore: ISettingsStore) =
+
+    let createCopyToClipboardBulbItem (highlighting: TypeHintHighlighting) highlighter =
+        let text = highlighting.TypeText
+        BulbMenuItem(ExecutableItem(fun () -> copyToClipboard text highlighter),
+                     Strings.FSharpInferredTypeHighlighting_TooltipText, null, BulbMenuAnchors.FirstClassContextItems)
+
     interface IHighlighterAdornmentProvider with
         member x.IsValid(highlighter) =
             match highlighter.GetHighlighting() with
@@ -60,6 +71,8 @@ and [<SolutionComponent(Instantiation.DemandAnyThreadSafe)>]
                     override x.ContextMenuTitle = null
                     override x.ContextMenuItems =
                         [|
+                            yield createCopyToClipboardBulbItem thh highlighter
+
                             if isNotNull actionsProvider then
                                 yield! actionsProvider.CreateChangeVisibilityBulbMenuItems(settingsStore, thh)
 
