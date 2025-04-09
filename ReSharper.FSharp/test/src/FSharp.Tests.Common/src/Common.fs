9b74dd8c4e3ab9f5c40016bfbe6daaca2934cc64
Mikhail Pilin <mikhail@pilin.name>
2025-04-09 20:14:45 +0200

    [dotnet] Use different resources for internal and external CI.
    GitOrigin-RevId: c0c2db3b43b04028cea360c8b116f5380eb1516a

--- ReSharper.FSharp/test/src/FSharp.Tests.Common/src/Common.fs
+++ ReSharper.FSharp/test/src/FSharp.Tests.Common/src/Common.fs
@@ -87,7 +87,7 @@ type TestReferenceProjectOutput(projectName: string) =
         member this.Inherits = true
 
         member this.GetReferences(test, packageManager, targetFrameworkId) =
-            let compiler = DotNetTestCodeCompiler(test.TestLifetime, DotNetTestCodeCompiler.ContinuousIntegrationMode.External)
+            let compiler = DotNetTestCodeCompiler(test.TestLifetime, ContinuousIntegration.External)
             let slnPath = test.BaseTestDataPath / "TestAssembliesSources" / "TestAssembliesSources.sln"
 
             let compileResult =
