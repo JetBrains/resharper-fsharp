b237911ac666bdd80f867574c0eedb065c467bec
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-04-15 20:21:19 +0300

    SpecifyTypeActionProvider: add 'deadlock safe' annotation

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Services/src/Intentions/ISpecifyTypeActionsProvider.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Services/src/Intentions/ISpecifyTypeActionsProvider.fs
@@ -1,8 +1,10 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Intentions
 
+open JetBrains.Application.Parts
 open JetBrains.Application.UI.Controls.BulbMenu.Items
 open JetBrains.ReSharper.Psi.Tree
 
 [<Interface>]
+[<DerivedComponentsInstantiationRequirement(InstantiationRequirement.DeadlockSafe)>]
 type ISpecifyTypeActionProvider =
     abstract member TryCreateSpecifyTypeAction: node: ITreeNode -> BulbMenuItem
