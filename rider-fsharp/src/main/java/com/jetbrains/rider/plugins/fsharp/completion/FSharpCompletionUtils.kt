454d73379f7efa0ca971e634ced79b7798731a52
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-04-24 01:23:58 +0300

    Frontend/Nuget Completion: fix completion context mutation

--- rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/completion/FSharpCompletionUtils.kt
+++ rider-fsharp/src/main/java/com/jetbrains/rider/plugins/fsharp/completion/FSharpCompletionUtils.kt
@@ -1,25 +1,30 @@
 package com.jetbrains.rider.plugins.fsharp.completion
 
 import com.intellij.lang.ASTNode
+import com.intellij.psi.PsiFile
 import com.intellij.psi.impl.source.tree.TreeUtil
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.lexer.FSharpTokenType
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.psi.FSharpFile
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.psi.FSharpStringLiteralExpression
 import com.jetbrains.rider.ideaInterop.fileTypes.fsharp.psi.FSharpStringLiteralType
 
-// check for #r "... <caret> ..." 
-fun insideReferenceDirective(file: FSharpFile, offset: Int): Boolean {
+// try to get string for #r "... <caret> ..."
+fun getStringInsideReferenceDirective(file: PsiFile, offset: Int): FSharpStringLiteralExpression? {
+  if (file !is FSharpFile) return null
+
   val string = file.findElementAt(offset)?.parent
-  if (string !is FSharpStringLiteralExpression) return false
+  if (string !is FSharpStringLiteralExpression) return null
 
   return when (string.literalType) {
     FSharpStringLiteralType.RegularString,
     FSharpStringLiteralType.VerbatimString,
     FSharpStringLiteralType.TripleQuoteString -> {
       val previousNode = TreeUtil.skipWhitespaceAndComments(string.prevSibling as? ASTNode, false)
-      previousNode?.elementType == FSharpTokenType.PP_REFERENCE
+      return if (previousNode?.elementType == FSharpTokenType.PP_REFERENCE) string else null
     }
 
-    else -> false
+    else -> null
   }
 }
+
+fun insideReferenceDirective(file: PsiFile, offset: Int) = getStringInsideReferenceDirective(file, offset) != null
