2b6e8adc1955fab896c50955b9aaabbc1bca50cb
Andrey Dyatlov <Andrey.Dyatlov@jetbrains.com>
2025-06-12 23:24:58 +0200

    [dotnet] Setup F# syntax checker from existing source file when possible
    GitOrigin-RevId: 96d570f5410e1745ce38fe6e24be0dfc38473d0a

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/AI/ErrorCheck/FSharpBackendSyntaxErrorChecker.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/AI/ErrorCheck/FSharpBackendSyntaxErrorChecker.fs
@@ -11,6 +11,7 @@ open JetBrains.ReSharper.Feature.Services.AI
 open JetBrains.ReSharper.Plugins.FSharp.Checker
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.LanguageService.Parsing
+open JetBrains.ReSharper.Psi
 open JetBrains.Rider.Model
 open JetBrains.Util
 
@@ -18,15 +19,19 @@ open JetBrains.Util
 [<SolutionComponent(Instantiation.DemandAnyThreadSafe)>]
 type FSharpBackendSyntaxErrorChecker(checkerService: FcsCheckerService, documentFactory: IInMemoryDocumentFactory) =    
     let convertDiagnostic(document: IDocument) (diagnostic: FSharpDiagnostic) :BackendError =
-                let startOffset = getDocumentOffset document (docCoords diagnostic.StartLine diagnostic.StartColumn)
-                BackendError(startOffset.Offset, diagnostic.Message)
+        let startOffset = getDocumentOffset document (docCoords diagnostic.StartLine diagnostic.StartColumn)
+        BackendError(startOffset.Offset, diagnostic.Message)
 
     interface ICustomSyntaxErrorChecker with
         member this.IsAvailable(language) = language :? FSharpLanguage
         
-        member this.CheckSyntaxErrors(lifetime, psiModule, modifiedContent)=
+        member this.CheckSyntaxErrors(lifetime, psiModule, modifiedContent, sourceFile)=
+            let parsingOptions = if isNotNull sourceFile then checkerService.FcsProjectProvider.GetParsingOptions(sourceFile) else sandboxParsingOptions
+            let path = if isNotNull sourceFile then sourceFile.GetLocation() else FSharpParser.SandBoxPath
             let document = documentFactory.CreateSimpleDocumentFromText(modifiedContent, "F# Sandbox File for Syntax Check")
-            let parsingResult = checkerService.ParseFile(FSharpParser.SandBoxPath, document, sandboxParsingOptions, true)
+            
+            let parsingResult = checkerService.ParseFile(path, document, parsingOptions, noCache = true)
+            
             match parsingResult with
             | None -> EmptyList<BackendError>.InstanceList
             | Some result ->
