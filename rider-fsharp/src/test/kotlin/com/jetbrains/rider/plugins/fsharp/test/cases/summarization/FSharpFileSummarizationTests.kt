88fe401be898b16c650070307d4cfd5791292f3f
Ivan Migalev <ivan.migalev@jetbrains.com>
2025-07-18 20:44:19 +0200

    (RIDER-127969) Junie: add tests for F#
    (cherry picked from commit f746d79d150121416641a32b97323ba77ccfb528)
    
    IJ-CR-169809
    
    GitOrigin-RevId: 37f68d5687934812e48cb82e2f029ee307a4cf3a

---
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/summarization/FSharpFileSummarizationTests.kt
@@ -0,0 +1,56 @@
+package com.jetbrains.rider.plugins.fsharp.test.cases.summarization
+
+import com.intellij.openapi.components.serviceAsync
+import com.intellij.platform.backend.workspace.WorkspaceModel
+import com.jetbrains.rd.ide.model.FileSummaryRequest
+import com.jetbrains.rd.ide.model.junieModel
+import com.jetbrains.rdclient.util.idea.toIOFile
+import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.projectView.solutionDirectory
+import com.jetbrains.rider.projectView.workspace.getId
+import com.jetbrains.rider.projectView.workspace.getProjectModelEntities
+import com.jetbrains.rider.protocol.protocol
+import com.jetbrains.rider.test.annotations.Solution
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.asserts.shouldNotBeNull
+import com.jetbrains.rider.test.base.PerTestSolutionTestBase
+import com.jetbrains.rider.test.env.enums.SdkVersion
+import com.jetbrains.rider.test.framework.executeWithGold
+import com.jetbrains.rider.test.scriptingApi.changeFileSystem2
+import com.jetbrains.rider.test.scriptingApi.getVirtualFileFromPath
+import com.jetbrains.rider.test.scriptingApi.replaceFileContent
+import com.jetbrains.rider.test.scriptingApi.runBlockingWithProtocolPumping
+import org.testng.annotations.Test
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.div
+import kotlin.io.path.name
+
+class FSharpFileSummarizationTests : PerTestSolutionTestBase() {
+
+  @TestEnvironment(sdkVersion = SdkVersion.LATEST_STABLE)
+  @Solution("CoreConsoleApp")
+  @Test
+  fun testFSharpFileSummarization() = doTest(Path("Program.fs"))
+
+  private fun doTest(solutionRelativeFilePath: Path) {
+    changeFileSystem2(project) {
+      replaceFileContent(project, solutionRelativeFilePath.name)
+      arrayOf(getVirtualFileFromPath(solutionRelativeFilePath.name, project.solutionDirectory).toIOFile())
+    }
+
+    executeWithGold(testGoldFile) { gold ->
+      val summary = runBlockingWithProtocolPumping(project.protocol, testMethod.name) {
+        val projectModelEntityId = project.serviceAsync<WorkspaceModel>()
+          .getProjectModelEntities(project.solutionDirectory.toPath() / solutionRelativeFilePath, project)
+          .single()
+          .getId(project).shouldNotBeNull("Project model id should not be null")
+
+        val model = project.solution.junieModel
+        model.getFileSummary.startSuspending(FileSummaryRequest(projectModelEntityId)).summary
+      }
+
+      gold.print(summary)
+    }
+  }
+}
