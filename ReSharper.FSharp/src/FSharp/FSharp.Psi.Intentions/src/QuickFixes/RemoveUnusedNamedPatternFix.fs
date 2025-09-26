dc60995d805ca9f45573b9272ae982ed342818a6
Edgar Gonzalez <edgargonzalez.info@gmail.com>
2025-09-26 21:39:49 +0100

    Delete pattern nodes and let the formatter handle newlines and whitespaces

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/RemoveUnusedNamedPatternFix.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/RemoveUnusedNamedPatternFix.fs
@@ -10,63 +10,6 @@ type RemoveUnusedNamedPatternFix(warning: UnusedValueWarning) =
     
     let fieldPat = FieldPatNavigator.GetByPattern(warning.Pat)
     let listPat = NamedPatternsListPatNavigator.GetByFieldPattern(fieldPat)
-    
-    /// Remove the middle field and keep separators/comments stable.
-    let deleteMiddleFieldPreservingTrailingComment (prevFieldPat: IFieldPat) (fieldPat: IFieldPat) (nextFieldPat: IFieldPat) =
-        let fieldPatNode = fieldPat.Pattern
-        // preserve an inline comment on the same line as the field being deleted
-        let trailingComment = findSubsequentComment fieldPatNode
-        // Decide rangeStart so that we preserve the preceding semicolon when prev/field/next are on the same line.
-        // - If prev and field are on the same line AND next is also on the same line, start at the field itself
-        //   to keep the semicolon after prev (so it becomes the separator between prev and next).
-        // - If prev and field are on the same line BUT next starts on the next line, start from the token after prev
-        //   to remove the trailing semicolon after prev. (We don't want a dangling semicolon at the end of the line.)
-        // - Otherwise, start at the field itself to avoid consuming the preceding newline.
-        let prevSameLine = isNotNull prevFieldPat && prevFieldPat.EndLine = fieldPat.EndLine
-        let nextSameLine = nextFieldPat.StartLine = fieldPat.EndLine
-        let erasePrevSemicolon = prevSameLine && not nextSameLine && shouldEraseSemicolon prevFieldPat
-
-        let rangeStart =
-            if erasePrevSemicolon then prevFieldPat.NextSibling else fieldPat
-        // If there is an inline comment on the same line, stop before it; otherwise stop before the newline
-        // preceding the next field to keep the next line and its indentation intact.
-        let rangeEnd =
-            if isNotNull trailingComment then trailingComment.PrevSibling
-            else
-                if fieldPat.EndLine < nextFieldPat.StartLine then
-                    let beforeNext = getFirstMatchingNodeBefore isInlineSpaceOrComment nextFieldPat
-                    let maybeNewLine = getThisOrPrevNewLine beforeNext
-                    // If we are removing the first field and the next field starts on a new line,
-                    // also remove the newline and its indentation so that the opening '{' stays
-                    // on the same line as the first remaining field (if any).
-                    if isNull prevFieldPat && isNotNull maybeNewLine && isNewLine maybeNewLine then
-                        beforeNext
-                    else
-                        if isNotNull maybeNewLine && isNewLine maybeNewLine then maybeNewLine.PrevSibling else nextFieldPat.PrevSibling
-                else
-                    nextFieldPat.PrevSibling
-
-        deleteChildRange rangeStart rangeEnd
-
-    /// Remove the last field; if the previous field has an inline end-of-line comment, keep it attached to the previous field.
-    let liftPrevInlineCommentThenRemoveLast (prevFieldPat: IFieldPat) (lastFieldPat: IFieldPat) =
-        let prevFieldNode = prevFieldPat.Pattern
-        // Finds an inline // comment on the same line as the given node (if any).
-        let inlineCommentOnSameLine node =
-            let comment = findSubsequentComment node
-            if isNotNull comment && comment.EndLine = node.EndLine then comment else null
-
-        let inlineComment = inlineCommentOnSameLine prevFieldNode
-
-        let lastWithTrivia =
-            lastFieldPat
-            |> skipSemicolonsAndWhiteSpacesAfter
-            |> getThisOrNextNewLine // keep the next line intact if any
-
-        let startOfDeletion =
-            if isNotNull inlineComment then inlineComment.NextSibling else prevFieldPat.NextSibling
-
-        deleteChildRange startOfDeletion lastWithTrivia
 
     override x.Text = $"Remove unused pattern"
 
@@ -93,12 +36,17 @@ type RemoveUnusedNamedPatternFix(warning: UnusedValueWarning) =
         if fieldPatterns.Count = 1 then
             collapseSingleNamedField listPat
         else
-            let indexPat = fieldPatterns.IndexOf(fieldPat) // index of the unused field
-            let isLast = indexPat = fieldPatterns.Count - 1 // is the unused field the last one?
-
-            if not isLast then
-                let prevPat = if indexPat > 0 then fieldPatterns[indexPat - 1] else null
-                deleteMiddleFieldPreservingTrailingComment prevPat fieldPat fieldPatterns[indexPat + 1]
+            // Prefer removing a preceding semicolon (…; field) to avoid leaving a dangling separator, scanning back over whitespace and comments.
+            let prevNonWsOrComment = skipMatchingNodesBefore isWhitespaceOrComment fieldPat
+            if isNotNull prevNonWsOrComment && prevNonWsOrComment != fieldPat && isSemicolon prevNonWsOrComment then
+                deleteChildRange prevNonWsOrComment fieldPat
             else
-                liftPrevInlineCommentThenRemoveLast fieldPatterns[indexPat - 1] fieldPat
+                // Otherwise, if the next sibling skipping whitespace or comments is a semicolon (field ; …), remove it too.
+                let nextNonWsOrComment = skipMatchingNodesAfter isWhitespaceOrComment fieldPat
+                if isNotNull nextNonWsOrComment && isSemicolon nextNonWsOrComment then
+                    deleteChildRange fieldPat nextNonWsOrComment
+                else
+                    // No adjacent separator: just delete the field and let the formatter handle whitespace/newlines.
+                    deleteChild fieldPat
+
 
