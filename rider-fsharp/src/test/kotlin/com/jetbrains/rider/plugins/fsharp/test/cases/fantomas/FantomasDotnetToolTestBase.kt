3c7466399306615e0905cf61ab3e10066ddd638e
Ilnar Agliamzianov <ilnar.agliamzianov@jetbrains.com>
2025-10-17 17:46:41 +0300

    Test tools are not different per test environment
    GitOrigin-RevId: 27f2e104442f49e6f6ea3bae0bdf2035c05d134f

--- rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/fantomas/FantomasDotnetToolTestBase.kt
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/fantomas/FantomasDotnetToolTestBase.kt
@@ -91,7 +91,7 @@ abstract class FantomasDotnetToolTestBase : EditorTestBase() {
 
       withDotnetToolsUpdate {
         runProcessWaitForExit(
-          testTools().build.dotNetSdk[testMethod.settings.sdk].dotnetExecutable.toPath(),
+          testTools(testEnvironment).build.dotNetSdk[testMethod.settings.sdk].dotnetExecutable.toPath(),
           listOf("tool", "install", "fantomas-tool", "-g", "--version", globalVersion),
           env
         )
