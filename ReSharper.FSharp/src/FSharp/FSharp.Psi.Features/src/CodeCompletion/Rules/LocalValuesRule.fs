f09bc1b38a44250ca53f39cbdecc3b3d1be2edd4
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2025-11-10 17:12:08 +0100

    RIDER-16740 Debugger: fix missing local F# values in editor
    GitOrigin-RevId: fb9c7d66cd30bc4ee0ff49d670fa0ccfbb66791a

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeCompletion/Rules/LocalValuesRule.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeCompletion/Rules/LocalValuesRule.fs
@@ -1,8 +1,6 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.CodeCompletion.Rules
 
-open System.Collections.Generic
 open FSharp.Compiler.CodeAnalysis
-open JetBrains.Application
 open JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure
 open JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.AspectLookupItems.BaseInfrastructure
 open JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.AspectLookupItems.Behaviors
@@ -13,12 +11,11 @@ open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.CodeCompletion
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util.FcsTypeUtil
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Services.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Util
 open JetBrains.ReSharper.Psi
-open JetBrains.ReSharper.Psi.Tree
 open JetBrains.UI.RichText
-open JetBrains.Util
 
 type FcsSymbolInfo(text, symbolUse: FSharpSymbolUse) =
     inherit TextualInfo(text, text)
@@ -30,116 +27,6 @@ type FcsSymbolInfo(text, symbolUse: FSharpSymbolUse) =
         member this.FcsSymbol = if isNotNull symbolUse then symbolUse.Symbol else Unchecked.defaultof<_>
         member this.FcsSymbolUse = symbolUse
         member this.NamespaceToOpen = [||]
-
-module rec LocalValuesRule =
-    let valuesKey = Key<IDictionary<string, FSharpSymbolUse>>(nameof LocalValuesRule)
-
-    let getLocalValues (context: ITreeNode) : IDictionary<_, _> =
-        let values = Dictionary<string, FSharpSymbolUse>()
-
-        let addValue (decl: IFSharpDeclaration) =
-            let sourceName = decl.SourceName
-            if sourceName <> "_" then
-                values.TryAdd(sourceName, decl.GetFcsSymbolUse()) |> ignore
-
-        let addPatternValues (pat: IFSharpPattern) =
-            if isNull pat then () else
-
-            for declPattern in pat.Declarations do
-                let pat = declPattern.As<IFSharpPattern>()
-                if pat.IsDeclaration then
-                    addValue declPattern
-
-        let addLetBindingsValues (letBindings: ILetBindings) =
-            for otherBinding in letBindings.Bindings do
-                addPatternValues otherBinding.HeadPattern
-
-        let addSelfIdValue (selfId: ISelfId) =
-            if isNotNull selfId then
-                addValue selfId
-
-        let addMembersValues (contextOffset: int) (members: ITreeNode seq) =
-            let members = members |> Seq.takeWhile (fun m -> m.GetTreeStartOffset().Offset < contextOffset)
-
-            for otherMember in members do
-                match otherMember with
-                | :? ILetBindings as letBindings ->
-                    for binding in letBindings.Bindings do
-                        addPatternValues binding.HeadPattern
-
-                | _ -> ()
-
-        for parentNode in context.ContainingNodes() do
-            Interruption.Current.CheckAndThrow()
-
-            match parentNode with
-            | :? IMatchClause as matchClause ->
-                addPatternValues matchClause.Pattern
-
-            | :? IBinding as binding ->
-                Seq.iter addPatternValues binding.ParameterPatterns
-
-                let letBindings = LetBindingsNavigator.GetByBinding(binding)
-                if isNotNull letBindings && letBindings.IsRecursive then
-                    addLetBindingsValues letBindings
-
-            | :? IAccessorDeclaration as decl ->
-                Seq.iter addPatternValues decl.ParameterPatternsEnumerable
-
-            | :? IFSharpTypeDeclaration as typeDecl ->
-                let ctorDecl = typeDecl.PrimaryConstructorDeclaration
-                if isNotNull ctorDecl then
-                    addPatternValues ctorDecl.ParameterPatterns
-                    addSelfIdValue ctorDecl.SelfIdentifier
-
-            | :? IFSharpExpression as expr ->
-                let letExpr = LetOrUseExprNavigator.GetByInExpression(expr)
-                if isNotNull letExpr then
-                    addLetBindingsValues letExpr
-
-                match expr with
-                | :? ILambdaExpr as lambdaExpr ->
-                    for pattern in lambdaExpr.PatternsEnumerable do
-                        addPatternValues pattern
-
-                | :? IForEachExpr as forExpr ->
-                    addPatternValues forExpr.Pattern
-
-                | :? IForExpr as forExpr ->
-                    addValue forExpr.Identifier
-
-                | _ -> ()
-
-            | :? IModuleMember as moduleMember ->
-                let moduleDecl = ModuleLikeDeclarationNavigator.GetByMember(moduleMember)
-                if isNotNull moduleDecl then
-                    let isRecursive =
-                        match moduleDecl with
-                        | :? IDeclaredModuleLikeDeclaration as moduleDecl -> moduleDecl.IsRecursive
-                        | _ -> false
-
-                    let contextOffset =
-                        if isRecursive then moduleDecl.GetTreeEndOffset() else moduleMember.GetTreeStartOffset()
-
-                    addMembersValues contextOffset.Offset (moduleDecl.MembersEnumerable |> Seq.cast)
-
-                let typeDecl = FSharpTypeDeclarationNavigator.GetByTypeMember(moduleMember.As())
-                if isNotNull typeDecl then
-                    addMembersValues (moduleMember.GetTreeStartOffset().Offset) (typeDecl.TypeMembersEnumerable |> Seq.cast)
-
-            | :? ITypeBodyMemberDeclaration as typeMember ->
-                let typeDecl = FSharpTypeDeclarationNavigator.GetByTypeMember(typeMember)
-                if isNotNull typeDecl then
-                    addMembersValues (typeMember.GetTreeStartOffset().Offset) (typeDecl.TypeMembersEnumerable |> Seq.cast)
-
-                let memberDecl = typeMember.As<IMemberDeclaration>()
-                if isNotNull memberDecl then
-                    addSelfIdValue memberDecl.SelfId
-                    Seq.iter addPatternValues memberDecl.ParameterPatterns
-
-            | _ -> ()
-
-        values
 
 [<Language(typeof<FSharpLanguage>)>]
 type LocalValuesRule() =
@@ -157,9 +44,9 @@ type LocalValuesRule() =
     override this.AddLookupItems(context, collector) =
         let values =
             let treeNode = context.ReparsedContext.TreeNode
-            context.GetOrCreateDataUnderLock(LocalValuesRule.valuesKey, treeNode, LocalValuesRule.getLocalValues)
+            context.GetOrCreateDataUnderLock(LocalValuesUtil.valuesKey, treeNode, LocalValuesUtil.getLocalValues)
 
-        for KeyValue(name, fcsSymbolUse) in values do
+        for KeyValue(name, (_, fcsSymbolUse)) in values do
             let icon = if isNull fcsSymbolUse then null else getIconId fcsSymbolUse.Symbol
 
             let info = FcsSymbolInfo(name, fcsSymbolUse, Ranges = context.Ranges)
@@ -181,7 +68,7 @@ type LocalValuesRule() =
         false
 
     override this.TransformItems(context, collector) =
-        let values = context.GetData(LocalValuesRule.valuesKey)
+        let values = context.GetData(LocalValuesUtil.valuesKey)
         if isNull values then () else
 
         collector.RemoveWhere(fun item ->
--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/Debugger/FSharpDebuggerLocalSymbolProvider.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/Debugger/FSharpDebuggerLocalSymbolProvider.fs
@@ -1,79 +1,25 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Services.Debugger
 
-open FSharp.Compiler.Syntax
-open FSharp.Compiler.Text
-open JetBrains.DocumentModel
 open JetBrains.ReSharper.Feature.Services.Debugger
 open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Services.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
-open JetBrains.ReSharper.Plugins.FSharp.Util
 open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.Tree
 
 [<Language(typeof<FSharpLanguage>)>]
-type FSharpDebuggerLocalSymbolProvider() =
+type FSharpDebuggerLocalSymbolProvider2() =
     interface IDebuggerLocalSymbolProvider with
-        member x.FindLocalDeclarationAt(file: IFile, range: DocumentRange, name: string) =
-            match file.AsFSharpFile() with
-            | null -> null, null
-            | fsFile ->
+        member this.FindLocalDeclarationAt(file, range, name) =
+            let token = file.FindTokenAt(range.StartOffset)
+            if isNull token then null, null else
 
-            match fsFile.ParseTree with
-            | None -> null, null
-            | Some parseTree ->
+            let values = LocalValuesUtil.getLocalValues token
+            match values.TryGetValue(name) with
+            | false, _ -> null, null
+            | true, (decl, _) -> decl, decl.DeclaredElement
 
-            let pos = getPosFromDocumentOffset range.EndOffset
-            let mutable declRange = None
-
-            let visitor =
-                let inline (|SuitableIdent|_|) (ident: Ident) =
-                    if ident.idText = name && (Position.posLt ident.idRange.End pos || Position.posEq ident.idRange.End pos) then
-                        Some ident.idRange
-                    else None
-
-                let updateDeclRange (range: Range) =
-                    match declRange with
-                    | None ->
-                        declRange <- Some range
-                    | Some oldDeclRange when Position.posGt range.Start oldDeclRange.Start ->
-                        declRange <- Some range
-                    | _ -> ()
-
-                let visitPat pat defaultTraverse =
-                    match pat with
-                    | SynPat.Named(SynIdent(SuitableIdent range, _), false, _, _) -> updateDeclRange range
-                    | _ -> ()
-                    defaultTraverse pat
-
-                { new SyntaxVisitorBase<_>() with
-                    member this.VisitExpr(_, traverseSynExpr, defaultTraverse, expr) =
-                        match expr with
-                        | SynExpr.For(ident = SuitableIdent range) -> updateDeclRange range
-                        | SynExpr.ForEach(pat = pat) -> visitPat pat ignore
-                        | SynExpr.App(argExpr = SynExpr.Ident (SuitableIdent range)) -> updateDeclRange range
-                        | _ -> ()
-                        defaultTraverse expr
-
-                    member this.VisitPat(_, defaultTraverse, pat) = visitPat pat defaultTraverse
-
-                    member this.VisitLetOrUse(_, _, _, bindings, range) =
-                        bindings |> List.tryPick (fun (SynBinding(headPat = headPat)) ->
-                            visitPat headPat (fun _ -> None))
-
-                    member this.VisitMatchClause(_, defaultTraverse, (SynMatchClause(pat, _, _, _, _, _) as clause)) =
-                        visitPat pat (fun _ -> None) |> Option.orElseWith (fun _ -> defaultTraverse clause) }
-
-            SyntaxTraversal.Traverse(pos, parseTree, visitor) |> ignore
-
-            match declRange with
-            | Some declRange ->
-                let endOffset = getTreeEndOffset range.Document declRange
-                let treeNode = fsFile.FindTokenAt(endOffset - 1)
-                match treeNode.GetContainingNode<IFSharpDeclaration>() with
-                | null -> treeNode, null
-                | declaration -> treeNode, declaration.DeclaredElement
-
-            | None -> null, null
-
-        member x.FindContainingFunctionDeclarationBody(treeNode) =
-            treeNode // todo: refactor to get tree node instead of function declaration in SDK
+        member this.FindContainingFunctionDeclarationBody(node) =
+            match node.GetContainingNode<IChameleonExpression>() with
+            | null -> node.GetContainingFile()
+            | chameleonExpr -> chameleonExpr
