f72d75c428a134ac25b79eea5de2e5d453ddc448
Ilnar Agliamzianov <ilnar.agliamzianov@jetbrains.com>
2025-11-18 14:55:09 +0200

    test [Rider]: rename RiderTestEnvironment and related classes
    GitOrigin-RevId: d13c5c894fde5507ee3322d0119b8b06ff5ca54d

--- rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/fantomas/FantomasDotnetToolTestBase.kt
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/fantomas/FantomasDotnetToolTestBase.kt
@@ -91,7 +91,7 @@ abstract class FantomasDotnetToolTestBase : EditorTestBase() {
 
       withDotnetToolsUpdate {
         runProcessWaitForExit(
-          testTools(testEnvironment).build.dotNetSdk[testMethod.settings.sdk].dotnetExecutable.toPath(),
+          testTools(executionTarget).build.dotNetSdk[testMethod.settings.sdk].dotnetExecutable.toPath(),
           listOf("tool", "install", "fantomas-tool", "-g", "--version", globalVersion),
           env
         )
