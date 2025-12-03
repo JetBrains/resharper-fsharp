85ad0245e1c8eb444c9ef06df3bbb962be10d9e0
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2025-12-03 06:17:55 +0300

    wip

---
+++ ReSharper.FSharp/test/data/features/intentions/disableWarning/Disable all 04.fs
@@ -0,0 +1,10 @@
+module M
+
+// ReSharper disable
+// ReSharper disable A, B
+// ReSharper disable once B, C
+let _ = id{caret} 1
+// ReSharper restore A
+// ReSharper restore B, C
+
+// ${QF_TO_EXECUTE:Disable all inspections in file}
