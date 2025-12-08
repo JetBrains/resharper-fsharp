8dc384352c92e1f2b987fd9ce3725f31c5078e70
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2025-12-08 17:26:04 +0200

    RIDER-133652 [rider-tests]: Add new modules for integration test framework and remove TestNG dependency from intellij.rider.test.framework
    GitOrigin-RevId: 49c638282817cd56659b5f6f14c49382a97fad64

--- rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/FSharpCompletionTest.kt
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/FSharpCompletionTest.kt
@@ -5,6 +5,7 @@ import com.jetbrains.rider.test.annotations.TestSettings
 import com.jetbrains.rider.test.base.PatchEngineCompletionTestBase
 import com.jetbrains.rider.test.enums.BuildTool
 import com.jetbrains.rider.test.enums.sdk.SdkVersion
+import com.jetbrains.rider.test.facades.editor.PatchEngineEditorTestMode
 import com.jetbrains.rider.test.scriptingApi.dumpOpenedEditorFacade
 import org.testng.annotations.Test
 
