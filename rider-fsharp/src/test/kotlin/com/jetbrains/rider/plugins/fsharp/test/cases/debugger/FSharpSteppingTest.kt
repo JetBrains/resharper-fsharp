9c711fd19febe2d04662ccfe5a61c29cb15cc975
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2025-12-17 16:00:16 +0100

    RIDER-134031 Debugger: always step over hidden sequence points
    In class constructors, F# compiler generates an additional hidden sequence point with the base constructor call, and we tried to step into again when ended up in this sequence point. The expected behavior is to step over such sequence points
    
    (cherry picked from commit 6504ba06d4ec3330783c125cbffaa2c9f51b0ced)
    
    IJ-MR-186246
    
    GitOrigin-RevId: df7d28e4617253fadd2b5e057a4b31f4c805c319

---
+++ rider-fsharp/src/test/kotlin/com/jetbrains/rider/plugins/fsharp/test/cases/debugger/FSharpSteppingTest.kt
@@ -0,0 +1,42 @@
+package com.jetbrains.rider.plugins.fsharp.test.cases.debugger
+
+import com.jetbrains.rider.test.annotations.Solution
+import com.jetbrains.rider.test.annotations.TestSettings
+import com.jetbrains.rider.test.base.DebuggerTestBase
+import com.jetbrains.rider.test.enums.BuildTool
+import com.jetbrains.rider.test.enums.sdk.SdkVersion
+import com.jetbrains.rider.test.scriptingApi.dumpFullCurrentData
+import com.jetbrains.rider.test.scriptingApi.resumeSession
+import com.jetbrains.rider.test.scriptingApi.stepInto
+import com.jetbrains.rider.test.scriptingApi.stepOver
+import com.jetbrains.rider.test.scriptingApi.toggleBreakpoint
+import com.jetbrains.rider.test.scriptingApi.waitForPause
+import org.testng.annotations.Test
+
+@Test
+@TestSettings(sdkVersion = SdkVersion.LATEST_STABLE, buildTool = BuildTool.SDK)
+@Solution("SteppingTests")
+class FSharpSteppingTest : DebuggerTestBase() {
+    override val projectName = "SteppingTests"
+
+    @Test
+    fun testHiddenSequencePointInConstructor() {
+        testDebugProgram({
+            toggleBreakpoint("Program.fs", 5)
+        }, {
+            waitForPause()
+            dumpFullCurrentData()
+
+            stepInto()
+            dumpFullCurrentData()
+
+            stepOver()
+            dumpFullCurrentData()
+
+            stepOver()
+            dumpFullCurrentData()
+
+            resumeSession()
+        }, true)
+    }
+}
