83d0163b830502ceee8864629b5ccef47f022143
andrew.karpov <andrew.karpov@jetbrains.com>
2025-12-23 19:49:55 +0100

    RSRP-502572 Reduce the perf impact of the type-checking
    WIP
    
    `INodeTypesInitializer` language service
    
    GitOrigin-RevId: 797b5c122308f0f4e54da958c3b9200178a190d8

--- ReSharper.FSharp/src/FSharp/FSharp.Psi/src/Impl/Tree/ChameleonExpressionNodeType.cs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi/src/Impl/Tree/ChameleonExpressionNodeType.cs
@@ -7,25 +7,28 @@ using Microsoft.FSharp.Collections;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Impl.Tree
 {
-  public class ChameleonExpressionNodeType : CompositeNodeWithArgumentType
+  partial class ElementType
   {
+    public static readonly CompositeNodeType CHAMELEON_EXPRESSION_WITH_ARG = new ChameleonExpressionNodeType();
+
     // Tokens start at 1000, generated elements at 2000, so let's just use 3000.
-    public const int NodeIndex = 3000;
+    public static readonly int CHAMELEON_EXPRESSION_WITH_ARG_NODE_TYPE_INDEX = 3000;
 
-    public static readonly NodeType Instance = new ChameleonExpressionNodeType();
-
-    private ChameleonExpressionNodeType() : base("F# Chameleon Expression Node Type", NodeIndex, typeof(ChameleonExpression))
+    private sealed class ChameleonExpressionNodeType : CompositeNodeWithArgumentType
     {
-    }
+      public ChameleonExpressionNodeType() : base("F# Chameleon Expression Node Type", CHAMELEON_EXPRESSION_WITH_ARG_NODE_TYPE_INDEX, typeof(ChameleonExpression))
+      {
+      }
 
-    public override CompositeElement Create() =>
-      throw new InvalidOperationException("Should not be created without data.");
+      public override CompositeElement Create() =>
+        throw new InvalidOperationException("Should not be created without data.");
 
-    public override CompositeElement Create(object data)
-    {
-      var (expr, warningDirectives, startOffset, lineStart) = (Tuple<SynExpr, FSharpList<WarnDirectiveTrivia>, int, int>) data;
-      var chameleonExpression = new ChameleonExpression(expr, warningDirectives, startOffset, lineStart);
-      return chameleonExpression;
+      public override CompositeElement Create(object data)
+      {
+        var (expr, warningDirectives, startOffset, lineStart) = (Tuple<SynExpr, FSharpList<WarnDirectiveTrivia>, int, int>) data;
+        var chameleonExpression = new ChameleonExpression(expr, warningDirectives, startOffset, lineStart);
+        return chameleonExpression;
+      }
     }
   }
 }
