189d914f5b8e4716669ad5fa93f32fc4a6326d40
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-02-27 18:16:07 +0100

    RIDER-124491 Completion/fcs: don't commit on Accept
    (cherry picked from commit 21049be39936507dab4e11d9888ae6ae4969ac90)
    
    GitOrigin-RevId: fc5b06d190656aec8745efb1c92d355e2c4b94cd

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeCompletion/FcsLookupItem.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Features/src/CodeCompletion/FcsLookupItem.fs
@@ -3,26 +3,19 @@ namespace rec JetBrains.ReSharper.Plugins.FSharp.Psi.Features.CodeCompletion
 open FSharp.Compiler.CodeAnalysis
 open FSharp.Compiler.EditorServices
 open FSharp.Compiler.Symbols
-open JetBrains.Diagnostics
 open JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems
 open JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure.LookupItems.Impl
 open JetBrains.ReSharper.Feature.Services.Lookup
 open JetBrains.ReSharper.Feature.Services.ParameterInfo
 open JetBrains.RdBackend.Common.Features.Completion
-open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.CodeCompletion
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util.FcsTypeUtil
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Util
 open JetBrains.ReSharper.Plugins.FSharp.Util
 open JetBrains.ReSharper.Plugins.FSharp.Util.FcsTaggedText
-open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.Modules
-open JetBrains.ReSharper.Psi.Transactions
-open JetBrains.ReSharper.Resources.Shell
 open JetBrains.UI.RichText
 open JetBrains.Util
 
@@ -77,7 +70,6 @@ type FcsErrorLookupItem(item: DeclarationListItem) =
 type IFcsLookupItemInfo =
     abstract FcsSymbol: FSharpSymbol
     abstract FcsSymbolUse: FSharpSymbolUse
-    abstract NamespaceToOpen: string array
 
 type FcsLookupItem(items: RiderDeclarationListItems, context: FSharpCodeCompletionContext) =
     inherit TextLookupItemBase()
@@ -95,7 +87,6 @@ type FcsLookupItem(items: RiderDeclarationListItems, context: FSharpCodeCompleti
 
     interface IFcsLookupItemInfo with
         member this.FcsSymbol = this.FcsSymbol
-        member this.NamespaceToOpen = this.NamespaceToOpen
         member this.FcsSymbolUse = this.FcsSymbolUse
 
     member x.Candidates =
@@ -121,71 +112,10 @@ type FcsLookupItem(items: RiderDeclarationListItems, context: FSharpCodeCompleti
             | _ -> null
         with _ -> null
 
-    override this.Accept(textControl, nameRange, insertType, suffix, solution, keepCaretStill) =
-        use pinCheckResultsCookie =
-            Assertion.Assert(context.ParseAndCheckResults.IsValueCreated)
-            textControl.GetFSharpFile(solution).PinTypeCheckResults(context.ParseAndCheckResults.Value)
-
-        base.Accept(textControl, nameRange, insertType, suffix, solution, keepCaretStill)
-
-    override x.OnAfterComplete(textControl, nameRange, decorationRange, tailType, suffix, caretPositionRangeMarker) =
-        base.OnAfterComplete(textControl, &nameRange, &decorationRange, tailType, &suffix, &caretPositionRangeMarker)
-
-        // todo: 213: exit early if there's no need in additional binding
-        context.BasicContext.Solution.GetPsiServices().Files.CommitAllDocuments()
-
-        let fsFile = context.BasicContext.SourceFile.FSharpFile
-        let psiServices = fsFile.GetPsiServices()
-
-        use writeCookie = WriteLockCookie.Create(fsFile.IsPhysical())
-        use transactionCookie = PsiTransactionCookie.CreateAutoCommitCookieWithCachesUpdate(psiServices, "Add open")
-
-        let declaredElement = x.FcsSymbol.GetDeclaredElement(context.PsiModule)
-
-        let typeElement =
-            // todo: other declared elements
-            match declaredElement with
-            | :? ITypeElement as typeElement -> typeElement
-            | :? IField as field when (field.ContainingType :? IEnum) -> field.ContainingType
-            | _ -> null
-
-        let ns = items.NamespaceToOpen
-        let moduleToImport =
-            if isNotNull typeElement then
-                let moduleToOpen = getModuleToOpen typeElement
-                ModuleToImport.DeclaredElement(moduleToOpen) else
-
-            let ns = ns |> Array.map FSharpNamingService.normalizeBackticks |> String.concat "."
-            ModuleToImport.FullName(ns)
-
-        let offset = context.Ranges.InsertRange.StartOffset
-        // todo: getting reference owner in parse errors, e.g. unfinished `if`
-
-        let referenceOwner = fsFile.GetNode<IFSharpReferenceOwner>(offset)
-        if isNotNull referenceOwner && isNotNull typeElement && not referenceOwner.Reference.IsQualified then
-            let clrDeclaredElement: IClrDeclaredElement =
-                // todo: other elements: union cases
-                match declaredElement with
-                | :? ITypeElement as typeElement -> typeElement :> _
-                | :? IField as field when (field.ContainingType :? IEnum) -> field :> _
-                | _ -> null
-
-            if isNotNull clrDeclaredElement then
-                referenceOwner.Reference.SetRequiredQualifiers(clrDeclaredElement, referenceOwner)
-
-        if not (ns.IsEmpty()) then
-            addOpen offset fsFile moduleToImport
-
     override x.GetDisplayName() =
         let name = LookupUtil.FormatLookupString(items.Name, x.TextColor)
-
         if emphasize then
             LookupUtil.AddEmphasize(name, TextRange(0, name.Length))
-
-        let ns = items.NamespaceToOpen
-        if not (ns.IsEmpty()) then
-            let ns = String.concat "." ns
-            LookupUtil.AddInformationText(name, $"(in {ns})")
 
         name
 
