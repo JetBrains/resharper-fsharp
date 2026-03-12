8fa24ce1e81096df56c6f6375609a4d863d41e23
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-03-12 15:11:38 +0100

    RIDER-129505 Debugger: do not step into intrinsic methods
    * Allows reusing logic between frame filters and additional debugger hidden providers
    * Introduces an additional debugger hidden provider for intrinsic methods and types
    * Marks implicit conversion expression calls as hidden
    
    GitOrigin-RevId: b046cd943ae1a1bb24d7315f4965e68d689ef2a6

--- ReSharper.FSharp/src/FSharp.Debugger/src/FSharpTypeFuncFrameFilter.cs
+++ ReSharper.FSharp/src/FSharp.Debugger/src/FSharpTypeFuncFrameFilter.cs
@@ -1,25 +1,13 @@
 ﻿using JetBrains.Metadata.Reader.API;
 using Mono.Debugging.Autofac;
-using Mono.Debugging.Backend.CallStacks;
-using Mono.Debugging.Client.CallStacks;
-using Mono.Debugging.Client.Steppers;
 using Mono.Debugging.Utils;
 using Mono.Debugging.Win32;
-using Mono.Debugging.Win32.Frames;
 
 namespace JetBrains.ReSharper.Plugins.FSharp.Debugger;
 
 [DebuggerSessionComponent(typeof(CorDebuggerType))]
-public class FSharpTypeFuncFrameFilter(CorDebuggerSession session) : IFrameFilter, IAdditionalDebuggerHiddenProvider
+public class FSharpTypeFuncFrameFilter : IAdditionalDebuggerHiddenProvider
 {
-  public bool SkipFrame(IStackFrame frame)
-  {
-    if (frame is not IStackFrame<ICorFrame> corStackFrame) return false;
-
-    var method = session.GetMetadataMethod(corStackFrame.Frame);
-    return method.IsFSharpTypeFuncSpecialize();
-  }
-
-  public bool IsHidden(MethodSpecification methodSpec) =>
-    methodSpec.Method.IsFSharpTypeFuncSpecialize();
+  public bool IsHidden(IMetadataMethod metadataMethod) =>
+    metadataMethod.IsFSharpTypeFuncSpecialize();
 }
