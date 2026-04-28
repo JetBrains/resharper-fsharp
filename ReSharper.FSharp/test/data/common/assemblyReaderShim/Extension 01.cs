12ae63a04da0dfe0dd35e2228faefff720059e75
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-04-28 17:40:06 +0200

    RIDER-138441 Module reader: fix C# 14 extensions are not visible from F#
    GitOrigin-RevId: 614d0e0eebf540d5c3e4cf3a5f172aabeeb899c1

---
+++ ReSharper.FSharp/test/data/common/assemblyReaderShim/Extension 01.cs
@@ -0,0 +1,8 @@
+public static class Extensions
+{
+    extension(string str)
+    {
+        public int Method() => 1;
+        public int Property => 1;
+    }
+}
---
+++ ReSharper.FSharp/test/data/common/assemblyReaderShim/Extension 01.fs
@@ -0,0 +1,9 @@
+module Module
+
+let i1: int = "".Method()
+let i2: int = "".Property
+let i3: int = "".get_Property()
+
+let i4: int = Extensions.Method("")
+let i5: int = Extensions.get_Property("")
+let i6: int = Extensions.Property("")
