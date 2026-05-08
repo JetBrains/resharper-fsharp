d7b37780969bb861486d181fcc362cdea6dc744c
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2026-05-08 20:53:05 +0200

    RIDER-138774 Import/type: extract ordering file access check
    GitOrigin-RevId: 24ab8a64e1cd15ff9de6f8392e24a3d6c05daba6

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/FSharpImportTypeFix.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/FSharpImportTypeFix.fs
@@ -1,7 +1,19 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
 
 open JetBrains.ReSharper.Intentions.QuickFixes
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
+open JetBrains.ReSharper.Psi
 open JetBrains.ReSharper.Psi.Caches
+
+[<RequireQualifiedAccess>]
+module FSharpImportTypeFix =
+    let doAdditionalSorting (candidates: ITypeElement seq) =
+        candidates
+        |> Seq.sortByDescending (function
+            | :? IFSharpTypeElement as fsTypeElement -> fsTypeElement.GetFSharpAccessRights().IsFilePrivate
+            | _ -> false
+        )
 
 type FSharpImportTypeFix(reference) =
     inherit ImportTypeFix(reference)
@@ -10,6 +22,9 @@ type FSharpImportTypeFix(reference) =
         let symbolCache = context.GetPsiServices().Symbols
         symbolCache.GetAlternativeNamesSymbolScope(context.GetPsiModule(), true)
 
+    override this.DoAdditionalOrdering(candidates) =
+        FSharpImportTypeFix.doAdditionalSorting candidates
+
 
 type FSharpReferenceModuleAndTypeFix(reference) =
     inherit ReferenceModuleAndTypeFix(reference)
@@ -17,3 +32,6 @@ type FSharpReferenceModuleAndTypeFix(reference) =
     override this.GetSymbolScope(context) =
         let symbolCache = context.GetPsiServices().Symbols
         symbolCache.GetAlternativeNamesSymbolScope(LibrarySymbolScope.FULL)
+
+    override this.DoAdditionalOrdering(candidates) =
+        FSharpImportTypeFix.doAdditionalSorting candidates
