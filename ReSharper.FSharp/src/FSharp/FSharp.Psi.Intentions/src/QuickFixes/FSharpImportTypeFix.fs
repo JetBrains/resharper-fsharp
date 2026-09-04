83cc31b5cf0c3dc6af212679892d4138f3f1da8b
Alexey.Berezhnykh <alexey.berezhnykh@jetbrains.com>
2026-09-04 18:31:47 +0300

    rebase

--- ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/FSharpImportTypeFix.fs
+++ ReSharper.FSharp/src/FSharp/FSharp.Psi.Intentions/src/QuickFixes/FSharpImportTypeFix.fs
@@ -4,58 +4,8 @@ open JetBrains.ReSharper.Intentions.QuickFixes
 open JetBrains.ReSharper.Plugins.FSharp.Psi
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
-namespace JetBrains.ReSharper.Plugins.FSharp.Psi.Features.Daemon.QuickFixes
-
-open JetBrains.ReSharper.Intentions.QuickFixes
-open JetBrains.ReSharper.Plugins.FSharp.Psi
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Impl
-open JetBrains.ReSharper.Plugins.FSharp.Psi.Services.Util
-open JetBrains.ReSharper.Psi
-open JetBrains.ReSharper.Psi.Caches
-
-[<RequireQualifiedAccess>]
-module FSharpImportTypeFix =
-    let doAdditionalSorting (candidates: ITypeElement seq) =
-        candidates
-        |> Seq.sortByDescending (function
-            | :? IFSharpTypeElement as fsTypeElement -> fsTypeElement.GetFSharpAccessRights().IsFilePrivate
-            | _ -> false
-        )
-
-type FSharpImportTypeFix(reference) =
-    inherit ImportTypeFix(reference)
-
-    override this.GetSymbolScope(context) =
-        let symbolCache = context.GetPsiServices().Symbols
-        symbolCache.GetAlternativeNamesSymbolScope(context.GetPsiModule(), true)
-
-    override this.DoAdditionalOrdering(candidates) =
-        FSharpImportTypeFix.doAdditionalSorting candidates
-
-type FSharpPopupImportTypeFix(reference) =
-    inherit ImportTypeQuickPopupFix(reference)
-
-    override this.GetSymbolScope(context) =
-        let symbolCache = context.GetPsiServices().Symbols
-        symbolCache.GetAlternativeNamesSymbolScope(context.GetPsiModule(), true)
-
-    override this.DoAdditionalOrdering(candidates) =
-        FSharpImportTypeFix.doAdditionalSorting candidates
-
-
-type FSharpReferenceModuleAndTypeFix(reference) =
-    inherit ReferenceModuleAndTypeFix(reference)
-
-    override this.IsAvailable(cache) =
-        base.IsAvailable(cache) &&
-        not (FSharpImportStaticMemberUtil.isAvailable reference)
-
-    override this.GetSymbolScope(context) =
-        let symbolCache = context.GetPsiServices().Symbols
-        symbolCache.GetAlternativeNamesSymbolScope(LibrarySymbolScope.FULL)
-
-    override this.DoAdditionalOrdering(candidates) =
-        FSharpImportTypeFix.doAdditionalSorting candidates
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Intentions.Resources
+open JetBrains.ReSharper.Plugins.FSharp.Psi.Parsing
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Services.Util
 open JetBrains.ReSharper.Plugins.FSharp.Psi.Tree
 open JetBrains.ReSharper.Plugins.FSharp.Util
